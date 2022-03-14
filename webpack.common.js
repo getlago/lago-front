fa2b47f0121ca7d8b976895d8fb570d5d10c59c6
MorganeLecurieux <morgane.lecurieux.lafayette@gmail.com>
2022-03-14 09:07:46 -0700

    chore: 👩‍✈️ Configure Linter/Eslint

--- webpack.common.js
+++ webpack.common.js
@@ -1,34 +1,34 @@
-require("dotenv").config();
-const path = require("path");
+require('dotenv').config()
+const path = require('path')
 
-const webpack = require("webpack");
+const webpack = require('webpack')
 
-const APP_ENV = process.env.APP_ENV ?? "development";
+const APP_ENV = process.env.APP_ENV ?? 'development'
 
 module.exports = () => {
   return {
-    entry: path.resolve(__dirname, "src/index.js"),
+    entry: path.resolve(__dirname, 'src/index.js'),
     output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "[name].[chunkhash].js",
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].[chunkhash].js',
     },
     resolve: {
-      extensions: ["*", ".ts", ".tsx", ".js"],
+      extensions: ['*', '.ts', '.tsx', '.js'],
       alias: {
-        "~": path.resolve(__dirname, "src"),
+        '~': path.resolve(__dirname, 'src'),
       },
       symlinks: false,
     },
     plugins: [
       new webpack.ProvidePlugin({
-        React: "react",
+        React: 'react',
       }),
       new webpack.DefinePlugin({
-        IS_PROD_ENV: APP_ENV === "production",
+        IS_PROD_ENV: APP_ENV === 'production',
         APP_ENV: JSON.stringify(APP_ENV),
         API_URL: JSON.stringify(process.env.API_URL),
         APP_VERSION: JSON.stringify(process.env.APP_VERSION),
       }),
     ],
-  };
-};
+  }
+}
