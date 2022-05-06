6a9b680197546cb7b1a93ac15f15c594f8449a73
MorganeLecurieux <morgane.lecurieux.lafayette@gmail.com>
2022-05-06 17:46:40 +0200

    feat: 🌲 Update bm + plan layout

--- src/components/CodeSnippet.tsx
+++ src/components/CodeSnippet.tsx
@@ -4,11 +4,11 @@ import 'prismjs/components/prism-javascript'
 import 'prismjs/components/prism-ruby'
 import 'prismjs/components/prism-bash'
 import 'prismjs/plugins/line-numbers/prism-line-numbers'
-import 'prismjs/themes/prism-okaidia.css'
+// import 'prismjs/themes/prism.css'
 import styled from 'styled-components'
 
-import { Typography, Button, Skeleton } from '~/components/designSystem'
-import { theme } from '~/styles'
+import { Typography, Button } from '~/components/designSystem'
+import { theme, NAV_HEIGHT } from '~/styles'
 import { useI18nContext } from '~/core/I18nContext'
 import { addToast } from '~/core/apolloClient'
 
@@ -34,28 +34,20 @@ export const CodeSnippet = memo(
 
     return (
       <Content className={className}>
-        {loading ? (
+        {loading ? null : (
           <>
-            <Skeleton variant="text" width="inherit" height={12} marginBottom={theme.spacing(9)} />
-            <Skeleton variant="text" width={168} height={12} marginBottom={theme.spacing(4)} />
-            <BlockSkeleton variant="text" width="inherit" height={112} />
-          </>
-        ) : (
-          <>
-            <Title variant="bodyHl" color="textSecondary">
-              {translate('text_623b42ff8ee4e000ba87d0b2')}
-            </Title>
-
+            <Head>
+              <Typography variant="bodyHl">{translate('text_623b42ff8ee4e000ba87d0b2')}</Typography>
+            </Head>
             <Pre className="line-numbers">
               <Code ref={codeRef} className={`language-${language}`}>
                 {code}
               </Code>
               <span />
             </Pre>
-            <Button
+            <CopyButton
               variant="secondary"
               startIcon="duplicate"
-              fullWidth
               onClick={() => {
                 navigator.clipboard.writeText(code)
 
@@ -66,7 +58,7 @@ export const CodeSnippet = memo(
               }}
             >
               {translate('text_623b42ff8ee4e000ba87d0c6')}
-            </Button>
+            </CopyButton>
           </>
         )}
       </Content>
@@ -76,14 +68,19 @@ export const CodeSnippet = memo(
 
 CodeSnippet.displayName = 'CodeSnippet'
 
-const BlockSkeleton = styled(Skeleton)`
-  border-radius: 12px !important;
+const Head = styled.div`
+  height: ${NAV_HEIGHT}px;
+  padding: 0 ${theme.spacing(8)};
+  display: flex;
+  align-items: center;
+  box-shadow: ${theme.shadows[7]};
 `
 
 const Pre = styled.pre`
-  background-color: ${theme.palette.grey[700]};
   padding: ${theme.spacing(4)};
-  border-radius: 12px;
+  background-color: transparent !important;
+  height: calc(100% - ${NAV_HEIGHT}px);
+  box-sizing: border-box;
 `
 
 const Code = styled.code`
@@ -93,11 +90,19 @@ const Code = styled.code`
   line-height: 20px;
 `
 
-const Title = styled(Typography)`
-  margin-bottom: ${theme.spacing(6)};
+const CopyButton = styled(Button)`
+  position: absolute;
+  bottom: ${theme.spacing(12)};
+  margin: auto;
+  left: 0;
+  right: 0;
+  width: fit-content;
 `
 
 const Content = styled.div`
+  position: relative;
+  height: 100%;
+
   pre[class*='language-'] {
     margin: 0 0 ${theme.spacing(3)} 0;
     display: flex;
@@ -135,12 +140,157 @@ const Content = styled.div`
 
   .line-numbers-rows > span:before {
     content: counter(linenumber);
-    color: ${theme.palette.common.white};
+    color: ${theme.palette.grey[500]};
     display: block;
     text-align: right;
   }
 
   > span {
     width: ${theme.spacing(3)};
+  }
+
+  /**
+ * prism.js default theme for JavaScript, CSS and HTML
+ * Based on dabblet (http://dabblet.com)
+ * @author Lea Verou
+ */
+
+  code[class*='language-'],
+  pre[class*='language-'] {
+    color: #19212e;
+    background: none;
+    text-shadow: 0 1px white;
+    font-family: Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace;
+    font-size: 1em;
+    text-align: left;
+    white-space: pre;
+    word-spacing: normal;
+    word-break: normal;
+    word-wrap: normal;
+    line-height: 1.5;
+
+    -moz-tab-size: 4;
+    -o-tab-size: 4;
+    tab-size: 4;
+
+    -webkit-hyphens: none;
+    -moz-hyphens: none;
+    -ms-hyphens: none;
+    hyphens: none;
+  }
+
+  pre[class*='language-']::-moz-selection,
+  pre[class*='language-'] ::-moz-selection,
+  code[class*='language-']::-moz-selection,
+  code[class*='language-'] ::-moz-selection {
+    text-shadow: none;
+    background: #b3d4fc;
+  }
+
+  pre[class*='language-']::selection,
+  pre[class*='language-'] ::selection,
+  code[class*='language-']::selection,
+  code[class*='language-'] ::selection {
+    text-shadow: none;
+    background: #b3d4fc;
+  }
+
+  @media print {
+    code[class*='language-'],
+    pre[class*='language-'] {
+      text-shadow: none;
+    }
+  }
+
+  /* Code blocks */
+  pre[class*='language-'] {
+    padding: 1em;
+    margin: 0.5em 0;
+    overflow: auto;
+  }
+
+  :not(pre) > code[class*='language-'],
+  pre[class*='language-'] {
+    background: #f5f2f0;
+  }
+
+  /* Inline code */
+  :not(pre) > code[class*='language-'] {
+    padding: 0.1em;
+    border-radius: 0.3em;
+    white-space: normal;
+  }
+
+  .token.comment,
+  .token.prolog,
+  .token.doctype,
+  .token.cdata {
+    color: slategray;
+  }
+
+  .token.punctuation {
+    color: #8c95a6;
+  }
+
+  .token.namespace {
+    opacity: 0.7;
+  }
+
+  .token.property,
+  .token.tag,
+  .token.boolean,
+  .token.number,
+  .token.constant,
+  .token.symbol,
+  .token.deleted {
+    color: #422cc1;
+  }
+
+  .token.selector,
+  .token.attr-name,
+  .token.string,
+  .token.char,
+  .token.builtin,
+  .token.inserted {
+    color: #008559;
+  }
+
+  .token.operator,
+  .token.entity,
+  .token.url,
+  .language-css .token.string,
+  .style .token.string {
+    color: #c86d02;
+    /* This background color was intended by the author of this theme. */
+    background: hsla(0, 0%, 100%, 0.5);
+  }
+
+  .token.atrule,
+  .token.attr-value,
+  .token.keyword {
+    color: #006cfa;
+  }
+
+  .token.function,
+  .token.class-name {
+    color: #f6491e;
+  }
+
+  .token.regex,
+  .token.important,
+  .token.variable {
+    color: #ff7e1d;
+  }
+
+  .token.important,
+  .token.bold {
+    font-weight: bold;
+  }
+  .token.italic {
+    font-style: italic;
+  }
+
+  .token.entity {
+    cursor: help;
   }
 `
