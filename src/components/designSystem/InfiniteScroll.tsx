678b0208c4adfe83b9c493d663c3c1428b42869d
MorganeLecurieux <morgane.lecurieux.lafayette@gmail.com>
2022-05-18 17:44:36 +0200

    chore: 🖱 Infinite scroll on lists

---
+++ src/components/designSystem/InfiniteScroll.tsx
@@ -0,0 +1,42 @@
+import React, { useRef, useEffect, ReactNode } from 'react'
+
+interface InfiniteScrollProps {
+  onBottom: () => void
+  children: ReactNode
+}
+
+export const InfiniteScroll = ({ children, onBottom }: InfiniteScrollProps) => {
+  const hiddenBottom = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        // isIntersecting is true when element and viewport are overlapping
+        if (entries[0].isIntersecting === true) {
+          onBottom && onBottom()
+        }
+      },
+      { threshold: [0] }
+    )
+
+    let element: HTMLDivElement
+
+    if (hiddenBottom.current) {
+      observer.observe(hiddenBottom.current)
+      element = hiddenBottom.current
+    }
+
+    return () => {
+      if (element) {
+        observer.unobserve(element)
+      }
+    }
+  }, [hiddenBottom, onBottom])
+
+  return (
+    <>
+      {children}
+      <div ref={hiddenBottom} />
+    </>
+  )
+}
