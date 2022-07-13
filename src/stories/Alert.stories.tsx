2d1897af1eee14d1c44210e941c749b96bc5895d
Alexandre Monjol <alexandre.monjol@qonto.com>
2022-07-13 11:01:44 +0200

    chore: 💅 Add story for the Alert component

---
+++ src/stories/Alert.stories.tsx
@@ -0,0 +1,38 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import Maneki from './assets/maneki.png'
+
+import { Alert } from '../components/designSystem/Alert'
+
+export default {
+  title: 'Design System/Alert',
+  component: Alert,
+  argTypes: {
+    ButtonProps: { table: { disable: true } },
+  },
+} as ComponentMeta<typeof Alert>
+
+const Template: ComponentStory<typeof Alert> = (args) => (
+  <Alert type={args.type} className={args.className}>
+    {args.children}
+  </Alert>
+)
+
+export const Default = Template.bind({})
+Default.args = {
+  type: 'info',
+  children: "I'm an alert with a simple text",
+}
+
+export const ChildNode = Template.bind({})
+ChildNode.args = {
+  type: 'info',
+  children: (
+    <div>
+      Children can be a <b>node</b>
+      <br />
+      <br />
+      <img src={Maneki} alt="maneki" width="200" />
+    </div>
+  ),
+}
