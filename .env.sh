e3e84380293636ea64dbd1cd5e579e843a8075f2
MorganeLecurieux <morgane.lecurieux.lafayette@gmail.com>
2022-07-18 14:52:22 +0200

    chore: 🫀 Add APP_ENV to env config

--- .env.sh
+++ .env.sh
@@ -4,5 +4,7 @@ rm -rf ./env-config.js
 touch ./env-config.js
 
 api_url_value=$(echo $API_URL)
+app_env_value=$(echo $APP_ENV)
 
 echo "window.API_URL = \"$api_url_value\"" >> ./env-config.js
+echo "window.APP_ENV = \"$app_env_value\"" >> ./env-config.js
