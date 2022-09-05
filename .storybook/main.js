0a2f17d457d4a3530703090da2b3fb90bd5a89ac
MorganeLecurieux <morgane.lecurieux.lafayette@gmail.com>
2022-09-05 15:07:31 +0200

    chore: 💿 NavigationTab story

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,6 +8,7 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    'storybook-addon-react-router-v6',
   ],
   framework: '@storybook/react',
   core: {
