364c6527f43b5b1d56222315ba028ca74761ecfb
Alexandre Monjol <alexandre.monjol@gmail.com>
2022-09-16 11:40:16 +0200

    Member item with revoke doalog

---
+++ src/components/settings/members/MembershipItem.tsx
@@ -0,0 +1,115 @@
+import { forwardRef, MutableRefObject } from 'react'
+import styled from 'styled-components'
+import { gql } from '@apollo/client'
+
+import { theme } from '~/styles'
+import { Avatar, Typography, Skeleton, Button, Tooltip } from '~/components/designSystem'
+import { MembershipItemFragment } from '~/generated/graphql'
+import { useInternationalization } from '~/hooks/core/useInternationalization'
+import { useCurrentUserInfosVar } from '~/core/apolloClient'
+
+import { RevokeMembershipDialogRef } from './RevokeMembershipDialog'
+
+gql`
+  fragment MembershipItem on Membership {
+    id
+    user {
+      id
+      email
+    }
+    organization {
+      id
+      name
+    }
+  }
+`
+
+interface MembershipItemProps {
+  membership: MembershipItemFragment
+}
+
+export const MembershipItem = forwardRef<RevokeMembershipDialogRef, MembershipItemProps>(
+  ({ membership }: MembershipItemProps, ref) => {
+    const { user: currentUser } = useCurrentUserInfosVar()
+    const { id, user, organization } = membership
+    const { translate } = useInternationalization()
+
+    return (
+      <ItemContainer>
+        <LeftBlock>
+          <Avatar variant="user" identifier={(user.email || '').charAt(0)} size="medium" />
+          <Typography variant="body">{user.email}</Typography>
+        </LeftBlock>
+        {currentUser?.id !== user.id && (
+          <RightBlock>
+            <Tooltip placement="bottom-end" title={translate('text_63208bfc99e69a28211ec7f0')}>
+              <ActionButtonIcon
+                icon="trash"
+                variant="quaternary"
+                onClick={() => {
+                  ;(ref as MutableRefObject<RevokeMembershipDialogRef>)?.current?.openDialog({
+                    id,
+                    email: user.email || '',
+                    organizationName: organization.name,
+                  })
+                }}
+              />
+            </Tooltip>
+          </RightBlock>
+        )}
+      </ItemContainer>
+    )
+  }
+)
+
+MembershipItem.displayName = 'MembershipItem'
+
+export const MembershipItemSkeleton = () => {
+  return (
+    <ItemContainer>
+      <Skeleton variant="userAvatar" size="medium" marginRight={theme.spacing(6)} />
+      <Skeleton variant="text" height={12} width={240} />
+    </ItemContainer>
+  )
+}
+
+const ItemContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: ${theme.spacing(4)};
+  border: 1px solid ${theme.palette.grey[400]};
+  border-radius: 12px;
+`
+
+const LeftBlock = styled.div`
+  display: flex;
+  align-items: center;
+
+  > *:not(:last-child) {
+    margin-right: ${theme.spacing(4)};
+  }
+`
+
+const RightBlock = styled.div`
+  display: flex;
+  align-items: center;
+
+  > *:nth-child(1) {
+    margin-right: ${theme.spacing(4)};
+  }
+
+  > *:nth-child(2) {
+    margin-right: ${theme.spacing(3)};
+  }
+`
+
+const ActionButtonIcon = styled(Button)`
+  width: 24px !important;
+  height: 24px;
+  border-radius: 8px;
+
+  > *:hover {
+    cursor: pointer;
+  }
+`
---
+++ src/components/settings/members/RevokeMembershipDialog.tsx
@@ -0,0 +1,72 @@
+import { gql } from '@apollo/client'
+import { forwardRef, useImperativeHandle, useRef, useState } from 'react'
+
+import { Typography, DialogRef } from '~/components/designSystem'
+import { useRevokeMembershipMutation } from '~/generated/graphql'
+import { WarningDialog } from '~/components/WarningDialog'
+import { useInternationalization } from '~/hooks/core/useInternationalization'
+import { addToast } from '~/core/apolloClient'
+
+gql`
+  mutation revokeMembership($input: RevokeMembershipInput!) {
+    revokeMembership(input: $input) {
+      id
+    }
+  }
+`
+
+export interface RevokeMembershipDialogRef {
+  openDialog: (membershipInfos: { id: string; email: string; organizationName: string }) => unknown
+  closeDialog: () => unknown
+}
+
+export const RevokeMembershipDialog = forwardRef<RevokeMembershipDialogRef>((_, ref) => {
+  const dialogRef = useRef<DialogRef>(null)
+  const { translate } = useInternationalization()
+  const [revokeMembership] = useRevokeMembershipMutation({
+    onCompleted(data) {
+      if (data && data.revokeMembership) {
+        addToast({
+          message: translate('text_63208c711ce25db78140755d'),
+          severity: 'success',
+        })
+      }
+    },
+  })
+
+  const [membershipInfos, setMembershipInfos] = useState<
+    { id: string; email: string; organizationName: string } | undefined
+  >()
+
+  useImperativeHandle(ref, () => ({
+    openDialog: (infos) => {
+      setMembershipInfos(infos)
+      dialogRef.current?.openDialog()
+    },
+    closeDialog: () => dialogRef.current?.closeDialog(),
+  }))
+
+  return (
+    <WarningDialog
+      ref={dialogRef}
+      title={translate('text_63208bfc99e69a28211ec794')}
+      description={
+        <Typography>
+          {translate('text_63208bfc99e69a28211ec7a6', {
+            memberEmail: membershipInfos?.email,
+            organisationName: membershipInfos?.organizationName,
+          })}
+        </Typography>
+      }
+      onContinue={async () =>
+        await revokeMembership({
+          variables: { input: { id: membershipInfos?.id as string } },
+          refetchQueries: ['getMemberships'],
+        })
+      }
+      continueText={translate('text_63208bfc99e69a28211ec7b4')}
+    />
+  )
+})
+
+RevokeMembershipDialog.displayName = 'RevokeMembershipDialog'
