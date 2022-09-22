85477fc963be27ee51ed757bf63526287587f9a9
MorganeLecurieux <morgane.lecurieux.lafayette@gmail.com>
2022-09-22 12:35:21 -0600

    feat: 🪗 Accordion component

---
+++ src/components/designSystem/Accordion.tsx
@@ -0,0 +1,149 @@
+import { useState } from 'react'
+import { ReactNode } from 'react'
+import { Accordion as MuiAccordion, AccordionSummary, AccordionDetails } from '@mui/material'
+import styled from 'styled-components'
+
+import { theme, NAV_HEIGHT } from '~/styles'
+import { useInternationalization } from '~/hooks/core/useInternationalization'
+
+import { Button } from './Button'
+import { Tooltip } from './Tooltip'
+
+enum AccordionSizeEnum {
+  medium = 'medium',
+  large = 'large',
+}
+
+type AccordionSize = keyof typeof AccordionSizeEnum
+
+interface AccordionProps {
+  id?: string
+  summary: ReactNode
+  children: ReactNode | ((args: { isOpen: boolean }) => ReactNode)
+  initiallyOpen?: boolean
+  size?: AccordionSize
+  noContentMargin?: boolean
+}
+
+export const Accordion = ({
+  id,
+  summary,
+  children,
+  initiallyOpen = false,
+  size = AccordionSizeEnum.medium,
+  noContentMargin = false,
+}: AccordionProps) => {
+  const [isOpen, setIsOpen] = useState(initiallyOpen)
+  const { translate } = useInternationalization()
+
+  return (
+    <Container id={id}>
+      <StyledAccordion expanded={isOpen} onChange={(_, expanded) => setIsOpen(expanded)} square>
+        <Summary $size={size}>
+          <Tooltip
+            placement="top-start"
+            title={translate(
+              isOpen ? 'text_624aa732d6af4e0103d40e61' : 'text_624aa79870f60300a3c4d074'
+            )}
+          >
+            <Button
+              tabIndex={-1}
+              data-test="open-charge"
+              variant="quaternary"
+              size="small"
+              icon={isOpen ? 'chevron-down' : 'chevron-right'}
+            />
+          </Tooltip>
+          {summary}
+        </Summary>
+        <Details $size={size} $noContentMargin={noContentMargin}>
+          {typeof children === 'function' ? children({ isOpen }) : children}
+        </Details>
+      </StyledAccordion>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  border: 1px solid ${theme.palette.grey[400]};
+  border-radius: 12px;
+`
+
+const StyledAccordion = styled(MuiAccordion)`
+  border-radius: 12px;
+  overflow: hidden;
+
+  &.MuiAccordion-root.MuiPaper-root {
+    border-radius: 12px;
+    background-color: transparent;
+  }
+  &.MuiAccordion-root {
+    overflow: inherit;
+
+    &:before {
+      height: 0;
+    }
+    &.Mui-expanded {
+      margin: 0;
+    }
+  }
+
+  .MuiAccordionSummary-content {
+    width: 100%;
+  }
+`
+
+const Summary = styled(AccordionSummary)<{ $size?: AccordionSize }>`
+  && {
+    height: ${({ $size }) => ($size === AccordionSizeEnum.medium ? NAV_HEIGHT : 92)}px;
+    border-radius: 12px;
+
+    &.Mui-expanded {
+      border-radius: 12px 12px 0 0;
+    }
+
+    &.MuiAccordionSummary-root.Mui-focusVisible {
+      background-color: inherit;
+      box-shadow: 0px 0px 0px 4px ${theme.palette.primary[200]};
+      &:hover {
+        background-color: ${theme.palette.grey[100]};
+      }
+    }
+
+    &:hover {
+      background-color: ${theme.palette.grey[100]};
+    }
+
+    &:active {
+      background-color: ${theme.palette.grey[200]};
+    }
+
+    .MuiAccordionSummary-content {
+      display: flex;
+      height: ${({ $size }) => ($size === AccordionSizeEnum.medium ? NAV_HEIGHT : 92)}px;
+      box-sizing: border-box;
+      align-items: center;
+      padding: ${({ $size }) =>
+        $size === AccordionSizeEnum.medium ? theme.spacing(4) : theme.spacing(8)};
+
+      > *:first-child {
+        margin-right: ${theme.spacing(3)};
+      }
+    }
+  }
+`
+
+const Details = styled(AccordionDetails)<{ $size?: AccordionSize; $noContentMargin?: boolean }>`
+  display: flex;
+  flex-direction: column;
+  box-shadow: ${theme.shadows[5]};
+
+  &.MuiAccordionDetails-root {
+    padding: ${({ $size, $noContentMargin }) =>
+      $noContentMargin
+        ? 0
+        : $size === AccordionSizeEnum.medium
+        ? theme.spacing(4)
+        : theme.spacing(8)};
+  }
+`
--- src/components/plans/ChargeAccordion.tsx
+++ src/components/plans/ChargeAccordion.tsx
@@ -1,10 +1,9 @@
-import { useState, useCallback, MouseEvent } from 'react'
-import { Accordion, AccordionSummary, AccordionDetails } from '@mui/material'
+import { useCallback, MouseEvent } from 'react'
 import { FormikProps } from 'formik'
 import styled from 'styled-components'
 
-import { theme, NAV_HEIGHT } from '~/styles'
-import { Button, Typography, Tooltip } from '~/components/designSystem'
+import { theme } from '~/styles'
+import { Button, Typography, Tooltip, Accordion } from '~/components/designSystem'
 import { useInternationalization } from '~/hooks/core/useInternationalization'
 import { ChargeModelEnum, CurrencyEnum } from '~/generated/graphql'
 import { ComboBox, TextInput } from '~/components/form'
@@ -30,7 +29,6 @@ export const ChargeAccordion = ({
   disabled,
   formikProps,
 }: ChargeAccordionProps) => {
-  const [isOpen, setIsOpen] = useState(!formikProps.values.charges?.[index]?.id ? true : false)
   const { translate } = useInternationalization()
   const localCharge = formikProps.values.charges[index]
 
@@ -43,22 +41,11 @@ export const ChargeAccordion = ({
   )
 
   return (
-    <Container id={id}>
-      <StyledAccordion expanded={isOpen} onChange={(_, expanded) => setIsOpen(expanded)} square>
-        <Summary>
-          <Tooltip
-            placement="top-start"
-            title={translate(
-              isOpen ? 'text_624aa732d6af4e0103d40e61' : 'text_624aa79870f60300a3c4d074'
-            )}
-          >
-            <Button
-              data-test="open-charge"
-              variant="quaternary"
-              size="small"
-              icon={isOpen ? 'chevron-down' : 'chevron-right'}
-            />
-          </Tooltip>
+    <Accordion
+      id={id}
+      initiallyOpen={!formikProps.values.charges?.[index]?.id ? true : false}
+      summary={
+        <>
           <Title>
             <Typography variant="bodyHl" color="textSecondary" noWrap>
               {localCharge?.billableMetric?.name}
@@ -92,174 +79,115 @@ export const ChargeAccordion = ({
               />
             </Tooltip>
           )}
-        </Summary>
-        <Details>
-          <ComboBox
-            name="chargeModel"
-            disabled={disabled}
-            label={translate('text_624c5eadff7db800acc4ca0d')}
-            data={[
-              {
-                label: translate('text_624aa732d6af4e0103d40e6f'),
-                value: ChargeModelEnum.Standard,
-              },
-              {
-                label: translate('text_62793bbb599f1c01522e919f'),
-                value: ChargeModelEnum.Graduated,
-              },
-              {
-                label: translate('text_62a0b7107afa2700a65ef6e2'),
-                value: ChargeModelEnum.Percentage,
-              },
-              {
-                label: translate('text_6282085b4f283b0102655868'),
-                value: ChargeModelEnum.Package,
-              },
-              {
-                label: translate('text_6304e74aab6dbc18d615f386'),
-                value: ChargeModelEnum.Volume,
-              },
-            ]}
-            disableClearable
-            value={localCharge.chargeModel}
-            helperText={translate(
-              localCharge.chargeModel === ChargeModelEnum.Percentage
-                ? 'text_62ff5d01a306e274d4ffcc06'
-                : localCharge.chargeModel === ChargeModelEnum.Graduated
-                ? 'text_62793bbb599f1c01522e91a1'
-                : localCharge.chargeModel === ChargeModelEnum.Package
-                ? 'text_6282085b4f283b010265586c'
-                : localCharge.chargeModel === ChargeModelEnum.Volume
-                ? 'text_6304e74aab6dbc18d615f38a'
-                : 'text_624d9adba93343010cd14ca7'
-            )}
-            onChange={(value) => handleUpdate('chargeModel', value)}
-          />
+        </>
+      }
+    >
+      <Details>
+        <ComboBox
+          name="chargeModel"
+          disabled={disabled}
+          label={translate('text_624c5eadff7db800acc4ca0d')}
+          data={[
+            {
+              label: translate('text_624aa732d6af4e0103d40e6f'),
+              value: ChargeModelEnum.Standard,
+            },
+            {
+              label: translate('text_62793bbb599f1c01522e919f'),
+              value: ChargeModelEnum.Graduated,
+            },
+            {
+              label: translate('text_62a0b7107afa2700a65ef6e2'),
+              value: ChargeModelEnum.Percentage,
+            },
+            {
+              label: translate('text_6282085b4f283b0102655868'),
+              value: ChargeModelEnum.Package,
+            },
+            {
+              label: translate('text_6304e74aab6dbc18d615f386'),
+              value: ChargeModelEnum.Volume,
+            },
+          ]}
+          disableClearable
+          value={localCharge.chargeModel}
+          helperText={translate(
+            localCharge.chargeModel === ChargeModelEnum.Percentage
+              ? 'text_62ff5d01a306e274d4ffcc06'
+              : localCharge.chargeModel === ChargeModelEnum.Graduated
+              ? 'text_62793bbb599f1c01522e91a1'
+              : localCharge.chargeModel === ChargeModelEnum.Package
+              ? 'text_6282085b4f283b010265586c'
+              : localCharge.chargeModel === ChargeModelEnum.Volume
+              ? 'text_6304e74aab6dbc18d615f38a'
+              : 'text_624d9adba93343010cd14ca7'
+          )}
+          onChange={(value) => handleUpdate('chargeModel', value)}
+        />
 
-          {localCharge.chargeModel === ChargeModelEnum.Standard && (
-            <LineAmount>
-              <TextInput
-                name="amount"
-                beforeChangeFormatter={['positiveNumber', 'chargeDecimal']}
-                disabled={disabled}
-                label={translate('text_624453d52e945301380e49b6')}
-                placeholder={translate('text_624453d52e945301380e49b8')}
-                value={localCharge.amount || ''}
-                onChange={(value) => handleUpdate('amount', value)}
-              />
-              <ComboBox
-                name="amountCurrency"
-                disabled
-                data={Object.values(CurrencyEnum).map((currencyType) => ({
-                  value: currencyType,
-                }))}
-                disableClearable
-                value={currency}
-                onChange={() => {}}
-              />
-            </LineAmount>
-          )}
-          {localCharge.chargeModel === ChargeModelEnum.Package && (
-            <PackageCharge
-              currency={currency}
+        {localCharge.chargeModel === ChargeModelEnum.Standard && (
+          <LineAmount>
+            <TextInput
+              name="amount"
+              beforeChangeFormatter={['positiveNumber', 'chargeDecimal']}
               disabled={disabled}
-              chargeIndex={index}
-              formikProps={formikProps}
+              label={translate('text_624453d52e945301380e49b6')}
+              placeholder={translate('text_624453d52e945301380e49b8')}
+              value={localCharge.amount || ''}
+              onChange={(value) => handleUpdate('amount', value)}
             />
-          )}
-          {localCharge.chargeModel === ChargeModelEnum.Graduated && (
-            <GraduatedChargeTable
-              disabled={disabled}
-              chargeIndex={index}
-              currency={currency}
-              formikProps={formikProps}
+            <ComboBox
+              name="amountCurrency"
+              disabled
+              data={Object.values(CurrencyEnum).map((currencyType) => ({
+                value: currencyType,
+              }))}
+              disableClearable
+              value={currency}
+              onChange={() => {}}
             />
-          )}
-          {localCharge.chargeModel === ChargeModelEnum.Percentage && (
-            <ChargePercentage
-              currency={currency}
-              disabled={disabled}
-              chargeIndex={index}
-              formikProps={formikProps}
-            />
-          )}
-          {localCharge.chargeModel === ChargeModelEnum.Volume && (
-            <VolumeChargeTable
-              currency={currency}
-              disabled={disabled}
-              chargeIndex={index}
-              formikProps={formikProps}
-            />
-          )}
-        </Details>
-      </StyledAccordion>
-    </Container>
+          </LineAmount>
+        )}
+        {localCharge.chargeModel === ChargeModelEnum.Package && (
+          <PackageCharge
+            currency={currency}
+            disabled={disabled}
+            chargeIndex={index}
+            formikProps={formikProps}
+          />
+        )}
+        {localCharge.chargeModel === ChargeModelEnum.Graduated && (
+          <GraduatedChargeTable
+            disabled={disabled}
+            chargeIndex={index}
+            currency={currency}
+            formikProps={formikProps}
+          />
+        )}
+        {localCharge.chargeModel === ChargeModelEnum.Percentage && (
+          <ChargePercentage
+            currency={currency}
+            disabled={disabled}
+            chargeIndex={index}
+            formikProps={formikProps}
+          />
+        )}
+        {localCharge.chargeModel === ChargeModelEnum.Volume && (
+          <VolumeChargeTable
+            currency={currency}
+            disabled={disabled}
+            chargeIndex={index}
+            formikProps={formikProps}
+          />
+        )}
+      </Details>
+    </Accordion>
   )
 }
 
-const Container = styled.div`
-  border: 1px solid ${theme.palette.grey[400]};
-  border-radius: 12px;
-`
-
-const StyledAccordion = styled(Accordion)`
-  border-radius: 12px;
-  overflow: hidden;
-
-  &.MuiAccordion-root.MuiPaper-root {
-    border-radius: 12px;
-    background-color: transparent;
-  }
-  &.MuiAccordion-root:before {
-    height: 0;
-  }
-  &.MuiAccordion-root.Mui-expanded {
-    margin: 0;
-  }
-
-  .MuiAccordionSummary-content {
-    width: 100%;
-  }
-`
-
-const Summary = styled(AccordionSummary)`
-  && {
-    height: ${NAV_HEIGHT}px;
-    border-radius: 12px;
-
-    &.MuiAccordionSummary-root.Mui-focused {
-      border-radius: 12px;
-    }
-
-    .MuiAccordionSummary-content {
-      display: flex;
-      height: ${NAV_HEIGHT}px;
-      box-sizing: border-box;
-      align-items: center;
-      padding: ${theme.spacing(4)};
-
-      &:hover {
-        background-color: ${theme.palette.grey[100]};
-      }
-
-      > *:first-child {
-        margin-right: ${theme.spacing(4)};
-      }
-    }
-  }
-`
-
-const Details = styled(AccordionDetails)`
-  display: flex;
-  flex-direction: column;
-  box-shadow: ${theme.shadows[5]};
-
-  &.MuiAccordionDetails-root {
-    padding: ${theme.spacing(4)};
-
-    > *:not(:last-child) {
-      margin-bottom: ${theme.spacing(6)};
-    }
+const Details = styled.div`
+  > *:not(:last-child) {
+    margin-bottom: ${theme.spacing(6)};
   }
 `
 
