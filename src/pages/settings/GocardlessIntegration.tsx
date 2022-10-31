fa78b6ce07997365bbef0933019d1c6483d99ecf
Lovro Colic <lovro.colic.lc@gmail.com>
2022-10-31 12:19:46 +0100

    finalise gcl details page with mutation that creates access token

--- src/pages/settings/GocardlessIntegration.tsx
+++ src/pages/settings/GocardlessIntegration.tsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
+import { useLocation } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import { gql } from '@apollo/client'
 
 import { theme, PageHeader, NAV_HEIGHT, HEADER_TABLE_HEIGHT } from '~/styles'
@@ -13,7 +15,7 @@ import {
 } from '~/components/designSystem'
 import {
   useGocardlessIntegrationsSettingQuery,
-  useUpdateStripeIntegrationMutation,
+  useAddGocardlessPaymentProviderMutation,
 } from '~/generated/graphql'
 import { useInternationalization } from '~/hooks/core/useInternationalization'
 import GoCardless from '~/public/images/gocardless-large.svg'
@@ -32,29 +34,59 @@ gql`
       }
     }
   }
+  
+  mutation addGocardlessPaymentProvider($input: AddGocardlessPaymentProviderInput!) {
+    addGocardlessPaymentProvider(input: $input) {
+      id
+      hasAccessToken
+    }
+  }
 `
 
 const GocardlessIntegration = () => {
   const { translate } = useInternationalization()
   const { data, loading } = useGocardlessIntegrationsSettingQuery()
-  const [update] = useUpdateStripeIntegrationMutation({
-    onCompleted({ addStripePaymentProvider }) {
-      if (addStripePaymentProvider?.id) {
-        addToast({
-          message: translate('text_634ea0ecc6147de10ddb6645'),
-          severity: 'success',
-        })
-      }
-    },
-  })
+  const query = new URLSearchParams(useLocation().search);
+  const code = query.get('code');
+  const [isConnectionEstablished, setIsConnectionEstablished] = useState(false)
   const tabsOptions = [
     {
       title: translate('text_62b1edddbf5f461ab9712725'),
       link: GOCARDLESS_INTEGRATION_ROUTE,
     },
   ]
-
   const gocardlessPaymentProvider = (data?.currentUser?.organizations || [])[0]?.gocardlessPaymentProvider
+  const [addPaymentProvider] = useAddGocardlessPaymentProviderMutation({
+    onCompleted({ addGocardlessPaymentProvider }) {
+      if (addGocardlessPaymentProvider?.id) {
+        setIsConnectionEstablished(true)
+        addToast({
+          message: translate(
+             'text_634ea0ecc6147de10ddb6645'
+          ),
+          severity: 'success',
+        })
+      }
+    },
+  })
+
+  useEffect(() => {
+    if (code) {
+      addPaymentProvider({
+        variables: {
+          input: {
+            accessCode: code,
+          },
+        },
+      })
+    }
+  }, [])
+
+  useEffect(() => {
+    if (gocardlessPaymentProvider) {
+      setIsConnectionEstablished(true)
+    }
+  }, [gocardlessPaymentProvider])
 
   return (
     <div>
@@ -93,7 +125,7 @@ const GocardlessIntegration = () => {
                 <Typography variant="headline">
                   {translate('text_634ea0ecc6147de10ddb6629')}
                 </Typography>
-                <Chip label={translate('text_634ea0ecc6147de10ddb662d')} />
+                {isConnectionEstablished && <Chip label={translate('text_634ea0ecc6147de10ddb662d')} />}
               </Line>
               <Typography>{translate('text_634ea0ecc6147de10ddb6631')}</Typography>
             </div>
@@ -105,14 +137,14 @@ const GocardlessIntegration = () => {
         <Head $empty={!!gocardlessPaymentProvider && !loading}>
           <Title variant="subhead">{translate('text_634ea0ecc6147de10ddb663d')}</Title>
           <Button
-            disabled={!gocardlessPaymentProvider}
+            disabled={!isConnectionEstablished}
             variant="secondary"
             onClick={() => window.open('https://proxy.lago.dev/gocardless/auth', '_blank') }
           >
             {translate('text_634ea0ecc6147de10ddb6639')}
           </Button>
         </Head>
-        <Subtitle>{translate('text_634ea0ecc6147de10ddb6641')}</Subtitle>
+        {isConnectionEstablished && <Subtitle>{translate('text_634ea0ecc6147de10ddb6641')}</Subtitle>}
       </Settings>
     </div>
   )
