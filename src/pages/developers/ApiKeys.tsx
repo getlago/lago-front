75e4a8d4b4e12452d5d0324a69e080c8d2c23f71
Alexandre Monjol <alexandre.monjol@gmail.com>
2022-11-25 15:39:14 +0100

    refactor: 🗝️ ApiKeys new UI

--- src/pages/developers/ApiKeys.tsx
+++ src/pages/developers/ApiKeys.tsx
@@ -3,8 +3,8 @@ import styled from 'styled-components'
 
 import { useCurrentUserInfosVar, addToast } from '~/core/apolloClient'
 import { useInternationalization } from '~/hooks/core/useInternationalization'
-import { Typography, Button, Avatar, Icon } from '~/components/designSystem'
-import { HEADER_TABLE_HEIGHT, NAV_HEIGHT, theme } from '~/styles'
+import { Typography, Button } from '~/components/designSystem'
+import { NAV_HEIGHT, theme } from '~/styles'
 
 gql`
   fragment ApiKeyOrganization on Organization {
@@ -18,101 +18,98 @@ const ApiKeys = () => {
   const { translate } = useInternationalization()
 
   return (
-    <>
-      <ApiKey>
-        <Typography variant="headline">{translate('text_6227a2e847fcd700e903893f')}</Typography>
-        <Subtitle>{translate('text_6227a2e847fcd700e9038947')}</Subtitle>
-        <SubtitleSecretKey variant="bodyHl" color="disabled">
-          {translate('text_6227a2e847fcd700e903893f')}
-        </SubtitleSecretKey>
-        <CopyBlock>
-          <Avatar variant="connector" size="medium">
-            <Icon color="dark" name="key" />
-          </Avatar>
-          <SecretKey color="textSecondary">{currentOrganization?.apiKey}</SecretKey>
-          <Button
-            variant="quaternary"
-            size="large"
-            startIcon="duplicate"
-            onClick={() => {
-              navigator.clipboard.writeText(currentOrganization?.apiKey || '')
-              addToast({
-                severity: 'info',
-                translateKey: 'text_6227a2e847fcd700e9038952',
-              })
-            }}
-          >
-            {translate('text_6227a2e847fcd700e903894f')}
-          </Button>
-        </CopyBlock>
-      </ApiKey>
-      <OrganizationId>
-        <Typography variant="headline">{translate('text_636df520279a9e1b3c68cc75')}</Typography>
-        <SubtitleSecretKey variant="bodyHl" color="disabled">
+    <Wrapper>
+      <Header>
+        <Typography variant="headline">{translate('text_637f813d31381b1ed90ab2f6')}</Typography>
+        <Typography variant="body" color="grey600">
+          {translate('text_637f813d31381b1ed90ab300')}
+        </Typography>
+      </Header>
+
+      <InlineSectionTitle>
+        <Typography variant="subhead" color="grey700">
+          {translate('text_637f813d31381b1ed90ab313')}
+        </Typography>
+        <Button
+          variant="quaternary"
+          size="large"
+          startIcon="duplicate"
+          onClick={() => {
+            navigator.clipboard.writeText(currentOrganization?.apiKey || '')
+            addToast({
+              severity: 'info',
+              translateKey: 'text_6227a2e847fcd700e9038952',
+            })
+          }}
+        >
+          {translate('text_637f813d31381b1ed90ab30a')}
+        </Button>
+      </InlineSectionTitle>
+
+      <InfoBlock>
+        <Typography variant="body" color="grey700">
+          {currentOrganization?.apiKey}
+        </Typography>
+        <Typography variant="caption" color="grey600">
+          {translate('text_637f813d31381b1ed90ab320')}
+        </Typography>
+      </InfoBlock>
+
+      <InlineSectionTitle>
+        <Typography variant="subhead" color="grey700">
           {translate('text_636df520279a9e1b3c68cc75')}
-        </SubtitleSecretKey>
-        <CopyBlock>
-          <Avatar variant="connector" size="medium">
-            <Icon color="dark" name="key" />
-          </Avatar>
-          <SecretKey color="textSecondary">{currentOrganization?.id}</SecretKey>
-          <Button
-            variant="quaternary"
-            size="large"
-            startIcon="duplicate"
-            onClick={() => {
-              navigator.clipboard.writeText(currentOrganization?.id || '')
-              addToast({
-                severity: 'info',
-                translateKey: 'text_636df520279a9e1b3c68cc7d',
-              })
-            }}
-          >
-            {translate('text_6227a2e847fcd700e903894f')}
-          </Button>
-        </CopyBlock>
-      </OrganizationId>
-    </>
+        </Typography>
+        <Button
+          variant="quaternary"
+          size="large"
+          startIcon="duplicate"
+          onClick={() => {
+            navigator.clipboard.writeText(currentOrganization?.id || '')
+            addToast({
+              severity: 'info',
+              translateKey: 'text_636df520279a9e1b3c68cc7d',
+            })
+          }}
+        >
+          {translate('text_637f813d31381b1ed90ab326')}
+        </Button>
+      </InlineSectionTitle>
+
+      <InfoBlock>
+        <Typography variant="body" color="grey700">
+          {currentOrganization?.id}
+        </Typography>
+        <Typography variant="caption" color="grey600">
+          {translate('text_637f813d31381b1ed90ab332')}
+        </Typography>
+      </InfoBlock>
+    </Wrapper>
   )
 }
 
-const ApiKey = styled.div`
-  padding: ${theme.spacing(12)};
+const Wrapper = styled.div`
+  padding: ${theme.spacing(8)} ${theme.spacing(12)};
+  max-width: ${theme.spacing(168)};
 `
 
-const OrganizationId = styled.div`
-  padding-right: ${theme.spacing(12)};
-  padding-left: ${theme.spacing(12)};
-  padding-bottom: ${theme.spacing(12)};
+const Header = styled.div`
+  margin-bottom: ${theme.spacing(8)};
 `
 
-const Subtitle = styled(Typography)`
-  margin-top: ${theme.spacing(2)};
-`
-
-const CopyBlock = styled.div`
+const InlineSectionTitle = styled.div`
   height: ${NAV_HEIGHT}px;
-  width: 100%;
-  box-shadow: ${theme.shadows[7]};
   display: flex;
   align-items: center;
+  justify-content: space-between;
+`
 
-  > *:first-child {
-    margin-right: ${theme.spacing(3)};
+const InfoBlock = styled.div`
+  padding-bottom: ${theme.spacing(8)};
+  box-shadow: ${theme.shadows[7]};
+
+  > *:not(:last-child) {
+    margin-bottom: ${theme.spacing(1)};
   }
-`
-
-const SubtitleSecretKey = styled(Typography)`
-  height: ${HEADER_TABLE_HEIGHT}px;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  margin-top: ${theme.spacing(8)};
-  box-shadow: ${theme.shadows[7]};
-`
-
-const SecretKey = styled(Typography)`
-  margin-right: auto;
 `
 
 export default ApiKeys
