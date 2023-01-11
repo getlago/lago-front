06d9bc85c2ccfe988d9b1861f9725173b92df3c4
Alexandre Monjol <alexandre.monjol@gmail.com>
2023-01-11 19:15:18 +0100

    feat: add search in Customers list

--- src/pages/CustomersList.tsx
+++ src/pages/CustomersList.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 
 import { Typography, Button, InfiniteScroll } from '~/components/designSystem'
 import { GenericPlaceholder } from '~/components/GenericPlaceholder'
-import { useCustomersQuery, CustomerItemFragmentDoc } from '~/generated/graphql'
+import { CustomerItemFragmentDoc, useCustomersLazyQuery } from '~/generated/graphql'
 import { useInternationalization } from '~/hooks/core/useInternationalization'
 import { theme, PageHeader, ListHeader, ListContainer } from '~/styles'
 import ErrorImage from '~/public/images/maneki/error.svg'
@@ -12,10 +12,12 @@ import EmptyImage from '~/public/images/maneki/empty.svg'
 import { AddCustomerDrawer, AddCustomerDrawerRef } from '~/components/customers/AddCustomerDrawer'
 import { CustomerItemSkeleton, CustomerItem } from '~/components/customers/CustomerItem'
 import { useListKeysNavigation } from '~/hooks/ui/useListKeyNavigation'
+import { SearchInput } from '~/components/SearchInput'
+import { useDebouncedSearch } from '~/hooks/useDebouncedSearch'
 
 gql`
-  query customers($page: Int, $limit: Int) {
-    customers(page: $page, limit: $limit) {
+  query customers($page: Int, $limit: Int, $searchTerm: String) {
+    customers(page: $page, limit: $limit, searchTerm: $searchTerm) {
       metadata {
         currentPage
         totalPages
@@ -35,10 +37,12 @@ const CustomersList = () => {
     getElmId: (i) => `customer-item-${i}`,
   })
   const { translate } = useInternationalization()
-  const { data, error, loading, fetchMore } = useCustomersQuery({
+  const [getCustomers, { data, error, loading, fetchMore, variables }] = useCustomersLazyQuery({
     variables: { limit: 20 },
     notifyOnNetworkStatusChange: true,
   })
+  const { debouncedSearch, isSearchLoading } = useDebouncedSearch(getCustomers, loading)
+  const isLoading = isSearchLoading || loading
   const list = data?.customers?.collection || []
   let index = -1
 
@@ -48,55 +52,87 @@ const CustomersList = () => {
         <Typography variant="bodyHl" color="textSecondary" noWrap>
           {translate('text_624efab67eb2570101d117a5')}
         </Typography>
-        <Button
-          data-test="create-customer"
-          onClick={() => addCustomerDrawerRef.current?.openDrawer()}
-        >
-          {translate('text_624efab67eb2570101d117bc')}
-        </Button>
+        <HeaderRigthBlock>
+          <SearchInput
+            onChange={debouncedSearch}
+            placeholder={translate('text_63befc65efcd9374da45b801')}
+          />
+          <Button
+            data-test="create-customer"
+            onClick={() => addCustomerDrawerRef.current?.openDrawer()}
+          >
+            {translate('text_624efab67eb2570101d117bc')}
+          </Button>
+        </HeaderRigthBlock>
       </Header>
 
-      {!loading && !!error ? (
-        <GenericPlaceholder
-          title={translate('text_624efab67eb2570101d117d0')}
-          subtitle={translate('text_624efab67eb2570101d117d8')}
-          buttonTitle={translate('text_624efab67eb2570101d117e0')}
-          buttonVariant="primary"
-          buttonAction={() => location.reload()}
-          image={<ErrorImage width="136" height="104" />}
-        />
-      ) : !loading && (!list || !list.length) ? (
-        <GenericPlaceholder
-          title={translate('text_624efab67eb2570101d117a9')}
-          subtitle={translate('text_624efab67eb2570101d117af')}
-          buttonTitle={translate('text_624efab67eb2570101d117b9')}
-          buttonVariant="primary"
-          buttonAction={() => addCustomerDrawerRef.current?.openDrawer()}
-          image={<EmptyImage width="136" height="104" />}
-        />
-      ) : (
-        <ListContainer>
-          <ListHead $withActions>
-            <PlanNameSection>
-              <Typography color="disabled" variant="bodyHl">
-                {translate('text_624efab67eb2570101d117cc')}
-              </Typography>
-            </PlanNameSection>
-            <PlanInfosSection>
-              <MediumCell color="disabled" variant="bodyHl">
-                {translate('text_62d95e42c1e1dfe7376fdf35')}
-              </MediumCell>
-              <SmallCell color="disabled" variant="bodyHl">
-                {translate('text_624efab67eb2570101d117e3')}
-              </SmallCell>
-            </PlanInfosSection>
-          </ListHead>
+      <ListContainer>
+        <ListHead $withActions>
+          <PlanNameSection>
+            <Typography color="disabled" variant="bodyHl">
+              {translate('text_624efab67eb2570101d117cc')}
+            </Typography>
+          </PlanNameSection>
+          <PlanInfosSection>
+            <MediumCell color="disabled" variant="bodyHl">
+              {translate('text_62d95e42c1e1dfe7376fdf35')}
+            </MediumCell>
+            <SmallCell color="disabled" variant="bodyHl">
+              {translate('text_624efab67eb2570101d117e3')}
+            </SmallCell>
+          </PlanInfosSection>
+        </ListHead>
+        {!!isLoading && variables?.searchTerm ? (
+          <>
+            {[0, 1, 2].map((i) => (
+              <CustomerItemSkeleton key={`customer-item-skeleton-${i}`} />
+            ))}
+          </>
+        ) : !isLoading && !!error ? (
+          <>
+            {!!variables?.searchTerm ? (
+              <GenericPlaceholder
+                title={translate('text_623b53fea66c76017eaebb6e')}
+                subtitle={translate('text_63bab307a61c62af497e0599')}
+                image={<ErrorImage width="136" height="104" />}
+              />
+            ) : (
+              <GenericPlaceholder
+                title={translate('text_624efab67eb2570101d117d0')}
+                subtitle={translate('text_624efab67eb2570101d117d8')}
+                buttonTitle={translate('text_624efab67eb2570101d117e0')}
+                buttonVariant="primary"
+                buttonAction={() => location.reload()}
+                image={<ErrorImage width="136" height="104" />}
+              />
+            )}
+          </>
+        ) : !isLoading && (!list || !list.length) ? (
+          <>
+            {!!variables?.searchTerm ? (
+              <GenericPlaceholder
+                title={translate('text_63befc65efcd9374da45b813')}
+                subtitle={translate('text_63befc65efcd9374da45b817')}
+                image={<EmptyImage width="136" height="104" />}
+              />
+            ) : (
+              <GenericPlaceholder
+                title={translate('text_624efab67eb2570101d117a9')}
+                subtitle={translate('text_624efab67eb2570101d117af')}
+                buttonTitle={translate('text_624efab67eb2570101d117b9')}
+                buttonVariant="primary"
+                buttonAction={() => addCustomerDrawerRef.current?.openDrawer()}
+                image={<EmptyImage width="136" height="104" />}
+              />
+            )}
+          </>
+        ) : (
           <InfiniteScroll
             onBottom={() => {
               const { currentPage = 0, totalPages = 0 } = data?.customers?.metadata || {}
 
               currentPage < totalPages &&
-                !loading &&
+                !isLoading &&
                 fetchMore({
                   variables: { page: currentPage + 1 },
                 })
@@ -115,12 +151,12 @@ const CustomersList = () => {
                     />
                   )
                 })}
-              {loading &&
+              {isLoading &&
                 [0, 1, 2].map((i) => <CustomerItemSkeleton key={`customer-item-skeleton-${i}`} />)}
             </>
           </InfiniteScroll>
-        </ListContainer>
-      )}
+        )}
+      </ListContainer>
 
       <AddCustomerDrawer ref={addCustomerDrawerRef} />
     </div>
@@ -134,6 +170,15 @@ const Header = styled(PageHeader)`
     &:first-child {
       margin-right: ${theme.spacing(4)};
     }
+  }
+`
+
+const HeaderRigthBlock = styled.div`
+  display: flex;
+  align-items: center;
+
+  > :first-child {
+    margin-right: ${theme.spacing(3)};
   }
 `
 
