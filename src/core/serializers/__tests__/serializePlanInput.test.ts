26a046bb1f6d8591fb4c6aaa6d45cdc128e2ed74
Alexandre Monjol <alexandre.monjol@gmail.com>
2023-01-23 16:51:18 +0100

    chore: add test for serializePlanInput hook

---
+++ src/core/serializers/__tests__/serializePlanInput.test.ts
@@ -0,0 +1,344 @@
+import { ChargeModelEnum, CurrencyEnum, PlanInterval } from '~/generated/graphql'
+
+import { serializePlanInput } from '../serializePlanInput'
+
+const fullProperty = {
+  amount: '1',
+  fixedAmount: '2',
+  freeUnits: 1,
+  freeUnitsPerEvents: 0,
+  freeUnitsPerTotalAggregation: '1',
+  packageSize: 12,
+  rate: '5',
+  graduatedRanges: [
+    {
+      flatAmount: '1',
+      fromValue: 0,
+      perUnitAmount: '1',
+    },
+    {
+      flatAmount: '1',
+      fromValue: 1,
+      perUnitAmount: '1',
+    },
+  ],
+  volumeRanges: [
+    {
+      flatAmount: '1',
+      fromValue: 0,
+      perUnitAmount: '1',
+    },
+    {
+      flatAmount: '1',
+      fromValue: 1,
+      perUnitAmount: '1',
+    },
+  ],
+}
+
+describe('serializePlanInput()', () => {
+  describe('a plan without charges', () => {
+    it('returns plan correctly serialized', () => {
+      const plan = serializePlanInput({
+        amountCents: '1',
+        amountCurrency: CurrencyEnum.Eur,
+        billChargesMonthly: true,
+        charges: [],
+        code: 'my-plan',
+        interval: PlanInterval.Monthly,
+        name: 'My plan',
+        payInAdvance: true,
+        trialPeriod: 1,
+      })
+
+      expect(plan).toStrictEqual({
+        amountCents: 100,
+        amountCurrency: 'EUR',
+        billChargesMonthly: true,
+        charges: [],
+        code: 'my-plan',
+        interval: 'monthly',
+        name: 'My plan',
+        payInAdvance: true,
+        trialPeriod: 1,
+      })
+    })
+  })
+
+  describe('a plan with graduated charge', () => {
+    it('returns plan correctly serialized', () => {
+      const plan = serializePlanInput({
+        amountCents: '1',
+        amountCurrency: CurrencyEnum.Eur,
+        billChargesMonthly: true,
+        charges: [
+          {
+            chargeModel: ChargeModelEnum.Graduated,
+            billableMetric: { id: '1234', name: 'simpleBM', code: 'simple-bm' },
+            properties: fullProperty,
+          },
+        ],
+        code: 'my-plan',
+        interval: PlanInterval.Monthly,
+        name: 'My plan',
+        payInAdvance: true,
+        trialPeriod: 1,
+      })
+
+      expect(plan).toStrictEqual({
+        amountCents: 100,
+        amountCurrency: 'EUR',
+        billChargesMonthly: true,
+        charges: [
+          {
+            billableMetricId: '1234',
+            chargeModel: 'graduated',
+            groupProperties: undefined,
+            properties: {
+              amount: '1',
+              fixedAmount: '2',
+              freeUnits: undefined,
+              freeUnitsPerEvents: 0,
+              freeUnitsPerTotalAggregation: '1',
+              graduatedRanges: [
+                {
+                  flatAmount: '1',
+                  fromValue: 0,
+                  perUnitAmount: '1',
+                },
+                {
+                  flatAmount: '1',
+                  fromValue: 1,
+                  perUnitAmount: '1',
+                },
+              ],
+              packageSize: undefined,
+              rate: '5',
+              volumeRanges: undefined,
+            },
+          },
+        ],
+        code: 'my-plan',
+        interval: 'monthly',
+        name: 'My plan',
+        payInAdvance: true,
+        trialPeriod: 1,
+      })
+    })
+  })
+
+  describe('a plan with package charge', () => {
+    it('returns plan correctly serialized', () => {
+      const plan = serializePlanInput({
+        amountCents: '1',
+        amountCurrency: CurrencyEnum.Eur,
+        billChargesMonthly: true,
+        charges: [
+          {
+            chargeModel: ChargeModelEnum.Package,
+            billableMetric: { id: '1234', name: 'simpleBM', code: 'simple-bm' },
+            properties: fullProperty,
+          },
+        ],
+        code: 'my-plan',
+        interval: PlanInterval.Monthly,
+        name: 'My plan',
+        payInAdvance: true,
+        trialPeriod: 1,
+      })
+
+      expect(plan).toStrictEqual({
+        amountCents: 100,
+        amountCurrency: 'EUR',
+        billChargesMonthly: true,
+        charges: [
+          {
+            billableMetricId: '1234',
+            chargeModel: 'package',
+            groupProperties: undefined,
+            properties: {
+              amount: '1',
+              fixedAmount: '2',
+              freeUnits: 1,
+              freeUnitsPerEvents: 0,
+              freeUnitsPerTotalAggregation: '1',
+              graduatedRanges: undefined,
+              packageSize: 12,
+              rate: '5',
+              volumeRanges: undefined,
+            },
+          },
+        ],
+        code: 'my-plan',
+        interval: 'monthly',
+        name: 'My plan',
+        payInAdvance: true,
+        trialPeriod: 1,
+      })
+    })
+  })
+
+  describe('a plan with percentage charge', () => {
+    it('returns plan correctly serialized', () => {
+      const plan = serializePlanInput({
+        amountCents: '1',
+        amountCurrency: CurrencyEnum.Eur,
+        billChargesMonthly: true,
+        charges: [
+          {
+            chargeModel: ChargeModelEnum.Percentage,
+            billableMetric: { id: '1234', name: 'simpleBM', code: 'simple-bm' },
+            properties: fullProperty,
+          },
+        ],
+        code: 'my-plan',
+        interval: PlanInterval.Monthly,
+        name: 'My plan',
+        payInAdvance: true,
+        trialPeriod: 1,
+      })
+
+      expect(plan).toStrictEqual({
+        amountCents: 100,
+        amountCurrency: 'EUR',
+        billChargesMonthly: true,
+        charges: [
+          {
+            billableMetricId: '1234',
+            chargeModel: 'percentage',
+            groupProperties: undefined,
+            properties: {
+              amount: undefined,
+              fixedAmount: '2',
+              freeUnits: undefined,
+              freeUnitsPerEvents: undefined,
+              freeUnitsPerTotalAggregation: '1',
+              graduatedRanges: undefined,
+              packageSize: undefined,
+              rate: '5',
+              volumeRanges: undefined,
+            },
+          },
+        ],
+        code: 'my-plan',
+        interval: 'monthly',
+        name: 'My plan',
+        payInAdvance: true,
+        trialPeriod: 1,
+      })
+    })
+  })
+
+  describe('a plan with standard charge', () => {
+    it('returns plan correctly serialized', () => {
+      const plan = serializePlanInput({
+        amountCents: '1',
+        amountCurrency: CurrencyEnum.Eur,
+        billChargesMonthly: true,
+        charges: [
+          {
+            chargeModel: ChargeModelEnum.Standard,
+            billableMetric: { id: '1234', name: 'simpleBM', code: 'simple-bm' },
+            properties: fullProperty,
+          },
+        ],
+        code: 'my-plan',
+        interval: PlanInterval.Monthly,
+        name: 'My plan',
+        payInAdvance: true,
+        trialPeriod: 1,
+      })
+
+      expect(plan).toStrictEqual({
+        amountCents: 100,
+        amountCurrency: 'EUR',
+        billChargesMonthly: true,
+        charges: [
+          {
+            billableMetricId: '1234',
+            chargeModel: 'standard',
+            groupProperties: undefined,
+            properties: {
+              amount: '1',
+              fixedAmount: '2',
+              freeUnits: undefined,
+              freeUnitsPerEvents: 0,
+              freeUnitsPerTotalAggregation: '1',
+              graduatedRanges: undefined,
+              packageSize: undefined,
+              rate: '5',
+              volumeRanges: undefined,
+            },
+          },
+        ],
+        code: 'my-plan',
+        interval: 'monthly',
+        name: 'My plan',
+        payInAdvance: true,
+        trialPeriod: 1,
+      })
+    })
+  })
+
+  describe('a plan with volume charge', () => {
+    it('returns plan correctly serialized', () => {
+      const plan = serializePlanInput({
+        amountCents: '1',
+        amountCurrency: CurrencyEnum.Eur,
+        billChargesMonthly: true,
+        charges: [
+          {
+            chargeModel: ChargeModelEnum.Volume,
+            billableMetric: { id: '1234', name: 'simpleBM', code: 'simple-bm' },
+            properties: fullProperty,
+          },
+        ],
+        code: 'my-plan',
+        interval: PlanInterval.Monthly,
+        name: 'My plan',
+        payInAdvance: true,
+        trialPeriod: 1,
+      })
+
+      expect(plan).toStrictEqual({
+        amountCents: 100,
+        amountCurrency: 'EUR',
+        billChargesMonthly: true,
+        charges: [
+          {
+            billableMetricId: '1234',
+            chargeModel: 'volume',
+            groupProperties: undefined,
+            properties: {
+              amount: '1',
+              fixedAmount: '2',
+              freeUnits: undefined,
+              freeUnitsPerEvents: 0,
+              freeUnitsPerTotalAggregation: '1',
+              graduatedRanges: undefined,
+              packageSize: undefined,
+              rate: '5',
+              volumeRanges: [
+                {
+                  flatAmount: '1',
+                  fromValue: 0,
+                  perUnitAmount: '1',
+                },
+                {
+                  flatAmount: '1',
+                  fromValue: 1,
+                  perUnitAmount: '1',
+                },
+              ],
+            },
+          },
+        ],
+        code: 'my-plan',
+        interval: 'monthly',
+        name: 'My plan',
+        payInAdvance: true,
+        trialPeriod: 1,
+      })
+    })
+  })
+})
