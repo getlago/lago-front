d991765a6c7406f699bb2571b76739ab0d62aaea
Alexandre Monjol <alexandre.monjol@gmail.com>
2023-02-28 11:24:46 +0100

    feat(nav): add feature requests link

--- src/externalUrls.ts
+++ src/externalUrls.ts
@@ -2,3 +2,4 @@ export const DOCUMENTATION_URL = 'https://doc.getlago.com/'
 export const DOCUMENTATION_AIRBYTE = 'https://docs.airbyte.com/integrations/sources/getlago/'
 export const DOCUMENTATION_OSO =
   'https://www.osohq.com/docs/guides/model-your-apps-authz#entitlements'
+export const FEATURE_REQUESTS_URL = 'https://getlago.canny.io/feature-requests'
