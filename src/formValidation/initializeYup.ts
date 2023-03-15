9e0a26a167b2cf115fb66d228b37b707ec037299
Alexandre Monjol <alexandre.monjol@gmail.com>
2023-03-15 17:58:37 +0100

    chore(yup): override email validation regex

---
+++ src/formValidation/initializeYup.ts
@@ -0,0 +1,14 @@
+import { addMethod, string } from 'yup'
+
+const EMAIL_REGEX: RegExp =
+  /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g
+
+export const initializeYup = () => {
+  addMethod(string, 'email', function validateEmail(message) {
+    return this.matches(EMAIL_REGEX, {
+      message,
+      name: 'email',
+      excludeEmptyString: true,
+    })
+  })
+}
