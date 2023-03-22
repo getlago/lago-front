e5a010ca750ae964e2cda672ffad02f77b31db0c
MorganeLecurieux <morgane.lecurieux.lafayette@gmail.com>
2023-03-22 13:13:10 -0500

    feat: ➕ Add Segment and Hightouch integration

--- src/externalUrls.ts
+++ src/externalUrls.ts
@@ -1,5 +1,7 @@
 export const DOCUMENTATION_URL = 'https://doc.getlago.com/'
 export const DOCUMENTATION_AIRBYTE = 'https://docs.airbyte.com/integrations/sources/getlago/'
+export const DOCUMENTATION_SEGMENT = 'https://doc.getlago.com/docs/integrations/usage/segment'
+export const DOCUMENTATION_HIGHTTOUCH = 'https://doc.getlago.com/docs/integrations/usage/hightouch'
 export const DOCUMENTATION_OSO =
   'https://www.osohq.com/docs/guides/model-your-apps-authz#entitlements'
 export const FEATURE_REQUESTS_URL = 'https://getlago.canny.io/feature-requests'
