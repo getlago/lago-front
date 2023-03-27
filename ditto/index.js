c42d1db81bee3aea4f3d81e3e1f7280de3f9eb04
Alexandre Monjol <alexandre.monjol@gmail.com>
2023-03-27 15:48:00 +0200

    chore(ditto): fix config

--- ditto/index.js
+++ ditto/index.js
@@ -1,204 +1,201 @@
 module.exports = {
-  "project_6295e54aa233f00167720fe8": {
-    "base": require('./Add-ons - Apply an Add-on to a Customer__base.json')
-  },
-  "project_62972835d3981000e8b0eb9d": {
-    "base": require('./Add-ons - Create, Edit, Delete an Add-on__base.json')
-  },
-  "project_6256de3622fcba00d9830a1e": {
-    "base": require('./All - Edit & delete ≠ objects__base.json')
-  },
-  "project_62bac374775f2a0472421630": {
-    "base": require('./All - Empty and error state in app__base.json')
-  },
-  "project_624d9ad91a1c4d0149998c5b": {
-    "base": require('./All - Give more context in creation flows__base.json')
-  },
-  "project_639710434b2a96dba29b9140": {
-    "base": require('./Amount placeholder__base.json')
-  },
-  "project_62694d8eb0d327016e05c5b5": {
-    "base": require('./Billable metrics - Add Sum, Max, Count unique aggregation type__base.json')
-  },
-  "project_623b42fddc2f2c017440cb5b": {
-    "base": require('./Billable metrics - Create a billable metric__base.json')
-  },
-  "project_63a5ba118e83e2c695c6f7c6": {
-    "base": require('./Copy to clipboard__base.json')
-  },
-  "project_628b8c5f8161d800e7c1c562": {
-    "base": require('./Coupons - Apply coupons to customers__base.json')
-  },
-  "project_62865490ae25a500b104b108": {
-    "base": require('./Coupons - Create, edit coupons__base.json')
-  },
-  "project_626c0c056054aa00f9967401": {
-    "base": require('./Customer - Add invoice data to customer creation and edition__base.json')
-  },
-  "project_6335e508a4f742dfb05f54fd": {
-    "base": require('./Customer - Subscription on past - future date__base.json')
-  },
-  "project_624ef2f26583e200fe08b6fd": {
-    "base": require('./Customers - Customers list and details__base.json')
-  },
-  "project_626162c105cb2c00e673c696": {
-    "base": require('./Customers - Edit & delete a customer__base.json')
-  },
-  "project_623b3ac9459a5d00df324533": {
-    "base": require('./Documentation asset__base.json')
-  },
-  "project_62b1edda611dee64a01ad185": {
-    "base": require('./Lago <> Stripe connection + impact on customer view__base.json')
-  },
-  "project_62c3f3f85ba98dad7106f33b": {
-    "base": require('./Logs - Log current usage__base.json')
-  },
-  "project_628cf7593d43eb010ec6b067": {
-    "base": require('./Logs - Log usage & measurement__base.json')
-  },
-  "project_620bc4d45f16c800e2324b82": {
-    "base": require('./Onboarding - Create & Connect to an account__base.json')
-  },
-  "project_62793bb8e62e1200cbcdddf8": {
-    "base": require('./Plans - Add Graduated in charge model__base.json')
-  },
-  "project_62820857abe2eb0109dfb546": {
-    "base": require('./Plans - Add Package per unit in charge model__base.json')
-  },
-  "project_62a0b709894be901198aeddb": {
-    "base": require('./Plans - Add Percentage in charge model__base.json')
-  },
-  "project_62b32ec020fff7fdc7426158": {
-    "base": require('./Plans - Add weekly interval to Plans__base.json')
-  },
-  "project_62a30bc3940c3583b1a429be": {
-    "base": require('./Plans - Bill Charges monthly on a yearly plan__base.json')
-  },
-  "project_62442e1098bdeb0134fd179b": {
-    "base": require('./Plans - Create a plan__base.json')
-  },
-  "project_62d7f6138ca07e88551c9dde": {
-    "base": require('./Plans,Customers - Add multiple plans to a customer__base.json')
-  },
-  "project_62728ff2d06c9b00f4ea46ad": {
-    "base": require('./Setting - Set tax rates in application__base.json')
-  },
-  "project_6227a2e6da6f5200e76ec9c2": {
-    "base": require('./Settings - API KEY__base.json')
-  },
-  "project_62ab2cff6d409cff712298ea": {
-    "base": require('./Settings - Invoice settings__base.json')
-  },
-  "project_6271200612648800e9bdfd47": {
-    "base": require('./Settings - Webhooks in app__base.json')
-  },
-  "ditto_component_library": {
-    "base": require('./ditto-component-library__base.json')
-  },
-  "project_63cfe207f9f4c2ea8dcf3aa7": {
-    "base": require('./⚙️ [WIP] - All - Delete objects linked to subscription__base.json')
-  },
-  "project_63ba9ee502cfb90849074f4b": {
-    "base": require('./⚙️ [WIP] - All - Search bar in app__base.json')
-  },
-  "project_633b622919283cdbfb2f7233": {
-    "base": require('./⚙️ [WIP] - B.Metrics and Plans - Dimension__base.json')
-  },
-  "project_63c83a31e697e8e9236da751": {
-    "base": require('./⚙️ [WIP] - Coupons - Set frequency forever and limited to a specific plan__base.json')
-  },
-  "project_62d175018d4659c9a3eec929": {
-    "base": require('./⚙️ [WIP] - Credits - Create prepaid credits__base.json')
-  },
-  "project_6419c64c72d2468f025ea25d": {
-    "base": require('./⚙️ [WIP] - Customer - Customer portal__base.json'),
-    "fr": require('./⚙️ [WIP] - Customer - Customer portal__fr.json'),
-    "nb": require('./⚙️ [WIP] - Customer - Customer portal__nb.json')
-  },
-  "project_632b49dccdbed9536c8497ab": {
-    "base": require('./⚙️ [WIP] - Customers - Customer hold currency__base.json')
-  },
-  "project_63fcc31ebb3fb91189619496": {
-    "base": require('./⚙️ [WIP] - Customers - Metadata on customer & invoice__base.json')
-  },
-  "project_6328e70891e49d4d9732180d": {
-    "base": require('./⚙️ [WIP] - Customers - Overwrite subscription per customer__base.json')
-  },
-  "project_62ea7cd19883fb6718fa823c": {
-    "base": require('./⚙️ [WIP] - Customers - Subscription on anniversary date__base.json')
-  },
-  "project_62f50d2512182c6df6c39e37": {
-    "base": require('./⚙️ [WIP] - General - FE environment infos__base.json')
-  },
-  "project_63e254ea592347d77dd3cf03": {
-    "base": require('./⚙️ [WIP] - Plan - Warn user if duplicate charge in plan creation and edition__base.json')
-  },
-  "project_63e51e8467659a55d188a68c": {
-    "base": require('./⚙️ [WIP] - Settings - Define preferred doc language generation__base.json')
-  },
-  "project_640766f4ff20c7dac058261b": {
-    "base": require('./⚙️ [WIP] - Settings - Email invoices to final customers__base.json'),
-    "fr": require('./⚙️ [WIP] - Settings - Email invoices to final customers__fr.json'),
-    "nb": require('./⚙️ [WIP] - Settings - Email invoices to final customers__nb.json')
-  },
-  "project_63e27c4800ff170963670e71": {
-    "base": require('./⚙️ [WIP] - Settings - Webhook debugger__base.json')
-  },
   "project_62ff5cfec490a4767eccdf6e": {
-    "base": require('./👍 [Ready for dev] - Add Percentage in charge model__base.json')
+    "base": require('./-ready-for-dev---add-percentage-in-charge-model__base.json')
   },
   "project_638906e129a144b0c4c27c83": {
-    "base": require('./👍 [Ready for dev] - All - Custom ≠ timezones__base.json')
+    "base": require('./-ready-for-dev---all---custom--timezones__base.json')
   },
   "project_63b315558a78f7b5a6551de7": {
-    "base": require('./👍 [Ready for dev] - All - License key__base.json')
+    "base": require('./-ready-for-dev---all---license-key__base.json')
   },
   "project_633336529ca3243d15ac9df4": {
-    "base": require('./👍 [Ready for dev] - All - Replace success screen by success toast__base.json')
+    "base": require('./-ready-for-dev---all---replace-success-screen-by-success-toast__base.json')
   },
   "project_63105dba2074ec779d08c6b4": {
-    "base": require('./👍 [Ready for dev] - B.metrics - Persistent bm in aggregation type__base.json')
+    "base": require('./-ready-for-dev---b.metrics---persistent-bm-in-aggregation-type__base.json')
   },
   "project_637b4d9f764dcb190431ad4d": {
-    "base": require('./👍 [Ready for dev] - Coupons - Apply several coupons to customer__base.json')
+    "base": require('./-ready-for-dev---coupons---apply-several-coupons-to-customer__base.json')
   },
   "project_632d6831010fafe9382b616d": {
-    "base": require('./👍 [Ready for dev] - Coupons - Create % and recurring coupons__base.json')
+    "base": require('./-ready-for-dev---coupons---create--and-recurring-coupons__base.json')
   },
   "project_638f4825992c44e4381e7a1f": {
-    "base": require('./👍 [Ready for dev] - Coupons - Option to apply several times the same coupon__base.json')
+    "base": require('./-ready-for-dev---coupons---option-to-apply-several-times-the-same-coupon__base.json')
   },
   "project_636bdeec2ea28ae182faa50e": {
-    "base": require('./👍 [Ready for dev] - Credit note - Create and apply credit note to a customer__base.json')
+    "base": require('./-ready-for-dev---credit-note---create-and-apply-credit-note-to-a-customer__base.json')
   },
   "project_63aa085ae8d6be2e0e0ee38b": {
-    "base": require('./👍 [Ready for dev] - Customer - Improve tax and grace period on customer__base.json')
+    "base": require('./-ready-for-dev---customer---improve-tax-and-grace-period-on-customer__base.json')
   },
   "project_63eba8199c936977d8e98991": {
-    "base": require('./👍 [Ready for dev] - Customer Invoice - Update invoice.payment_status__base.json')
+    "base": require('./-ready-for-dev---customer-invoice---update-invoice.payment_status__base.json')
   },
   "project_634687058efb4a10996fdbdc": {
-    "base": require('./👍 [Ready for dev] - Customers - Invoice detail page__base.json')
+    "base": require('./-ready-for-dev---customers---invoice-detail-page__base.json')
   },
   "project_637f813a9ec65c29fc3ceef3": {
-    "base": require('./👍 [Ready for dev] - Customers - Invoice grace period__base.json')
+    "base": require('./-ready-for-dev---customers---invoice-grace-period__base.json')
   },
   "project_63ac86d51bd1437a2a21f1f9": {
-    "base": require('./👍 [Ready for dev] - Invoices- Invoice list__base.json')
+    "base": require('./-ready-for-dev---invoices--invoice-list__base.json')
   },
   "project_63208b60a9b2a4c6186bbd26": {
-    "base": require('./👍 [Ready for dev] - Onboarding - Invite member to organisation__base.json')
+    "base": require('./-ready-for-dev---onboarding---invite-member-to-organisation__base.json')
   },
   "project_6304e74718860633c15905f2": {
-    "base": require('./👍 [Ready for dev] - Plans - Add Volume in charge model__base.json')
+    "base": require('./-ready-for-dev---plans---add-volume-in-charge-model__base.json')
   },
   "project_634ea0e94c99df2bb59820d9": {
-    "base": require('./👍 [Ready for dev] - Settings - Customers - Lago gocardless connection__base.json')
+    "base": require('./-ready-for-dev---settings---customers---lago-gocardless-connection__base.json')
   },
   "project_639c334bcee914d26afecf69": {
-    "base": require('./👍 [Ready for dev] - Settings Customers - Lago x Data Warehouse connection__base.json')
+    "base": require('./-ready-for-dev---settings-customers---lago-x-data-warehouse-connection__base.json')
   },
   "project_63e26d71cc5ddde09d90bd99": {
-    "base": require('./👍 [Ready for dev] - Settings Customers - Lago x Osso connection__base.json')
+    "base": require('./-ready-for-dev---settings-customers---lago-x-osso-connection__base.json')
+  },
+  "project_63cfe207f9f4c2ea8dcf3aa7": {
+    "base": require('./-wip---all---delete-objects-linked-to-subscription__base.json')
+  },
+  "project_63ba9ee502cfb90849074f4b": {
+    "base": require('./-wip---all---search-bar-in-app__base.json')
+  },
+  "project_633b622919283cdbfb2f7233": {
+    "base": require('./-wip---b.metrics-and-plans---dimension__base.json')
+  },
+  "project_63c83a31e697e8e9236da751": {
+    "base": require('./-wip---coupons---set-frequency-forever-and-limited-to-a-specific-plan__base.json')
+  },
+  "project_62d175018d4659c9a3eec929": {
+    "base": require('./-wip---credits---create-prepaid-credits__base.json')
+  },
+  "project_6419c64c72d2468f025ea25d": {
+    "base": require('./-wip---customer---customer-portal__base.json'),
+    "fr": require('./-wip---customer---customer-portal__fr.json'),
+    "nb": require('./-wip---customer---customer-portal__nb.json')
+  },
+  "project_632b49dccdbed9536c8497ab": {
+    "base": require('./-wip---customers---customer-hold-currency__base.json')
+  },
+  "project_63fcc31ebb3fb91189619496": {
+    "base": require('./-wip---customers---metadata-on-customer--invoice__base.json')
+  },
+  "project_6328e70891e49d4d9732180d": {
+    "base": require('./-wip---customers---overwrite-subscription-per-customer__base.json')
+  },
+  "project_62ea7cd19883fb6718fa823c": {
+    "base": require('./-wip---customers---subscription-on-anniversary-date__base.json')
+  },
+  "project_62f50d2512182c6df6c39e37": {
+    "base": require('./-wip---general---fe-environment-infos__base.json')
+  },
+  "project_63e254ea592347d77dd3cf03": {
+    "base": require('./-wip---plan---warn-user-if-duplicate-charge-in-plan-creation-and-edition__base.json')
+  },
+  "project_63e51e8467659a55d188a68c": {
+    "base": require('./-wip---settings---define-preferred-doc-language-generation__base.json')
+  },
+  "project_640766f4ff20c7dac058261b": {
+    "base": require('./-wip---settings---email-invoices-to-final-customers__base.json'),
+    "fr": require('./-wip---settings---email-invoices-to-final-customers__fr.json'),
+    "nb": require('./-wip---settings---email-invoices-to-final-customers__nb.json')
+  },
+  "project_63e27c4800ff170963670e71": {
+    "base": require('./-wip---settings---webhook-debugger__base.json')
+  },
+  "project_6295e54aa233f00167720fe8": {
+    "base": require('./add-ons---apply-an-add-on-to-a-customer__base.json')
+  },
+  "project_62972835d3981000e8b0eb9d": {
+    "base": require('./add-ons---create-edit-delete-an-add-on__base.json')
+  },
+  "project_6256de3622fcba00d9830a1e": {
+    "base": require('./all---edit--delete--objects__base.json')
+  },
+  "project_62bac374775f2a0472421630": {
+    "base": require('./all---empty-and-error-state-in-app__base.json')
+  },
+  "project_624d9ad91a1c4d0149998c5b": {
+    "base": require('./all---give-more-context-in-creation-flows__base.json')
+  },
+  "project_639710434b2a96dba29b9140": {
+    "base": require('./amount-placeholder__base.json')
+  },
+  "project_62694d8eb0d327016e05c5b5": {
+    "base": require('./billable-metrics---add-sum-max-count-unique-aggregation-type__base.json')
+  },
+  "project_623b42fddc2f2c017440cb5b": {
+    "base": require('./billable-metrics---create-a-billable-metric__base.json')
+  },
+  "project_63a5ba118e83e2c695c6f7c6": {
+    "base": require('./copy-to-clipboard__base.json')
+  },
+  "project_628b8c5f8161d800e7c1c562": {
+    "base": require('./coupons---apply-coupons-to-customers__base.json')
+  },
+  "project_62865490ae25a500b104b108": {
+    "base": require('./coupons---create-edit-coupons__base.json')
+  },
+  "project_626c0c056054aa00f9967401": {
+    "base": require('./customer---add-invoice-data-to-customer-creation-and-edition__base.json')
+  },
+  "project_6335e508a4f742dfb05f54fd": {
+    "base": require('./customer---subscription-on-past---future-date__base.json')
+  },
+  "project_624ef2f26583e200fe08b6fd": {
+    "base": require('./customers---customers-list-and-details__base.json')
+  },
+  "project_626162c105cb2c00e673c696": {
+    "base": require('./customers---edit--delete-a-customer__base.json')
+  },
+  "project_623b3ac9459a5d00df324533": {
+    "base": require('./documentation-asset__base.json')
+  },
+  "project_62b1edda611dee64a01ad185": {
+    "base": require('./lago--stripe-connection--impact-on-customer-view__base.json')
+  },
+  "project_62c3f3f85ba98dad7106f33b": {
+    "base": require('./logs---log-current-usage__base.json')
+  },
+  "project_628cf7593d43eb010ec6b067": {
+    "base": require('./logs---log-usage--measurement__base.json')
+  },
+  "project_620bc4d45f16c800e2324b82": {
+    "base": require('./onboarding---create--connect-to-an-account__base.json')
+  },
+  "project_62793bb8e62e1200cbcdddf8": {
+    "base": require('./plans---add-graduated-in-charge-model__base.json')
+  },
+  "project_62820857abe2eb0109dfb546": {
+    "base": require('./plans---add-package-per-unit-in-charge-model__base.json')
+  },
+  "project_62a0b709894be901198aeddb": {
+    "base": require('./plans---add-percentage-in-charge-model__base.json')
+  },
+  "project_62b32ec020fff7fdc7426158": {
+    "base": require('./plans---add-weekly-interval-to-plans__base.json')
+  },
+  "project_62a30bc3940c3583b1a429be": {
+    "base": require('./plans---bill-charges-monthly-on-a-yearly-plan__base.json')
+  },
+  "project_62442e1098bdeb0134fd179b": {
+    "base": require('./plans---create-a-plan__base.json')
+  },
+  "project_62d7f6138ca07e88551c9dde": {
+    "base": require('./planscustomers---add-multiple-plans-to-a-customer__base.json')
+  },
+  "project_62728ff2d06c9b00f4ea46ad": {
+    "base": require('./setting---set-tax-rates-in-application__base.json')
+  },
+  "project_6227a2e6da6f5200e76ec9c2": {
+    "base": require('./settings---api-key__base.json')
+  },
+  "project_62ab2cff6d409cff712298ea": {
+    "base": require('./settings---invoice-settings__base.json')
+  },
+  "project_6271200612648800e9bdfd47": {
+    "base": require('./settings---webhooks-in-app__base.json')
   }
 }
