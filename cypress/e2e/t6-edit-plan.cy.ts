6fa670f853f4731bd64bf31f69a4449a7856f0ea
Alexandre Monjol <alexandre.monjol@gmail.com>
2023-07-04 11:31:49 -0300

    feat(invoice) open combobox when adding an addon

--- cypress/e2e/t6-edit-plan.cy.ts
+++ cypress/e2e/t6-edit-plan.cy.ts
@@ -59,7 +59,6 @@ describe('Edit plan', () => {
     cy.get('[data-test="open-charge"]').first().click()
 
     cy.get('[data-test="add-charge"]').last().click()
-    cy.get('input[name="searchChargeInput"]').click()
     cy.get('[data-option-index="1"]').click()
     cy.get('[data-test="submit"]').should('be.disabled')
     cy.get('[data-test="remove-charge"]').should('exist').and('not.be.disabled')
