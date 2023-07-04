f7b1585cf592642063a4d54547992b3f651c4b0d
Alexandre Monjol <alexandre.monjol@gmail.com>
2023-07-04 11:22:15 -0300

    feat(plan) open combobox when adding a charge

--- src/core/constants/form.ts
+++ src/core/constants/form.ts
@@ -3,3 +3,7 @@ export enum FORM_ERRORS_ENUM {
   existingCode = 'existingCode',
   invalidGroupValue = 'invalidGroupValue',
 }
+
+// Selectors
+export const SEARCH_CHARGE_INPUT_NAME = 'searchChargeInput'
+export const MUI_INPUT_BASE_ROOT_CLASSNAME = 'MuiInputBase-root'
