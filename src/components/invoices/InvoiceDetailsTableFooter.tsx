c975de33e9db445ccfc51f90e5449bafecdbe54f
Alexandre Monjol <alexandre.monjol@gmail.com>
2023-08-02 10:33:16 -0500

    bug(invoice) remove feesAmountCents in invoice total

--- src/components/invoices/InvoiceDetailsTableFooter.tsx
+++ src/components/invoices/InvoiceDetailsTableFooter.tsx
@@ -22,7 +22,6 @@ gql`
     appliedTaxes {
       id
       amountCents
-      feesAmountCents
       taxRate
       taxName
     }
@@ -113,19 +112,12 @@ export const InvoiceDetailsTableFooter = memo(
                         <td></td>
                         <td>
                           <Typography variant="bodyHl" color="grey600">
-                            {translate('text_64c013a424ce2f00dffb7f4d', {
+                            {translate('text_64ca74b796e2f800bfdb9e62', {
                               name: appliedTax.taxName,
                               rate: intlFormatNumber(appliedTax.taxRate / 100 || 0, {
                                 maximumFractionDigits: 2,
                                 style: 'percent',
                               }),
-                              amount: intlFormatNumber(
-                                deserializeAmount(appliedTax.feesAmountCents || 0, currency),
-                                {
-                                  currencyDisplay: 'symbol',
-                                  currency,
-                                }
-                              ),
                             })}
                           </Typography>
                         </td>
