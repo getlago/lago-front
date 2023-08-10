c7a75ad4104be8df48592feee22ca197472842fb
Alexandre Monjol <alexandre.monjol@gmail.com>
2023-08-10 15:54:50 -0500

    chore(tests) change e2e tests order and config

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -6,5 +6,21 @@ module.exports = defineConfig({
   projectId: 'u863yi',
   e2e: {
     baseUrl: process.env.CYPRESS_APP_URL,
+    specPattern: [
+      // Auth
+      'auth/signup.cy.ts',
+      'auth/login.cy.ts',
+      // Resources
+      'create-bm.cy.ts',
+      'create-plan.cy.ts',
+      'create-customer.cy.ts',
+      'add-subscription.cy.ts',
+      'coupons-create-edit-apply.cy.ts',
+      'addon-create-edit.cy.ts',
+      // Other
+      'invitations.cy.ts',
+      'edit-plan.cy.ts',
+      '*.ts',
+    ],
   },
 })
