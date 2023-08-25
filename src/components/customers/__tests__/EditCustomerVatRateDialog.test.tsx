fc4d47fa6852343ab0c1a63f3cc8eec86142b7f5
Alexandre Monjol <alexandre.monjol@gmail.com>
2023-08-25 16:58:45 -0300

    chore(test) add test for EditCustomerVatRateDialog

---
+++ src/components/customers/__tests__/EditCustomerVatRateDialog.test.tsx
@@ -0,0 +1,75 @@
+import { act, cleanup, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import { TestMocksType, render } from '~/test-utils'
+import { GetTaxRatesForEditCustomerDocument } from '~/generated/graphql'
+import { CREATE_TAX_ROUTE } from '~/core/router'
+import {
+  MUI_INPUT_BASE_ROOT_CLASSNAME,
+  SEARCH_TAX_INPUT_FOR_CUSTOMER_CLASSNAME,
+} from '~/core/constants/form'
+
+import { EditCustomerVatRateDialog } from '../EditCustomerVatRateDialog'
+
+async function prepare({
+  mocks = [
+    {
+      request: {
+        query: GetTaxRatesForEditCustomerDocument,
+        variables: { limit: 20 },
+      },
+      result: {
+        data: {
+          taxes: {
+            metadata: {
+              currentPage: 1,
+              totalPages: 1,
+            },
+            collection: [],
+          },
+        },
+      },
+    },
+  ],
+}: { mocks?: TestMocksType } = {}) {
+  const customer = {
+    id: '1234',
+    name: 'Customer Name',
+    externalId: '4567',
+  }
+
+  await act(() =>
+    render(<EditCustomerVatRateDialog forceOpen customer={customer} />, {
+      mocks,
+    })
+  )
+}
+
+describe('EditCustomerVatRateDialog', () => {
+  afterEach(cleanup)
+
+  it('renders', async () => {
+    await prepare()
+
+    expect(screen.queryByTestId('edit-customer-vat-rate-dialog')).toBeInTheDocument()
+  })
+
+  it('should propose to create a new tax if none exists', async () => {
+    await prepare()
+
+    await waitFor(() =>
+      userEvent.click(
+        screen
+          .queryByTestId('edit-customer-vat-rate-dialog')
+          ?.querySelector(
+            `.${SEARCH_TAX_INPUT_FOR_CUSTOMER_CLASSNAME} .${MUI_INPUT_BASE_ROOT_CLASSNAME}`
+          ) as HTMLElement
+      )
+    )
+
+    expect(screen.queryByTestId('combobox-item-Create a tax_rate')).toBeInTheDocument()
+    expect(
+      screen.queryByTestId('combobox-item-Create a tax_rate')?.querySelector(`a`)
+    ).toHaveAttribute('href', CREATE_TAX_ROUTE)
+  })
+})
--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -1,8 +1,8 @@
 import React, { ReactElement, useEffect } from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import { configure, render, RenderOptions } from '@testing-library/react'
-import { queryHelpers } from '@testing-library/dom'
 import { ThemeProvider } from '@mui/material'
-import { MockedProvider } from '@apollo/client/testing'
+import { MockedProvider, MockedResponse } from '@apollo/client/testing'
 
 import { initializeTranslations } from './core/apolloClient'
 import { theme } from './styles'
@@ -10,27 +10,41 @@ import { initializeYup } from './formValidation/initializeYup'
 
 configure({ testIdAttribute: 'data-test' })
 
+export type TestMocksType = MockedResponse<Record<string, unknown>, Record<string, unknown>>[]
+
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
   useNavigate: () => jest.fn(),
 }))
 
-const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
+const AllTheProviders = ({
+  children,
+  mocks,
+}: {
+  children: React.ReactNode
+  mocks?: TestMocksType
+}) => {
   useEffect(() => {
     initializeTranslations()
     initializeYup()
   }, [])
 
   return (
-    <MockedProvider addTypename={false}>
-      <ThemeProvider theme={theme}>{children}</ThemeProvider>
-    </MockedProvider>
+    <BrowserRouter basename="/">
+      <MockedProvider addTypename={false} mocks={mocks}>
+        <ThemeProvider theme={theme}>{children}</ThemeProvider>
+      </MockedProvider>
+    </BrowserRouter>
   )
 }
 
-const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) =>
-  render(ui, { wrapper: AllTheProviders, ...options })
-
-export const queryByDataTest = queryHelpers.queryByAttribute.bind(null, 'data-test')
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'> & { mocks?: TestMocksType }
+) =>
+  render(ui, {
+    wrapper: (props) => <AllTheProviders {...props} mocks={options?.mocks} />,
+    ...options,
+  })
 
 export { customRender as render }
