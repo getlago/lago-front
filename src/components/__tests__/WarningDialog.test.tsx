18a8474bce80b5168baa9c0db2e0930a386ad8ca
Alexandre Monjol <alexandre.monjol@gmail.com>
2023-08-28 15:42:08 -0300

    chore: Add test for WarningDialog component

---
+++ src/components/__tests__/WarningDialog.test.tsx
@@ -0,0 +1,45 @@
+import { act, cleanup, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import { render } from '~/test-utils'
+
+import { WarningDialog } from '../WarningDialog'
+
+const onContinueMock = jest.fn()
+
+async function prepare() {
+  await act(() =>
+    render(
+      <WarningDialog
+        forceOpen
+        title="title"
+        description="description"
+        continueText="continueText"
+        onContinue={onContinueMock}
+      />
+    )
+  )
+}
+
+describe('WarningDialog', () => {
+  afterEach(cleanup)
+
+  it('renders', async () => {
+    await prepare()
+
+    expect(screen.queryByTestId('warning-dialog')).toBeInTheDocument()
+    expect(screen.getByText('title')).toBeInTheDocument()
+    expect(screen.getByText('description')).toBeInTheDocument()
+    expect(screen.getByText('continueText')).toBeInTheDocument()
+  })
+
+  it('should trigger the confirm action on click', async () => {
+    await prepare()
+
+    expect(onContinueMock).not.toHaveBeenCalled()
+
+    await waitFor(() => userEvent.click(screen.queryByTestId('warning-confirm') as HTMLElement))
+
+    expect(onContinueMock).toHaveBeenCalled()
+  })
+})
