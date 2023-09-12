a55e4694e2a8c33e293a71b05669a7369f759431
Alexandre Monjol <alexandre.monjol@gmail.com>
2023-09-12 16:49:13 +0200

    chore(sentry) update deprecated BrowserTracing usage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import { createRoot } from 'react-dom/client'
+import { BrowserTracing } from '@sentry/browser'
 import * as Sentry from '@sentry/react'
-import { BrowserTracing } from '@sentry/tracing'
+import { createRoot } from 'react-dom/client'
 
 import App from '~/App'
 import { envGlobalVar } from '~/core/apolloClient'
