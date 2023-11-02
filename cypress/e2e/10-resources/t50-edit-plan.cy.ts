0e9377b5283c57b20e27f38da72db5df3adef45c
Alexandre Monjol <alexandre.monjol@gmail.com>
2023-11-02 14:54:00 +0100

    bug(plan) fix redirection when closing a plan duplication form

--- cypress/e2e/10-resources/t50-edit-plan.cy.ts
+++ cypress/e2e/10-resources/t50-edit-plan.cy.ts
@@ -46,9 +46,8 @@ describe('Edit plan', () => {
     cy.get(`[data-test="add-subscription"]`).click({ force: true })
     cy.get('[data-test="submit"]').should('be.disabled')
     cy.get('input[name="planId"]').click({ force: true })
-    cy.get('[data-option-index="0"]').click({ force: true })
+    cy.get('[data-option-index="0"]').click()
     cy.get('[data-test="submit"]').click({ force: true })
-    cy.get('[data-test="submit"]').should('not.exist')
     cy.contains(customerName).should('exist')
   })
 
