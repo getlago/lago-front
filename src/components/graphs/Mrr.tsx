04cd263baedcb7b03237a2ea573d58e9b8517708
Alexandre Monjol <alexandre.monjol@gmail.com>
2023-11-20 11:16:42 +0100

    bug(mrr) data displayed should be the lext month's mrr values

--- src/components/graphs/Mrr.tsx
+++ src/components/graphs/Mrr.tsx
@@ -57,7 +57,7 @@ export function getAllDataForMrrDisplay({
     formatedData.splice(0, 10)
   }
 
-  const sum = formatedData.reduce((acc, curr) => acc + Number(curr.value), 0)
+  const lastMrr = formatedData[formatedData.length - 2].value || 0
   const [dateFrom, dateTo] = [
     formatedData[0].axisName,
     formatedData[formatedData.length - 1].axisName,
@@ -65,7 +65,7 @@ export function getAllDataForMrrDisplay({
 
   return {
     dataForAreaChart: formatedData,
-    amountSum: sum,
+    lastMonthMrr: lastMrr,
     dateFrom,
     dateTo,
   }
@@ -84,7 +84,7 @@ const Mrr = ({
     skip: demoMode || blur || !currency,
   })
 
-  const { dataForAreaChart, amountSum, dateFrom, dateTo } = useMemo(() => {
+  const { dataForAreaChart, lastMonthMrr, dateFrom, dateTo } = useMemo(() => {
     return getAllDataForMrrDisplay({
       data: data?.mrrs?.collection,
       currency,
@@ -107,7 +107,7 @@ const Mrr = ({
           <ChartHeader
             name={translate('text_6553885df387fd0097fd738c')}
             tooltipText={translate('text_655b21068fc7f80067fd6315')}
-            amount={intlFormatNumber(deserializeAmount(amountSum, currency), {
+            amount={intlFormatNumber(deserializeAmount(lastMonthMrr, currency), {
               currency: currency,
             })}
             period={translate('text_633dae57ca9a923dd53c2097', {
@@ -121,7 +121,7 @@ const Mrr = ({
             loading={loading}
             blur={blur}
             data={dataForAreaChart}
-            hasOnlyZeroValues={amountSum === 0}
+            hasOnlyZeroValues={lastMonthMrr === 0}
             currency={currency}
           />
         </>
