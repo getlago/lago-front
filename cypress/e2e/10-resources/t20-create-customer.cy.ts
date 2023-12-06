1163a967d95752a6804e34e409d3e1399e0242ec
Alexandre Monjol <alexandre.monjol@gmail.com>
2023-12-06 10:54:21 +0100

    chore: uncomment tests

--- cypress/e2e/10-resources/t20-create-customer.cy.ts
+++ cypress/e2e/10-resources/t20-create-customer.cy.ts
@@ -14,24 +14,24 @@ describe('Create customer', () => {
     cy.url().should('include', '/customer/')
     cy.contains(customerName).should('exist')
   })
-  // TODO: uncomment
-  // describe('anti-regression', () => {
-  //   // https://github.com/getlago/lago-front/pull/892
-  //   it('should be able to edit VAT right after creating a customer', () => {
-  //     const randomNumber = Math.round(Math.random() * 1000)
-  //     const randomId = `Customer ${randomNumber}`
 
-  //     cy.get('[data-test="create-customer"]').click()
-  //     cy.get('input[name="name"]').type(randomId)
-  //     cy.get('[data-test="submit"]').should('be.disabled')
-  //     cy.get('input[name="externalId"]').type(randomId)
-  //     cy.get('[data-test="submit"]').click()
-  //     cy.url().should('include', '/customer/')
-  //     cy.contains(randomId).should('exist')
+  describe('anti-regression', () => {
+    // https://github.com/getlago/lago-front/pull/892
+    it('should be able to edit VAT right after creating a customer', () => {
+      const randomNumber = Math.round(Math.random() * 1000)
+      const randomId = `Customer ${randomNumber}`
 
-  //     cy.get('[data-test="tab-internal-button-link-settings"]').last().click()
-  //     cy.get('[data-test="add-vat-rate-button"]').last().click()
-  //     cy.get('[data-test="edit-customer-vat-rate-dialog"]').should('exist')
-  //   })
-  // })
+      cy.get('[data-test="create-customer"]').click()
+      cy.get('input[name="name"]').type(randomId)
+      cy.get('[data-test="submit"]').should('be.disabled')
+      cy.get('input[name="externalId"]').type(randomId)
+      cy.get('[data-test="submit"]').click()
+      cy.url().should('include', '/customer/')
+      cy.contains(randomId).should('exist')
+
+      cy.get('[data-test="tab-internal-button-link-settings"]').last().click()
+      cy.get('[data-test="add-vat-rate-button"]').last().click()
+      cy.get('[data-test="edit-customer-vat-rate-dialog"]').should('exist')
+    })
+  })
 })
