65cfcafea5887401b10a281d8c02ffd3694ed1ae
Alexandre Monjol <alexandre.monjol@gmail.com>
2023-12-15 12:02:32 +0100

    misc(nav): remove beta badge on analytics item

--- src/layouts/SideNavLayout.tsx
+++ src/layouts/SideNavLayout.tsx
@@ -220,7 +220,6 @@ const SideNav = () => {
                     icon: 'chart-bar',
                     link: ANALYTIC_ROUTE,
                     match: [ANALYTIC_ROUTE, HOME_ROUTE],
-                    beta: true,
                   },
                   {
                     title: translate('text_623b497ad05b960101be3448'),
