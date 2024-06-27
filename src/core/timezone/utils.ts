f1f644d25f5d6af75751a2b9a387f03f502faca7
Alexandre Monjol <alexandre.monjol@gmail.com>
2024-06-27 09:20:36 +0200

    Handle missing TZ (#1568)
    * misc: securely access timezone config
    
    * misc: add new TZ config

--- src/core/timezone/utils.ts
+++ src/core/timezone/utils.ts
@@ -1,11 +1,31 @@
+import * as Sentry from '@sentry/browser'
 import { DateTime } from 'luxon'
 
 import { TimezoneEnum } from '~/generated/graphql'
 
 import { TimeZonesConfig } from './config'
 
+import { envGlobalVar } from '../apolloClient'
+
+const { sentryDsn } = envGlobalVar()
+
 export const getTimezoneConfig = (timezone: TimezoneEnum | null | undefined) => {
-  return TimeZonesConfig[timezone || TimezoneEnum.TzUtc]
+  if (!timezone) return TimeZonesConfig[TimezoneEnum.TzUtc]
+
+  const doesTimezoneConfigExist = Object.keys(TimeZonesConfig).includes(timezone)
+
+  if (!doesTimezoneConfigExist) {
+    // If given timezone is not present in config, we should default to UTC config.
+    // However, it's pretty critical as UI and date calculation will be wrong.
+    // Calling sentry to make sure we notice and add missing timezone to the TimeZonesConfig enum then.
+    if (!!sentryDsn) {
+      Sentry.captureMessage(`Timezone ${timezone} is missing in TimeZonesConfig`)
+    }
+
+    return TimeZonesConfig[TimezoneEnum.TzUtc]
+  }
+
+  return TimeZonesConfig[timezone as TimezoneEnum]
 }
 
 export const formatDateToTZ = (
