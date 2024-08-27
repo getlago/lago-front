0fd2a0e00e065c6336062f635179def29d066d8d
Alexandre Monjol <alexandre.monjol@gmail.com>
2024-08-27 15:42:44 -0300

    misc: arrange finalize invoice error toast message

--- src/components/invoices/FinalizeInvoiceDialog.tsx
+++ src/components/invoices/FinalizeInvoiceDialog.tsx
@@ -2,11 +2,12 @@ import { gql } from '@apollo/client'
 import { forwardRef, useImperativeHandle, useRef, useState } from 'react'
 
 import { Button, Dialog, DialogRef } from '~/components/designSystem'
-import { addToast } from '~/core/apolloClient'
+import { addToast, LagoGQLError } from '~/core/apolloClient'
 import { formatDateToTZ } from '~/core/timezone'
 import {
   AllInvoiceDetailsForCustomerInvoiceDetailsFragmentDoc,
   InvoiceForFinalizeInvoiceFragment,
+  LagoApiError,
   useFinalizeInvoiceMutation,
 } from '~/generated/graphql'
 import { useInternationalization } from '~/hooks/core/useInternationalization'
@@ -42,6 +43,9 @@ export const FinalizeInvoiceDialog = forwardRef<FinalizeInvoiceDialogRef>((_, re
   const [invoice, setInvoice] = useState<InvoiceForFinalizeInvoiceFragment>()
   const [finalizeInvoice] = useFinalizeInvoiceMutation({
     variables: { input: { id: invoice?.id || '' } },
+    context: {
+      silentErrorCodes: [LagoApiError.UnprocessableEntity, LagoApiError.InternalError],
+    },
     refetchQueries: ['getCustomerInvoices', 'getInvoiceDetails'],
     onCompleted({ finalizeInvoice: finalizeInvoiceRes }) {
       if (finalizeInvoiceRes?.id) {
@@ -50,6 +54,18 @@ export const FinalizeInvoiceDialog = forwardRef<FinalizeInvoiceDialogRef>((_, re
           severity: 'success',
         })
       }
+    },
+    onError: ({ graphQLErrors }) => {
+      graphQLErrors.forEach((graphQLError) => {
+        const { extensions } = graphQLError as LagoGQLError
+
+        if (extensions.details?.taxError?.length) {
+          addToast({
+            severity: 'danger',
+            translateKey: 'text_1724438705077s7oxv5be87m',
+          })
+        }
+      })
     },
   })
 
