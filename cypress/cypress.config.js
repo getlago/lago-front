a7db9c2cede24e7b1bd5dc6780d2d25abf487954
Kelly Phan <keelly.p@gmail.com>
2024-09-10 13:47:39 +0100

    feat(cypress): upload failed files as artifacts

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -7,5 +7,19 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: process.env.CYPRESS_APP_URL,
     experimentalRunAllSpecs: true,
+    setupNodeEvents(on) {
+      on('after:spec', (_spec, results) => {
+        if (results && results.video) {
+          // Do we have failures for any retry attempts?
+          const failures = results.tests.some((test) =>
+            test.attempts.some((attempt) => attempt.state === 'failed'),
+          )
+          if (!failures) {
+            // delete the video if the spec passed and no tests retried
+            fs.unlinkSync(results.video)
+          }
+        }
+      })
+    },
   },
 })
