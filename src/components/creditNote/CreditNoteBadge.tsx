23f3c99d154ac4cc811c29c3357588e5e423e7f6
Stefan Moraru <stefan@getlago.com>
2024-09-23 08:19:17 +0300

    Create Credit Note Badge component

---
+++ src/components/creditNote/CreditNoteBadge.tsx
@@ -0,0 +1,36 @@
+import {
+  CREDIT_NOTE_TYPE_TRANSLATIONS_MAP,
+  creditNoteTaxError,
+  creditNoteType,
+} from '~/components/creditNote/utils'
+import { Chip } from '~/components/designSystem/Chip'
+import { CreditNote, CreditNoteTableItemFragment } from '~/generated/graphql'
+import { useInternationalization } from '~/hooks/core/useInternationalization'
+
+const CreditNoteBadge = ({
+  creditNote,
+}: {
+  creditNote?: CreditNoteTableItemFragment | CreditNote | null
+}) => {
+  const { translate } = useInternationalization()
+
+  if (!creditNote) return null
+
+  const { creditAmountCents, refundAmountCents, voidedAt, errorDetails } = creditNote
+
+  const type = creditNoteType({
+    creditAmountCents,
+    refundAmountCents,
+    voidedAt,
+  })
+
+  if (!type) return null
+
+  const label = translate(CREDIT_NOTE_TYPE_TRANSLATIONS_MAP[type])
+
+  const hasError = creditNoteTaxError({ errorDetails })
+
+  return <Chip label={label} icon={hasError ? 'warning-filled' : undefined} />
+}
+
+export default CreditNoteBadge
