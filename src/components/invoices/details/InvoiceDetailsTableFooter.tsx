01f20003c5725625304eca3080f46f4dc940d1a5
Lovro Colic <lovro.colic.lc@gmail.com>
2024-09-25 15:37:32 +0200

    handle tax deduction case for tax provider

--- src/components/invoices/details/InvoiceDetailsTableFooter.tsx
+++ src/components/invoices/details/InvoiceDetailsTableFooter.tsx
@@ -31,6 +31,7 @@ gql`
       id
       amountCents
       feesAmountCents
+      taxableBaseAmountCents
       taxRate
       taxName
       enumedTaxCode
@@ -173,7 +174,7 @@ export const InvoiceDetailsTableFooter = memo(
                                   style: 'percent',
                                 }),
                                 amount: intlFormatNumber(
-                                  deserializeAmount(appliedTax.feesAmountCents || 0, currency),
+                                  deserializeAmount(appliedTax.taxableBaseAmountCents || 0, currency),
                                   {
                                     currencyDisplay: 'symbol',
                                     currency,
