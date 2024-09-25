306b590b54eb3fa7834da9b7de7f9b662719c39d
Kelly Phan <keelly.p@gmail.com>
2024-09-25 17:42:43 +0200

    feat(hmr): handle fast reloading

--- .babelrc.js
+++ .babelrc.js
@@ -1,6 +1,12 @@
-module.exports = () => {
+module.exports = (api) => {
+  // This caches the Babel config
+  api.cache.using(() => process.env.NODE_ENV)
   return {
-    presets: ['@babel/preset-env', '@babel/preset-react'],
+    presets: [
+      '@babel/preset-env',
+      // Enable development transform of React with new automatic runtime
+      ['@babel/preset-react', { development: !api.env('production'), runtime: 'automatic' }],
+    ],
     plugins: [
       '@babel/plugin-syntax-dynamic-import',
       ['babel-plugin-styled-components', { displayName: true }],
@@ -13,6 +19,7 @@ module.exports = () => {
           css: true,
         },
       ],
-    ],
+      ...(!api.env('production') && ['react-refresh/babel']),
+    ].filter(Boolean),
   }
 }
