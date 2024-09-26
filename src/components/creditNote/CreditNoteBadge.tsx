0ff2d1a00fadff343c2e97a4c3749761f151381e
Stefan Moraru <stefan@getlago.com>
2024-09-26 13:27:56 +0300

    LAGO-350 fix tooltip positioning

--- src/components/creditNote/CreditNoteBadge.tsx
+++ src/components/creditNote/CreditNoteBadge.tsx
@@ -27,17 +27,18 @@ const CreditNoteBadge = ({
   const hasError = taxProviderSyncable
 
   return (
-    <div className="flex items-center gap-2 rounded-lg border border-grey-400 bg-grey-100 px-2 py-1">
-      <Typography className="whitespace-nowrap text-sm font-medium text-grey-700">
-        {label}
-      </Typography>
+    <Tooltip
+      title={hasError ? translate('text_1727090499191gqzispoy1qz') : null}
+      placement="top-start"
+    >
+      <div className="flex items-center gap-2 rounded-lg border border-grey-400 bg-grey-100 px-2 py-1">
+        <Typography className="whitespace-nowrap text-sm font-medium text-grey-700">
+          {label}
+        </Typography>
 
-      {hasError ? (
-        <Tooltip title={translate('text_1727090499191gqzispoy1qz')} placement="top">
-          <Icon name="warning-unfilled" />
-        </Tooltip>
-      ) : null}
-    </div>
+        {hasError && <Icon name="warning-unfilled" />}
+      </div>
+    </Tooltip>
   )
 }
 
