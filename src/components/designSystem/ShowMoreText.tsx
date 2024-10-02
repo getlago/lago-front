7639a1dd46eeb2b4ed8b10c8105d6aefa4e0bbee
Kelly Phan <keelly.p@gmail.com>
2024-10-02 18:40:53 +0200

    refactor: migrate ShowMoreText

--- src/components/designSystem/ShowMoreText.tsx
+++ src/components/designSystem/ShowMoreText.tsx
@@ -1,9 +1,7 @@
 import { cloneElement, ReactElement, useState } from 'react'
-import styled from 'styled-components'
 
 import { Typography, TypographyProps } from '~/components/designSystem'
 import { useInternationalization } from '~/hooks/core/useInternationalization'
-import { theme } from '~/styles'
 
 interface ShowMoreTextProps extends TypographyProps {
   text: string
@@ -19,33 +17,22 @@ export const ShowMoreText = ({ text, limit, showMore, ...props }: ShowMoreTextPr
 
   return (
     <Typography {...props}>
-      <TruncatedText>{text.substring(0, limit)}...</TruncatedText>
-      <ShowMoreHandler>
+      <span className="mr-1">{text.substring(0, limit)}...</span>
+      <span className="text-blue-600">
         {!showMore || typeof showMore === 'string' ? (
-          /* eslint-disable jsx-a11y/no-static-element-interactions, jsx-a11y/click-events-have-key-events, jsx-a11y/anchor-is-valid */
-          <span onClick={() => setIsTextTruncated(false)}>
+          <button
+            className="h-auto rounded-none p-0 focus:ring"
+            onClick={() => setIsTextTruncated(false)}
+          >
             {showMore ? showMore : translate('text_62bdbf07117c3d1f178d6517')}
-          </span>
+          </button>
         ) : (
-          /* eslint-enable jsx-a11y/no-static-element-interactions, jsx-a11y/click-events-have-key-events, jsx-a11y/anchor-is-valid */
           !!showMore &&
           cloneElement(showMore as ReactElement, { onClick: () => setIsTextTruncated(false) })
         )}
-      </ShowMoreHandler>
+      </span>
     </Typography>
   )
 }
-
-const ShowMoreHandler = styled.span`
-  color: ${theme.palette.primary[600]};
-
-  &:hover {
-    cursor: pointer;
-  }
-`
-
-const TruncatedText = styled.span`
-  margin-right: ${theme.spacing(1)};
-`
 
 ShowMoreText.displayName = 'ShowMoreText'
