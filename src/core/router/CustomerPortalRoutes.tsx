78cb4af16ff9eb7dba082f9a14a9450a128131a1
Stefan Moraru <stefan@getlago.com>
2024-10-03 14:04:26 +0300

    Add route without itemId

--- src/core/router/CustomerPortalRoutes.tsx
+++ src/core/router/CustomerPortalRoutes.tsx
@@ -8,7 +8,8 @@ const PortalInit = lazyLoad(
 
 // ----------- Routes -----------
 export const CUSTOMER_PORTAL_ROUTE = '/customer-portal/:token'
-export const CUSTOMER_PORTAL_ROUTE_PAGE = '/customer-portal/:token/:page/:itemId'
+export const CUSTOMER_PORTAL_ROUTE_PAGE = '/customer-portal/:token/:page'
+export const CUSTOMER_PORTAL_ROUTE_PAGE_ITEMID = '/customer-portal/:token/:page/:itemId'
 
 export const customerPortalRoutes: CustomRouteObject[] = [
   {
@@ -17,6 +18,10 @@ export const customerPortalRoutes: CustomRouteObject[] = [
   },
   {
     path: CUSTOMER_PORTAL_ROUTE_PAGE,
+    element: <PortalInit />,
+  },
+  {
+    path: CUSTOMER_PORTAL_ROUTE_PAGE_ITEMID,
     element: <PortalInit />,
   },
 ]
