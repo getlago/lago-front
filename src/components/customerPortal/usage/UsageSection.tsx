627526f967752db944d0ad56c2c7c1c9cc3c6707
Stefan Moraru <stefan@getlago.com>
2024-10-09 09:38:29 +0300

    LAGO-377 do not display plan section without plans

--- src/components/customerPortal/usage/UsageSection.tsx
+++ src/components/customerPortal/usage/UsageSection.tsx
@@ -34,30 +34,34 @@ const UsageSection = ({ viewSubscription }: PortalUsageSectionProps) => {
   const applicableTimezone =
     data?.customerPortalSubscriptions?.collection?.[0]?.customer?.applicableTimezone
 
+  if (loading) {
+    return <SectionLoading />
+  }
+
+  if (!subscription?.length) {
+    return null
+  }
+
   return (
     <SectionContainer>
       <SectionTitle title={translate('text_1728377307160ilquuusbuwq')} />
 
-      {loading && <SectionLoading />}
-
-      {!loading && (
-        <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-          {subscription?.map((item) => (
-            <UsageSubscriptionItem
-              subscription={item}
-              applicableTimezone={applicableTimezone}
-              key={item.id}
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+        {subscription?.map((item) => (
+          <UsageSubscriptionItem
+            subscription={item}
+            applicableTimezone={applicableTimezone}
+            key={item.id}
+          >
+            <button
+              className="p-0 text-base font-normal text-blue-600"
+              onClick={() => viewSubscription(item.id)}
             >
-              <button
-                className="p-0 text-base font-normal text-blue-600"
-                onClick={() => viewSubscription(item.id)}
-              >
-                {translate('text_17283773071604x345yf0jbz')}
-              </button>
-            </UsageSubscriptionItem>
-          ))}
-        </div>
-      )}
+              {translate('text_17283773071604x345yf0jbz')}
+            </button>
+          </UsageSubscriptionItem>
+        ))}
+      </div>
     </SectionContainer>
   )
 }
