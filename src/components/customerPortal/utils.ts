5894ba707b4f3ce713b66e3d8628bcd6ae6f3f4a
Stefan Moraru <stefan@getlago.com>
2024-10-09 09:43:56 +0300

    LAGO-378 add day to subscription renewal date

--- src/components/customerPortal/utils.ts
+++ src/components/customerPortal/utils.ts
@@ -1,4 +1,6 @@
-import { formatDateToTZ } from '~/core/timezone/utils'
+import { DateTime } from 'luxon'
+
+import { getTimezoneConfig } from '~/core/timezone/utils'
 import { TimezoneEnum } from '~/generated/graphql'
 
 type PlanRenewalDateProps = {
@@ -6,7 +8,19 @@ type PlanRenewalDateProps = {
   applicableTimezone?: TimezoneEnum | null
 }
 
+const formatAndAddDay = (
+  date: string,
+  timezone: TimezoneEnum | null | undefined,
+  format?: string,
+) => {
+  return DateTime.fromISO(date, {
+    zone: getTimezoneConfig(timezone).name,
+  })
+    .plus({ days: 1 })
+    .toFormat(format || 'LLL. dd, yyyy')
+}
+
 export const planRenewalDate = ({
   currentBillingPeriodEndingAt,
   applicableTimezone,
-}: PlanRenewalDateProps) => formatDateToTZ(currentBillingPeriodEndingAt, applicableTimezone)
+}: PlanRenewalDateProps) => formatAndAddDay(currentBillingPeriodEndingAt, applicableTimezone)
