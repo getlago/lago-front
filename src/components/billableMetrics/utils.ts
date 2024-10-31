341200a60bfd1f1e98ada6073976ed2d1322d7cc
Stefan Moraru <stefan@getlago.com>
2024-10-31 16:38:43 +0200

    feat(billableMetrics): create utils for parsing and evaluating

---
+++ src/components/billableMetrics/utils.ts
@@ -0,0 +1,65 @@
+import { EventPayload, ValidationResult } from '~/components/billableMetrics/CustomExpressionDrawer'
+
+import { evaluateExpression, parseExpression } from '../../lago-expression/expression_js'
+
+export const wrappedEvaluateExpression = (
+  expression: string,
+  payload: EventPayload,
+): ValidationResult => {
+  try {
+    let eventPayload = payload
+
+    if (typeof payload === 'string') {
+      eventPayload = JSON.parse(payload)
+    }
+
+    const res = evaluateExpression(
+      parseExpression(expression),
+      eventPayload.events.code,
+      eventPayload.events.timestamp,
+      eventPayload.events.properties,
+    )
+
+    return {
+      result: res,
+    }
+  } catch (e) {
+    return {
+      error: e as string,
+    }
+  }
+}
+
+export const wrappedParseExpression = (expression?: string | null): boolean => {
+  if (!expression) {
+    return false
+  }
+
+  try {
+    parseExpression(expression)
+
+    return true
+  } catch (e) {
+    return false
+  }
+}
+
+export const isValidJSON = (json?: unknown) => {
+  if (!json) {
+    return false
+  }
+
+  try {
+    if (typeof json === 'object') {
+      return true
+    }
+
+    if (typeof json === 'string') {
+      JSON.parse(json)
+    }
+
+    return true
+  } catch (e) {
+    return false
+  }
+}
