3a35ca7231b8a9a11ad39804c1a1b15c3c888734
Alexandre Monjol <alexandre.monjol@gmail.com>
2024-11-06 17:38:25 +0100

    misc: migrate some Skeleton to use the new API

--- src/components/SkeletonDetailsPage.tsx
+++ src/components/SkeletonDetailsPage.tsx
@@ -1,68 +1,31 @@
-import styled from 'styled-components'
-
-import { NAV_HEIGHT, theme } from '~/styles'
+import { PropsWithChildren } from 'react'
 
 import { Skeleton } from './designSystem'
 
 const SkeletonDetailsPage = () => {
   return (
     <div>
-      <SkeletonHeader>
-        <Skeleton variant="text" width={312} height={12} />
-      </SkeletonHeader>
-      <SkeletonBody>
-        <SkeletonBodyFirst>
-          <Skeleton variant="text" width={80} height={12} />
-          <Skeleton variant="text" width={200} height={12} />
-        </SkeletonBodyFirst>
-        <SkeletonBodySecond>
-          <SkeletonBodyFirst>
-            <Skeleton variant="text" width={80} height={12} />
-            <Skeleton variant="text" width={200} height={12} />
-          </SkeletonBodyFirst>
-          <SkeletonBodyFirst>
-            <Skeleton variant="text" width={80} height={12} />
-            <Skeleton variant="text" width={200} height={12} />
-          </SkeletonBodyFirst>
-        </SkeletonBodySecond>
-      </SkeletonBody>
+      <div className="flex h-18 items-center">
+        <Skeleton className="w-78" variant="text" />
+      </div>
+      <div className="grid grid-cols-2 gap-6">
+        {[0, 1, 2, 3].map((i) => (
+          <div className="flex flex-col gap-3 pb-3 pt-1" key={`skeleton-details-page-${i}`}>
+            {i !== 1 && (
+              <>
+                <Skeleton className="w-20" variant="text" />
+                <Skeleton className="w-50" variant="text" />
+              </>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
 
 export default SkeletonDetailsPage
 
-const SkeletonHeader = styled.div`
-  height: ${NAV_HEIGHT}px;
-  display: flex;
-  align-items: center;
-`
-
-const SkeletonBody = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: ${theme.spacing(6)};
-`
-
-const SkeletonBodyFirst = styled.div`
-  width: 100%;
-  padding: ${theme.spacing(1)} 0 ${theme.spacing(2)} 0;
-  box-sizing: border-box;
-  display: flex;
-  flex-direction: column;
-  gap: ${theme.spacing(3)};
-`
-
-const SkeletonBodySecond = styled.div`
-  width: 100%;
-  padding: ${theme.spacing(1)} 0 ${theme.spacing(2)} 0;
-  box-sizing: border-box;
-  display: flex;
-  gap: ${theme.spacing(4)};
-`
-
-export const LoadingSkeletonWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: ${theme.spacing(12)};
-`
+export const LoadingSkeletonWrapper = ({ children }: PropsWithChildren) => (
+  <div className="flex flex-col gap-12">{children}</div>
+)
