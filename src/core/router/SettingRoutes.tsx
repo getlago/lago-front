224e421b13f8d73909155e336b212cf25bd38f36
Kelly Phan <keelly.p@gmail.com>
2024-11-25 18:10:58 +0100

    feat: create page

--- src/core/router/SettingRoutes.tsx
+++ src/core/router/SettingRoutes.tsx
@@ -18,6 +18,14 @@ const InvoiceSettings = lazyLoad(
   () =>
     import(/* webpackChunkName: 'invoice-settings' */ '~/pages/settings/Invoices/InvoiceSettings'),
 )
+
+const CreateInvoiceCustomFooter = lazyLoad(
+  () =>
+    import(
+      /* webpackChunkName: 'invoice-custom-footer' */ '~/pages/settings/Invoices/CreateCustomFooter'
+    ),
+)
+
 const TaxesSettings = lazyLoad(
   () => import(/* webpackChunkName: 'tax-settings' */ '~/pages/settings/TaxesSettings'),
 )
@@ -164,8 +172,13 @@ export const EMAILS_SCENARIO_CONFIG_ROUTE = `${SETTINGS_ROUTE}/emails/config/:ty
 export const XERO_INTEGRATION_ROUTE = `${INTEGRATIONS_ROUTE}/xero`
 export const XERO_INTEGRATION_DETAILS_ROUTE = `${INTEGRATIONS_ROUTE}/xero/:integrationId/:tab`
 export const DUNNINGS_SETTINGS_ROUTE = `${SETTINGS_ROUTE}/dunnings`
+
+/**
+ * Creation routes
+ */
 export const CREATE_DUNNING_ROUTE = `${SETTINGS_ROUTE}/dunnings/create`
 export const UPDATE_DUNNING_ROUTE = `${SETTINGS_ROUTE}/dunnings/:campaignId/edit`
+export const CREATE_INVOICE_CUSTOM_FOOTER = `${INVOICE_SETTINGS_ROUTE}/custom-footer/create`
 
 export const settingRoutes: CustomRouteObject[] = [
   {
@@ -347,5 +360,12 @@ export const settingRoutes: CustomRouteObject[] = [
     private: true,
     element: <CreateDunning />,
     permissions: ['dunningCampaignsCreate', 'dunningCampaignsView', 'dunningCampaignsUpdate'],
+  },
+  {
+    path: CREATE_INVOICE_CUSTOM_FOOTER,
+    private: true,
+    element: <CreateInvoiceCustomFooter />,
+    // TODO: Add permissions
+    permissions: ['organizationInvoicesUpdate'],
   },
 ]
---
+++ src/pages/settings/Invoices/CreateCustomFooter.tsx
@@ -0,0 +1,241 @@
+import { useFormik } from 'formik'
+import { useRef, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { boolean, object, string } from 'yup'
+
+import { Button, Skeleton, Tooltip, Typography } from '~/components/designSystem'
+import { SwitchField, TextInputField } from '~/components/form'
+import { WarningDialog, WarningDialogRef } from '~/components/WarningDialog'
+import { INVOICE_SETTINGS_ROUTE } from '~/core/router'
+import { useInternationalization } from '~/hooks/core/useInternationalization'
+import { PageHeader } from '~/styles'
+
+const CreateInvoiceCustomFooter = () => {
+  const { translate } = useInternationalization()
+  const navigate = useNavigate()
+
+  const warningDirtyAttributesDialogRef = useRef<WarningDialogRef>(null)
+
+  const loading = false
+
+  const formikProps = useFormik({
+    initialValues: {
+      name: '',
+      code: '',
+      description: '',
+      invoiceDisplayName: '',
+      details: '',
+      selected: false,
+    },
+    validationSchema: object().shape({
+      name: string().required(''),
+      code: string().required(''),
+      description: string(),
+      invoiceDisplayName: string().when('details', {
+        is: (details: string) => !details,
+        then: (schema) => schema.required(''),
+        otherwise: (schema) => schema.notRequired(),
+      }),
+      selected: boolean().required(''),
+    }),
+    enableReinitialize: true,
+    validateOnMount: true,
+    onSubmit: async (values) => {
+      console.log(values)
+    },
+  })
+
+  const [shouldDisplayDescription, setShouldDisplayDescription] = useState(
+    !!formikProps.initialValues.description,
+  )
+
+  const onSubmit = () => {
+    if (!!formikProps.values.selected) {
+      console.log('dialog')
+    } else {
+      formikProps.submitForm()
+    }
+  }
+
+  return (
+    <>
+      <div>
+        <PageHeader>
+          <Typography variant="bodyHl" color="textSecondary" noWrap>
+            {translate('text_1732553358445168zt8fopyf')}
+          </Typography>
+          <Button
+            variant="quaternary"
+            icon="close"
+            onClick={() =>
+              formikProps.dirty
+                ? warningDirtyAttributesDialogRef.current?.openDialog()
+                : navigate(INVOICE_SETTINGS_ROUTE)
+            }
+          />
+        </PageHeader>
+
+        {loading ? (
+          <div className="container mx-auto mb-15 mt-12 flex flex-col gap-12">
+            <div>
+              <Skeleton variant="text" className="mb-4 w-40" />
+              <Skeleton variant="text" className="w-100" />
+            </div>
+            {[0, 1].map((_, index) => (
+              <div key={`loading-${index}`}>
+                <div className="flex flex-col gap-5 pb-12 shadow-b">
+                  <Skeleton variant="text" className="w-40" />
+                  <Skeleton variant="text" className="w-100" />
+                  <Skeleton variant="text" className="w-74" />
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <>
+            <div className="container mx-auto min-h-[calc(100vh_-_theme(space.nav)_-_theme(space.footer))] pb-20 pt-12">
+              <div className="mb-12 not-last-child:mb-1">
+                <Typography variant="headline" color="textSecondary">
+                  {translate('text_1732553358445168zt8fopyf')}
+                </Typography>
+                <Typography variant="body">{translate('text_1732553358445p7rg0i0dzws')}</Typography>
+              </div>
+
+              <div className="flex flex-col gap-12 not-last-child:pb-12 not-last-child:shadow-b">
+                <section className="not-last-child:mb-6">
+                  <div className="not-last-child:mb-2">
+                    <Typography variant="subhead">
+                      {translate('text_1732553358445sjgzrnstueo')}
+                    </Typography>
+                    <Typography variant="caption">
+                      {translate('text_17325533584451rema9e6rs5')}
+                    </Typography>
+                  </div>
+                  <div className="flex items-start gap-6 *:flex-1">
+                    <TextInputField
+                      name="name"
+                      formikProps={formikProps}
+                      label={translate('text_6419c64eace749372fc72b0f')}
+                      placeholder={translate('text_6584550dc4cec7adf861504f')}
+                    />
+                    <TextInputField
+                      name="code"
+                      beforeChangeFormatter="code"
+                      formikProps={formikProps}
+                      label={translate('text_62876e85e32e0300e1803127')}
+                      placeholder={translate('text_6584550dc4cec7adf8615053')}
+                    />
+                  </div>
+                  {shouldDisplayDescription ? (
+                    <div className="flex items-center gap-2">
+                      <TextInputField
+                        className="flex-1"
+                        name="description"
+                        label={translate('text_623b42ff8ee4e000ba87d0c8')}
+                        placeholder={translate('text_1728584028187uqs16ra27ef')}
+                        rows="3"
+                        multiline
+                        formikProps={formikProps}
+                      />
+
+                      <Tooltip
+                        placement="top-end"
+                        title={translate('text_63aa085d28b8510cd46443ff')}
+                      >
+                        <Button
+                          icon="trash"
+                          variant="quaternary"
+                          onClick={() => {
+                            formikProps.setFieldValue('description', '')
+                            setShouldDisplayDescription(false)
+                          }}
+                        />
+                      </Tooltip>
+                    </div>
+                  ) : (
+                    <Button
+                      startIcon="plus"
+                      variant="quaternary"
+                      onClick={() => setShouldDisplayDescription(true)}
+                      data-test="show-description"
+                    >
+                      {translate('text_642d5eb2783a2ad10d670324')}
+                    </Button>
+                  )}
+                </section>
+
+                <section className="not-last-child:mb-6">
+                  <div className="not-last-child:mb-2">
+                    <Typography variant="subhead">
+                      {translate('text_1732553358445ia697d93gbj')}
+                    </Typography>
+                    <Typography variant="caption">
+                      {translate('text_1732553358445diim0lbo5nl')}
+                    </Typography>
+                  </div>
+                  <TextInputField
+                    name="invoiceDisplayName"
+                    formikProps={formikProps}
+                    label={translate('text_65018c8e5c6b626f030bcf26')}
+                    placeholder={translate('text_65a6b4e2cb38d9b70ec53d41')}
+                  />
+                  <TextInputField
+                    name="details"
+                    formikProps={formikProps}
+                    label={translate('text_1732553358445fhl5zibpn2l')}
+                    placeholder={translate('text_1732553358446t0zh79g9ruk')}
+                    rows="3"
+                    multiline
+                  />
+                </section>
+
+                <section className="not-last-child:mb-6">
+                  <SwitchField
+                    name="selected"
+                    formikProps={formikProps}
+                    label={translate('text_1732553889947h4iijpzflwj')}
+                    subLabel={translate('text_1732553889948thkn4jonnyy')}
+                  />
+                </section>
+              </div>
+            </div>
+
+            <footer className="sticky bottom-0 flex h-footer border border-grey-200 bg-white">
+              <div className="container mx-auto flex h-full items-center justify-end">
+                <div className="flex items-center gap-6">
+                  <Button
+                    variant="quaternary"
+                    onClick={() =>
+                      formikProps.dirty
+                        ? warningDirtyAttributesDialogRef.current?.openDialog()
+                        : navigate(INVOICE_SETTINGS_ROUTE)
+                    }
+                  >
+                    {translate('text_6411e6b530cb47007488b027')}
+                  </Button>
+                  <Button
+                    variant="primary"
+                    disabled={!formikProps.isValid || !formikProps.dirty}
+                    onClick={onSubmit}
+                  >
+                    {translate('text_17325538899488ftsvph8ko5')}
+                  </Button>
+                </div>
+              </div>
+            </footer>
+          </>
+        )}
+      </div>
+
+      <WarningDialog
+        ref={warningDirtyAttributesDialogRef}
+        title={translate('text_6244277fe0975300fe3fb940')}
+        description={translate('text_6244277fe0975300fe3fb946')}
+        continueText={translate('text_6244277fe0975300fe3fb94c')}
+        onContinue={() => navigate(INVOICE_SETTINGS_ROUTE)}
+      />
+    </>
+  )
+}
+
+export default CreateInvoiceCustomFooter
