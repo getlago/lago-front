272a1fc5f4f35fc45a1ce469d02e742f2c6afeb0
Alexandre Monjol <alexandre.monjol@gmail.com>
2024-12-23 15:55:37 +0100

    chore: remove debug info dialog

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,22 @@
 import { ApolloClient, ApolloProvider, NormalizedCacheObject } from '@apollo/client'
 import { loadDevMessages, loadErrorMessages } from '@apollo/client/dev'
 import { StyledEngineProvider, ThemeProvider } from '@mui/material'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 
-import { DebugInfoDialog, DebugInfoDialogRef } from '~/components/DebugInfoDialog'
 import { ToastContainer } from '~/components/designSystem/Toasts'
 import { ErrorBoundary } from '~/components/ErrorBoundary'
 import { RouteWrapper } from '~/components/RouteWrapper'
 import { UserIdentifier } from '~/components/UserIdentifier'
 import { envGlobalVar, initializeApolloClient, initializeTranslations } from '~/core/apolloClient'
 import { initializeYup } from '~/formValidation/initializeYup'
-import { useShortcuts } from '~/hooks/ui/useShortcuts'
 import { theme } from '~/styles'
 
 import { AppEnvEnum } from './core/constants/globalTypes'
 
 const App = () => {
   const [client, setClient] = useState<ApolloClient<NormalizedCacheObject> | null>(null)
-  const debugInfoDialogRef = useRef<DebugInfoDialogRef>(null)
   const { appEnv } = envGlobalVar()
-
-  useShortcuts([
-    {
-      keys: ['Ctrl', 'KeyI'],
-      action: () => debugInfoDialogRef.current?.openDialog(),
-    },
-  ])
 
   useEffect(() => {
     async function initApolloClient() {
@@ -58,7 +48,6 @@ const App = () => {
             </ErrorBoundary>
             <UserIdentifier />
             <ToastContainer />
-            <DebugInfoDialog ref={debugInfoDialogRef} />
           </ThemeProvider>
         </StyledEngineProvider>
       </ApolloProvider>
