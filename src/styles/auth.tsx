02a5fbbd4daabcf8aa24d543c57b71b53ce03619
Alexandre Monjol <alexandre.monjol@gmail.com>
2024-12-30 16:34:14 +0100

    misc: migrate some shared auth style to FC components using tailwind

--- src/styles/auth.tsx
+++ src/styles/auth.tsx
@@ -1,36 +1,27 @@
 import { FC, PropsWithChildren } from 'react'
-import styled from 'styled-components'
 
 import { Typography } from '~/components/designSystem'
 import Logo from '~/public/images/logo/lago-logo.svg'
-import { theme } from '~/styles'
 
 import { tw } from './utils'
 
-export const Page = styled.div`
-  box-sizing: border-box;
-  background-color: ${theme.palette.grey[100]};
-  min-height: 100vh;
-  padding: ${theme.spacing(20)};
+export const Page: FC<PropsWithChildren> = ({ children }) => (
+  <div className="min-h-full bg-grey-100 px-4 py-20 md:p-20">{children}</div>
+)
 
-  ${theme.breakpoints.down('md')} {
-    padding: ${theme.spacing(20)} ${theme.spacing(4)};
+export const StyledLogo: FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <Logo className="mb-12" {...props} />
+)
+
+export const Card: FC<
+  PropsWithChildren & {
+    className?: string
   }
-`
-
-export const StyledLogo = styled(Logo)`
-  margin-bottom: ${theme.spacing(12)};
-`
-
-export const Card = styled.div`
-  margin: 0 auto;
-  background-color: ${theme.palette.background.paper};
-  border-radius: 12px;
-  box-shadow: 0px 6px 8px 0px #19212e1f; // TODO
-  padding: ${theme.spacing(10)};
-  max-width: 576px;
-  box-sizing: border-box;
-`
+> = ({ children, className, ...props }) => (
+  <div className={tw('mx-auto max-w-144 rounded-xl bg-white p-10 shadow-md', className)} {...props}>
+    {children}
+  </div>
+)
 
 export const Title: FC<PropsWithChildren> = ({ children }) => (
   <Typography className="mb-3" variant="headline">
