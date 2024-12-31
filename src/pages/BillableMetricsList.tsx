2aaa7516c34a38d130f9b32e39154c5a5f0bd9ba
Alexandre Monjol <alexandre.monjol@gmail.com>
2024-12-31 11:21:07 +0100

    misc: use Table component for billable metrics list

--- src/pages/BillableMetricsList.tsx
+++ src/pages/BillableMetricsList.tsx
@@ -4,27 +4,27 @@ import { generatePath, useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
 
 import {
-  BillableMetricItem,
-  BillableMetricItemSkeleton,
-} from '~/components/billableMetrics/BillableMetricItem'
-import {
   DeleteBillableMetricDialog,
   DeleteBillableMetricDialogRef,
 } from '~/components/billableMetrics/DeleteBillableMetricDialog'
-import { ButtonLink, InfiniteScroll, Typography } from '~/components/designSystem'
-import { GenericPlaceholder } from '~/components/GenericPlaceholder'
+import { ButtonLink, InfiniteScroll, Table, Typography } from '~/components/designSystem'
 import { SearchInput } from '~/components/SearchInput'
 import { CREATE_BILLABLE_METRIC_ROUTE, UPDATE_BILLABLE_METRIC_ROUTE } from '~/core/router'
-import { BillableMetricItemFragmentDoc, useBillableMetricsLazyQuery } from '~/generated/graphql'
+import { useBillableMetricsLazyQuery } from '~/generated/graphql'
 import { useInternationalization } from '~/hooks/core/useInternationalization'
-import { useListKeysNavigation } from '~/hooks/ui/useListKeyNavigation'
 import { useDebouncedSearch } from '~/hooks/useDebouncedSearch'
+import { useOrganizationInfos } from '~/hooks/useOrganizationInfos'
 import { usePermissions } from '~/hooks/usePermissions'
-import EmptyImage from '~/public/images/maneki/empty.svg'
-import ErrorImage from '~/public/images/maneki/error.svg'
-import { ListContainer, ListHeader, PageHeader, theme } from '~/styles'
+import { PageHeader, theme } from '~/styles'
 
 gql`
+  fragment BillableMetricItem on BillableMetric {
+    id
+    name
+    code
+    createdAt
+  }
+
   query billableMetrics($page: Int, $limit: Int, $searchTerm: String) {
     billableMetrics(page: $page, limit: $limit, searchTerm: $searchTerm) {
       metadata {
@@ -36,14 +36,13 @@ gql`
       }
     }
   }
-
-  ${BillableMetricItemFragmentDoc}
 `
 
 const BillableMetricsList = () => {
   const { translate } = useInternationalization()
   const navigate = useNavigate()
   const { hasPermissions } = usePermissions()
+  const { formatTimeOrgaTZ } = useOrganizationInfos()
   const deleteDialogRef = useRef<DeleteBillableMetricDialogRef>(null)
   const [getBillableMetrics, { data, error, loading, fetchMore, variables }] =
     useBillableMetricsLazyQuery({
@@ -54,15 +53,9 @@ const BillableMetricsList = () => {
     })
   const { debouncedSearch, isLoading } = useDebouncedSearch(getBillableMetrics, loading)
   const list = data?.billableMetrics?.collection || []
-  const { onKeyDown } = useListKeysNavigation({
-    getElmId: (i) => `billable-metric-item-${i}`,
-    navigate: (id) =>
-      navigate(generatePath(UPDATE_BILLABLE_METRIC_ROUTE, { billableMetricId: String(id) })),
-  })
-  let index = -1
 
   return (
-    <div role="grid" tabIndex={-1} onKeyDown={onKeyDown}>
+    <>
       <Header $withSide>
         <Typography variant="bodyHl" color="textSecondary">
           {translate('text_623b497ad05b960101be3438')}
@@ -80,98 +73,111 @@ const BillableMetricsList = () => {
         </HeaderRigthBlock>
       </Header>
 
-      <ListContainer>
-        <ListHead $withActions>
-          <Typography color="disabled" variant="bodyHl">
-            {translate('text_623b497ad05b960101be343e')}
-          </Typography>
-          <Typography className="w-28" align="right" color="disabled" variant="bodyHl">
-            {translate('text_623b497ad05b960101be3440')}
-          </Typography>
-        </ListHead>
-        {!!isLoading && variables?.searchTerm ? (
-          <>
-            {isLoading &&
-              [0, 1, 2].map((i) => (
-                <BillableMetricItemSkeleton key={`billable-metric-item-skeleton-${i}`} />
-              ))}
-          </>
-        ) : !isLoading && !!error ? (
-          <>
-            {!!variables?.searchTerm ? (
-              <GenericPlaceholder
-                title={translate('text_623b53fea66c76017eaebb6e')}
-                subtitle={translate('text_63bab307a61c62af497e0599')}
-                image={<ErrorImage width="136" height="104" />}
-              />
-            ) : (
-              <GenericPlaceholder
-                title={translate('text_623b53fea66c76017eaebb6e')}
-                subtitle={translate('text_623b53fea66c76017eaebb76')}
-                buttonTitle={translate('text_623b53fea66c76017eaebb7a')}
-                buttonVariant="primary"
-                buttonAction={() => location.reload()}
-                image={<ErrorImage width="136" height="104" />}
-              />
-            )}
-          </>
-        ) : !isLoading && (!list || !list.length) ? (
-          <>
-            {!!variables?.searchTerm ? (
-              <GenericPlaceholder
-                title={translate('text_63bab307a61c62af497e05a2')}
-                subtitle={translate('text_63bab307a61c62af497e05a4')}
-                image={<EmptyImage width="136" height="104" />}
-              />
-            ) : (
-              <GenericPlaceholder
-                title={translate('text_623b53fea66c76017eaebb70')}
-                subtitle={translate('text_623b53fea66c76017eaebb78')}
-                buttonTitle={translate('text_623b53fea66c76017eaebb7c')}
-                buttonVariant="primary"
-                buttonAction={() => navigate(CREATE_BILLABLE_METRIC_ROUTE)}
-                image={<EmptyImage width="136" height="104" />}
-              />
-            )}
-          </>
-        ) : (
-          <InfiniteScroll
-            onBottom={() => {
-              const { currentPage = 0, totalPages = 0 } = data?.billableMetrics?.metadata || {}
+      <InfiniteScroll
+        onBottom={() => {
+          const { currentPage = 0, totalPages = 0 } = data?.billableMetrics?.metadata || {}
 
-              currentPage < totalPages &&
-                !isLoading &&
-                fetchMore({
-                  variables: { page: currentPage + 1 },
-                })
-            }}
-          >
-            {!!list &&
-              list.map((billableMetric) => {
-                index += 1
-
-                return (
-                  <BillableMetricItem
-                    key={billableMetric.id}
-                    billableMetric={billableMetric}
-                    deleteDialogRef={deleteDialogRef}
-                    navigationProps={{
-                      id: `billable-metric-item-${index}`,
-                      'data-id': billableMetric.id,
-                    }}
-                  />
-                )
-              })}
-            {isLoading &&
-              [0, 1, 2].map((i) => (
-                <BillableMetricItemSkeleton key={`billable-metric-item-skeleton-${i}`} />
-              ))}
-          </InfiniteScroll>
-        )}
-      </ListContainer>
+          currentPage < totalPages &&
+            !isLoading &&
+            fetchMore({
+              variables: { page: currentPage + 1 },
+            })
+        }}
+      >
+        <Table
+          name="billable-metrics-list"
+          data={list}
+          containerSize={{
+            default: 16,
+            md: 48,
+          }}
+          rowSize={72}
+          isLoading={isLoading}
+          hasError={!!error}
+          onRowAction={({ id }) =>
+            navigate(generatePath(UPDATE_BILLABLE_METRIC_ROUTE, { billableMetricId: id }))
+          }
+          columns={[
+            {
+              key: 'name',
+              title: translate('text_623b497ad05b960101be343e'),
+              minWidth: 200,
+              maxSpace: true,
+              content: ({ name, code }) => (
+                <>
+                  <Typography color="textSecondary" variant="bodyHl" noWrap>
+                    {name}
+                  </Typography>
+                  <Typography variant="caption" noWrap>
+                    {code}
+                  </Typography>
+                </>
+              ),
+            },
+            {
+              key: 'createdAt',
+              title: translate('text_623b497ad05b960101be3440'),
+              minWidth: 140,
+              content: ({ createdAt }) => (
+                <Typography variant="body" color="grey600">
+                  {formatTimeOrgaTZ(createdAt)}
+                </Typography>
+              ),
+            },
+          ]}
+          actionColumnTooltip={() => translate('text_6256de3bba111e00b3bfa51b')}
+          actionColumn={({ id }) => {
+            return [
+              hasPermissions(['billableMetricsUpdate'])
+                ? {
+                    startIcon: 'pen',
+                    title: translate('text_6256de3bba111e00b3bfa531'),
+                    onAction: () =>
+                      navigate(
+                        generatePath(UPDATE_BILLABLE_METRIC_ROUTE, { billableMetricId: id }),
+                      ),
+                  }
+                : null,
+              hasPermissions(['billableMetricsDelete'])
+                ? {
+                    startIcon: 'trash',
+                    title: translate('text_6256de3bba111e00b3bfa533'),
+                    onAction: () => deleteDialogRef.current?.openDialog(id),
+                  }
+                : null,
+            ]
+          }}
+          placeholder={{
+            errorState: !!variables?.searchTerm
+              ? {
+                  title: translate('text_623b53fea66c76017eaebb6e'),
+                  subtitle: translate('text_63bab307a61c62af497e0599'),
+                }
+              : {
+                  title: translate('text_623b53fea66c76017eaebb6e'),
+                  subtitle: translate('text_623b53fea66c76017eaebb76'),
+                  buttonTitle: translate('text_623b53fea66c76017eaebb7a'),
+                  buttonAction: () => location.reload(),
+                  buttonVariant: 'primary',
+                },
+            emptyState: !!variables?.searchTerm
+              ? {
+                  title: translate('text_63bab307a61c62af497e05a2'),
+                  subtitle: translate('text_63bab307a61c62af497e05a4'),
+                }
+              : {
+                  title: translate('text_623b53fea66c76017eaebb70'),
+                  subtitle: translate('text_623b53fea66c76017eaebb78'),
+                  buttonTitle: translate('text_623b53fea66c76017eaebb7c'),
+                  buttonAction: () => navigate(CREATE_BILLABLE_METRIC_ROUTE),
+                  buttonVariant: 'primary',
+                },
+          }}
+        />
+      </InfiniteScroll>
 
       <DeleteBillableMetricDialog ref={deleteDialogRef} />
-    </div>
+    </>
   )
 }
 
@@ -191,15 +197,6 @@ const HeaderRigthBlock = styled.div`
 
   > :first-child {
     margin-right: ${theme.spacing(3)};
-  }
-`
-
-const ListHead = styled(ListHeader)`
-  > *:first-child {
-    flex: 1;
-  }
-  > *:not(:last-child) {
-    margin-right: ${theme.spacing(6)};
   }
 `
 
