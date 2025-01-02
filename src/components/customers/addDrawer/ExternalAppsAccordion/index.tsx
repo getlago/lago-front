d6201e6d1fc06b247c9bc1f41359fa84270a8402
Kelly Phan <keelly.p@gmail.com>
2025-01-02 13:53:57 +0100

    refactor: migrate to tailwind

--- src/components/customers/addDrawer/ExternalAppsAccordion/index.tsx
+++ src/components/customers/addDrawer/ExternalAppsAccordion/index.tsx
@@ -1,8 +1,6 @@
 import { gql } from '@apollo/client'
-import { Stack } from '@mui/material'
 import { FormikProps } from 'formik'
 import { useEffect, useState } from 'react'
-import styled from 'styled-components'
 
 import { Accordion, Alert, Button, Popper, Typography } from '~/components/designSystem'
 import {
@@ -21,7 +19,7 @@ import {
 } from '~/generated/graphql'
 import { useInternationalization } from '~/hooks/core/useInternationalization'
 import PSPIcons from '~/public/images/psp-icons.svg'
-import { MenuPopper, theme } from '~/styles'
+import { MenuPopper } from '~/styles'
 
 import { AccountingProvidersAccordion } from './AccountingProvidersAccordion'
 import { CRMProvidersAccordion } from './CRMProvidersAccordion'
@@ -80,7 +78,7 @@ export const ExternalAppsAccordion = ({ formikProps, isEdition }: TExternalAppsA
     [IntegrationTypeEnum.Netsuite, IntegrationTypeEnum.Xero].includes(
       i.integrationType as IntegrationTypeEnum,
     ),
-    )
+  )
   const hadInitialTaxProvider = !!formikProps.values.integrationCustomers?.find((i) =>
     [IntegrationTypeEnum.Anrok].includes(i.integrationType as IntegrationTypeEnum),
   )
@@ -88,7 +86,7 @@ export const ExternalAppsAccordion = ({ formikProps, isEdition }: TExternalAppsA
     [IntegrationTypeEnum.Hubspot, IntegrationTypeEnum.Salesforce].includes(
       i.integrationType as IntegrationTypeEnum,
     ),
-    )
+  )
 
   const [showPaymentSection, setShowPaymentSection] = useState(!!formikProps.values.paymentProvider)
   const [showAccountingSection, setShowAccountingSection] = useState(hadInitialAccountingProvider)
@@ -102,7 +100,7 @@ export const ExternalAppsAccordion = ({ formikProps, isEdition }: TExternalAppsA
   useEffect(
     () => setShowAccountingSection(hadInitialAccountingProvider),
     [hadInitialAccountingProvider],
-    )
+  )
   useEffect(() => setShowTaxSection(hadInitialTaxProvider), [hadInitialTaxProvider])
   useEffect(() => setShowCRMSection(hadInitialCRMProvider), [hadInitialCRMProvider])
 
@@ -110,13 +108,15 @@ export const ExternalAppsAccordion = ({ formikProps, isEdition }: TExternalAppsA
     <Accordion
       size="large"
       summary={
-        <InlineSummaryForExternalApps>
-          <LocalPSPIcons />
+        <div className="flex items-center">
+          <div className="mr-3 h-6 [&>svg]:h-full">
+            <PSPIcons />
+          </div>
           <Typography variant="subhead">{translate('text_66423cad72bbad009f2f5689')}</Typography>
-        </InlineSummaryForExternalApps>
+        </div>
       }
     >
-      <Stack gap={6}>
+      <div className="flex flex-col gap-6">
         <div>
           <Typography variant="bodyHl" color="grey700">
             {translate('text_66423dbab233e60111c49461')}
@@ -180,7 +180,7 @@ export const ExternalAppsAccordion = ({ formikProps, isEdition }: TExternalAppsA
                 onClick={() => {
                   setShowPaymentSection(true)
                   scrollToIntegration(
-                      `.${ADD_CUSTOMER_PAYMENT_PROVIDER_ACCORDION} .${MUI_BUTTON_BASE_ROOT_CLASSNAME}`,
+                    `.${ADD_CUSTOMER_PAYMENT_PROVIDER_ACCORDION} .${MUI_BUTTON_BASE_ROOT_CLASSNAME}`,
                   )
                   closePopper()
                 }}
@@ -194,7 +194,7 @@ export const ExternalAppsAccordion = ({ formikProps, isEdition }: TExternalAppsA
                 onClick={() => {
                   setShowAccountingSection(true)
                   scrollToIntegration(
-                      `.${ADD_CUSTOMER_ACCOUNTING_PROVIDER_ACCORDION} .${MUI_BUTTON_BASE_ROOT_CLASSNAME}`,
+                    `.${ADD_CUSTOMER_ACCOUNTING_PROVIDER_ACCORDION} .${MUI_BUTTON_BASE_ROOT_CLASSNAME}`,
                   )
                   closePopper()
                 }}
@@ -209,7 +209,7 @@ export const ExternalAppsAccordion = ({ formikProps, isEdition }: TExternalAppsA
                 onClick={() => {
                   setShowTaxSection(true)
                   scrollToIntegration(
-                      `.${ADD_CUSTOMER_TAX_PROVIDER_ACCORDION} .${MUI_BUTTON_BASE_ROOT_CLASSNAME}`,
+                    `.${ADD_CUSTOMER_TAX_PROVIDER_ACCORDION} .${MUI_BUTTON_BASE_ROOT_CLASSNAME}`,
                   )
                   closePopper()
                 }}
@@ -224,7 +224,7 @@ export const ExternalAppsAccordion = ({ formikProps, isEdition }: TExternalAppsA
                 onClick={() => {
                   setShowCRMSection(true)
                   scrollToIntegration(
-                      `.${ADD_CUSTOMER_CRM_PROVIDER_ACCORDION} .${MUI_BUTTON_BASE_ROOT_CLASSNAME}`,
+                    `.${ADD_CUSTOMER_CRM_PROVIDER_ACCORDION} .${MUI_BUTTON_BASE_ROOT_CLASSNAME}`,
                   )
                   closePopper()
                 }}
@@ -244,19 +244,9 @@ export const ExternalAppsAccordion = ({ formikProps, isEdition }: TExternalAppsA
                 : translate('text_645d0728ea0a5a7bbf76d5c9')}
             </Alert>
           )}
-      </Stack>
+      </div>
     </Accordion>
   )
 }
 
 ExternalAppsAccordion.displayName = 'ExternalAppsAccordion'
-
-const InlineSummaryForExternalApps = styled.div`
-  display: flex;
-  align-items: center;
-`
-
-const LocalPSPIcons = styled(PSPIcons)`
-  height: 24px;
-  margin-right: ${theme.spacing(3)};
-`
