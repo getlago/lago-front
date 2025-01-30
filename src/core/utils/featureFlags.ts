ed98719388d5a9399c752627d45f19d2db825d58
Alexandre Monjol <alexandre.monjol@gmail.com>
2025-01-30 15:02:20 +0100

    bug: prevent side effect on slice chaining

--- src/core/utils/featureFlags.ts
+++ src/core/utils/featureFlags.ts
@@ -1,7 +1,7 @@
 // You can list your features such as FTR_ENABLED = 'ftr_enabled'
 export enum FeatureFlags {
   FTR_ENABLED = 'ftr_enabled',
-  FTR_SALESFORCE_ENABLED = 'ftr_salesforce_enabled',
+  FTR_STRIPE_BOLETO = 'ftr_stripe_boleto',
 }
 
 const FF_KEY = 'featureFlags'
