4f950ea9502a74ddce60b0d853516ae7745964ee
Kelly Phan <keelly.p@gmail.com>
2025-02-17 16:21:07 +0100

    fix: fix warnings and remove stack

--- src/components/subscriptions/SubscriptionUsageLifetimeGraph.tsx
+++ src/components/subscriptions/SubscriptionUsageLifetimeGraph.tsx
@@ -1,5 +1,4 @@
 import { ApolloError, gql } from '@apollo/client'
-import { Stack } from '@mui/material'
 import { useMemo } from 'react'
 import { generatePath } from 'react-router-dom'
 
@@ -113,15 +112,13 @@ export const SubscriptionUsageLifetimeGraphComponent = ({
     return getLifetimeGraphPercentages(lifetimeUsage)
   }, [lifetimeUsage])
 
+  const hasError = !!subscriptionError
+  const isSubscriptionActive = subscription?.status !== StatusTypeEnum.Pending
+  const isLifetimeUsageEmpty = !lifetimeUsage && !isLoading
+
   return (
     <section>
-      <Stack
-        direction={'row'}
-        height={40}
-        boxShadow={theme.shadows[7]}
-        alignItems={'flex-start'}
-        justifyContent={'space-between'}
-      >
+      <div className="flex h-10 flex-row items-start justify-between shadow-b">
         <div className="flex flex-row items-center gap-2">
           <Typography variant="subhead" color="grey700" noWrap>
             {translate('text_1726481163322ntor50xdm8k')}
@@ -131,9 +128,8 @@ export const SubscriptionUsageLifetimeGraphComponent = ({
           </Tooltip>
         </div>
 
-        {isLoading ? (
-          <Skeleton variant="text" className="mt-2 w-36" />
-        ) : !subscriptionError && !!lifetimeUsage ? (
+        {isLoading && <Skeleton variant="text" className="mt-2 w-36" />}
+        {!isLoading && !subscriptionError && !!lifetimeUsage && (
           <Typography
             variant="caption"
             color="grey600"
@@ -157,10 +153,10 @@ export const SubscriptionUsageLifetimeGraphComponent = ({
                 : formatDateToTZ(lifetimeUsage.totalUsageToDatetime, customerTimezone),
             })}
           </Typography>
-        ) : null}
-      </Stack>
+        )}
+      </div>
       <div className="flex flex-col gap-6 bg-white py-6">
-        {!!subscriptionError ? (
+        {hasError && (
           <GenericPlaceholder
             className="m-0 p-0"
             title={translate('text_636d023ce11a9d038819b579')}
@@ -170,102 +166,126 @@ export const SubscriptionUsageLifetimeGraphComponent = ({
             buttonAction={() => refetchLifetimeData()}
             image={<ErrorImage width="136" height="104" />}
           />
-        ) : (
+        )}
+
+        {!hasError && !isSubscriptionActive && (
+          <Typography variant="body" color="grey600">
+            {translate('text_1731423623190elua4pl3ccr')}
+          </Typography>
+        )}
+
+        {!hasError &&
+          isSubscriptionActive &&
+          isLifetimeUsageEmpty &&
+          hasProgressiveBillingPremiumIntegration && (
+            <Typography
+              variant="body"
+              color="grey600"
+              html={translate('text_17264811633225nhatoh524y', {
+                planLink: `${generatePath(UPDATE_PLAN_ROUTE, {
+                  planId: subscription?.plan?.id || '',
+                })}?origin=${REDIRECTION_ORIGIN_SUBSCRIPTION_USAGE}&subscriptionId=${subscriptionId}&customerId=${customerId}`,
+                subscriptionLink: `${generatePath(UPDATE_SUBSCRIPTION, {
+                  subscriptionId,
+                  customerId: subscription?.customer?.id || '',
+                })}?origin=${REDIRECTION_ORIGIN_SUBSCRIPTION_USAGE}&subscriptionId=${subscriptionId}&customerId=${customerId}`,
+              })}
+            />
+          )}
+
+        {!hasError && isSubscriptionActive && !isLifetimeUsageEmpty && (
           <>
-            {subscription?.status === StatusTypeEnum.Pending ? (
-              <Typography variant="body" color="grey600">
-                {translate('text_1731423623190elua4pl3ccr')}
-              </Typography>
-            ) : !lifetimeUsage && !isLoading && hasProgressiveBillingPremiumIntegration ? (
-              <Typography
-                variant="body"
-                color="grey600"
-                html={translate('text_17264811633225nhatoh524y', {
-                  planLink: `${generatePath(UPDATE_PLAN_ROUTE, {
-                    planId: subscription?.plan?.id || '',
-                  })}?origin=${REDIRECTION_ORIGIN_SUBSCRIPTION_USAGE}&subscriptionId=${subscriptionId}&customerId=${customerId}`,
-                  subscriptionLink: `${generatePath(UPDATE_SUBSCRIPTION, {
-                    subscriptionId,
-                    customerId: subscription?.customer?.id || '',
-                  })}?origin=${REDIRECTION_ORIGIN_SUBSCRIPTION_USAGE}&subscriptionId=${subscriptionId}&customerId=${customerId}`,
-                })}
-              />
-            ) : (
-              <>
-                <ChartHeader
-                  name={translate('text_1726481163322wngkv4nqfh9')}
-                  amount={intlFormatNumber(
-                    deserializeAmount(lifetimeUsage?.totalUsageAmountCents, currency),
-                    {
-                      currency,
-                      locale,
-                      currencyDisplay: locale ? 'narrowSymbol' : 'symbol',
-                    },
-                  )}
-                  blur={isBlurred}
-                  loading={isLoading}
-                />
+            <ChartHeader
+              name={translate('text_1726481163322wngkv4nqfh9')}
+              amount={intlFormatNumber(
+                deserializeAmount(lifetimeUsage?.totalUsageAmountCents, currency),
+                {
+                  currency,
+                  locale,
+                  currencyDisplay: locale ? 'narrowSymbol' : 'symbol',
+                },
+              )}
+              blur={isBlurred}
+              loading={isLoading}
+            />
 
-                <div className={tw({ 'pointer-events-none blur-sm': isBlurred })}>
+            <div className={tw({ 'pointer-events-none blur-sm': isBlurred })}>
+              <div className="flex flex-col gap-3">
+                {!!isLoading ? (
                   <div className="flex flex-col gap-3">
-                    {!!isLoading ? (
-                      <div className="flex flex-col gap-3">
-                        <Skeleton variant="text" className="mt-2 w-36" />
+                    <Skeleton variant="text" className="mt-2 w-36" />
 
-                        <div className="flex h-6 items-center gap-2">
-                          <Skeleton variant="text" className="w-[32%]" />
-                          <Skeleton variant="text" className="ml-auto w-[32%]" />
-                        </div>
-                      </div>
-                    ) : (
-                      <>
-                        <InlineBarsChart
-                          data={[
-                            {
-                              lastThresholdPercentage,
-                              nextThresholdPercentage,
-                            },
-                          ]}
-                          tooltipsData={
-                            !!nextThresholdPercentage && nextThresholdPercentage !== 0
-                              ? [
+                    <div className="flex h-6 items-center gap-2">
+                      <Skeleton variant="text" className="w-[32%]" />
+                      <Skeleton variant="text" className="ml-auto w-[32%]" />
+                    </div>
+                  </div>
+                ) : (
+                  <>
+                    <InlineBarsChart
+                      data={[
+                        {
+                          lastThresholdPercentage,
+                          nextThresholdPercentage,
+                        },
+                      ]}
+                      tooltipsData={
+                        !!nextThresholdPercentage && nextThresholdPercentage !== 0
+                          ? [
+                              {
+                                lastThresholdPercentage: translate(
+                                  'text_1726576554903s2dddtq5qz4',
                                   {
-                                    lastThresholdPercentage: translate(
-                                      'text_1726576554903s2dddtq5qz4',
-                                      {
-                                        rate: intlFormatNumber(lastThresholdPercentage / 100 || 0, {
-                                          style: 'percent',
-                                          // This is a ratio so we will only allow 2 decimal
-                                          maximumFractionDigits: 2,
-                                          locale,
-                                          currencyDisplay: locale ? 'narrowSymbol' : 'symbol',
-                                        }),
-                                      },
-                                    ),
-                                    nextThresholdPercentage: translate(
-                                      'text_1726576554903930hsa8vg23',
-                                      {
-                                        rate: intlFormatNumber(nextThresholdPercentage / 100 || 0, {
-                                          style: 'percent',
-                                          // This is a ratio so we will only allow 2 decimal
-                                          maximumFractionDigits: 2,
-                                          locale,
-                                          currencyDisplay: locale ? 'narrowSymbol' : 'symbol',
-                                        }),
-                                      },
-                                    ),
+                                    rate: intlFormatNumber(lastThresholdPercentage / 100 || 0, {
+                                      style: 'percent',
+                                      // This is a ratio so we will only allow 2 decimal
+                                      maximumFractionDigits: 2,
+                                      locale,
+                                      currencyDisplay: locale ? 'narrowSymbol' : 'symbol',
+                                    }),
                                   },
-                                ]
-                              : undefined
-                          }
-                          colors={GRAPH_COLORS}
-                        />
-                        <Stack direction={'row'} justifyContent={'space-between'}>
-                          <Typography variant="caption" color="grey600">
-                            {translate('text_17265631916994iccuwziryu', {
+                                ),
+                                nextThresholdPercentage: translate(
+                                  'text_1726576554903930hsa8vg23',
+                                  {
+                                    rate: intlFormatNumber(nextThresholdPercentage / 100 || 0, {
+                                      style: 'percent',
+                                      // This is a ratio so we will only allow 2 decimal
+                                      maximumFractionDigits: 2,
+                                      locale,
+                                      currencyDisplay: locale ? 'narrowSymbol' : 'symbol',
+                                    }),
+                                  },
+                                ),
+                              },
+                            ]
+                          : undefined
+                      }
+                      colors={GRAPH_COLORS}
+                    />
+                    <div className="flex flex-row justify-between">
+                      <Typography variant="caption" color="grey600">
+                        {translate('text_17265631916994iccuwziryu', {
+                          amount: intlFormatNumber(
+                            deserializeAmount(
+                              lifetimeUsage?.lastThresholdAmountCents || 0,
+                              currency,
+                            ),
+                            {
+                              currency,
+                              locale,
+                              currencyDisplay: locale ? 'narrowSymbol' : 'symbol',
+                            },
+                          ),
+                        })}
+                      </Typography>
+                      <Typography variant="caption" color="grey600">
+                        {!lifetimeUsage?.nextThresholdAmountCents
+                          ? translate('text_17265631917004vcwz8zc0gy')
+                          : translate('text_172656319170006dm5ta4sd8', {
                               amount: intlFormatNumber(
                                 deserializeAmount(
-                                  lifetimeUsage?.lastThresholdAmountCents || 0,
+                                  lifetimeUsage?.nextThresholdAmountCents || 0,
                                   currency,
                                 ),
                                 {
@@ -275,69 +295,40 @@ export const SubscriptionUsageLifetimeGraphComponent = ({
                                 },
                               ),
                             })}
-                          </Typography>
-                          <Typography variant="caption" color="grey600">
-                            {!lifetimeUsage?.nextThresholdAmountCents
-                              ? translate('text_17265631917004vcwz8zc0gy')
-                              : translate('text_172656319170006dm5ta4sd8', {
-                                  amount: intlFormatNumber(
-                                    deserializeAmount(
-                                      lifetimeUsage?.nextThresholdAmountCents || 0,
-                                      currency,
-                                    ),
-                                    {
-                                      currency,
-                                      locale,
-                                      currencyDisplay: locale ? 'narrowSymbol' : 'symbol',
-                                    },
-                                  ),
-                                })}
-                          </Typography>
-                        </Stack>
-                      </>
-                    )}
-                  </div>
-                </div>
-
-                {/* Non premium block */}
-                {!isLoading && !hasProgressiveBillingPremiumIntegration && (
-                  <Stack
-                    direction={'row'}
-                    gap={4}
-                    alignItems={'center'}
-                    justifyContent={'space-between'}
-                    padding={`${theme.spacing(4)} ${theme.spacing(6)}`}
-                    sx={{
-                      borderRadius: theme.spacing(2),
-                      backgroundColor: theme.palette.grey[100],
-                    }}
-                  >
-                    <Stack>
-                      <Stack direction={'row'} alignItems={'center'} gap={1}>
-                        <Typography variant="bodyHl" color="grey700">
-                          {translate('text_1724345142892pcnx5m2k3r2')}
-                        </Typography>
-                        <Icon name="sparkles" />
-                      </Stack>
-                      <Typography variant="caption" color="grey600">
-                        {translate('text_1724345142892ljzi79afhmc')}
                       </Typography>
-                    </Stack>
-                    <ButtonLink
-                      buttonProps={{
-                        variant: 'tertiary',
-                        size: 'medium',
-                        endIcon: 'sparkles',
-                      }}
-                      type="button"
-                      external
-                      to={`mailto:hello@getlago.com?subject=${translate('text_172434514289283gmf8bdhh3')}&body=${translate('text_1724346450317iqs2rtvx1tp')}`}
-                    >
-                      {translate('text_65ae73ebe3a66bec2b91d72d')}
-                    </ButtonLink>
-                  </Stack>
+                    </div>
+                  </>
                 )}
-              </>
+              </div>
+            </div>
+
+            {/* Non premium block */}
+            {!isLoading && !hasProgressiveBillingPremiumIntegration && (
+              <div className="flex flex-row items-center justify-between gap-4 rounded-sm bg-grey-100 px-6 py-4">
+                <div className="flex flex-col">
+                  <div className="flex flex-row items-center gap-1">
+                    <Typography variant="bodyHl" color="grey700">
+                      {translate('text_1724345142892pcnx5m2k3r2')}
+                    </Typography>
+                    <Icon name="sparkles" />
+                  </div>
+                  <Typography variant="caption" color="grey600">
+                    {translate('text_1724345142892ljzi79afhmc')}
+                  </Typography>
+                </div>
+                <ButtonLink
+                  buttonProps={{
+                    variant: 'tertiary',
+                    size: 'medium',
+                    endIcon: 'sparkles',
+                  }}
+                  type="button"
+                  external
+                  to={`mailto:hello@getlago.com?subject=${translate('text_172434514289283gmf8bdhh3')}&body=${translate('text_1724346450317iqs2rtvx1tp')}`}
+                >
+                  {translate('text_65ae73ebe3a66bec2b91d72d')}
+                </ButtonLink>
+              </div>
             )}
           </>
         )}
