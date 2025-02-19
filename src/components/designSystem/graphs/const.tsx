742f30108cec41d88acdd9c1743b6a5343bccc85
Alexandre Monjol <alexandre.monjol@gmail.com>
2025-02-19 15:23:11 +0100

    misc: add new MultipleLineChart component

---
+++ src/components/designSystem/graphs/const.tsx
@@ -0,0 +1,6 @@
+export const REVENUE_STREAMS_GRAPH_COLORS = {
+  subscriptionFeeAmountCents: '#4C9AFF',
+  usageBasedFeeAmountCents: '#AEA2F1',
+  commitmentFeeAmountCents: '#79F2CA',
+  oneOffFeeAmountCents: '#FFAB00',
+}
