65ba391d346b074a1673a9b1d87c83b62dbb8a38
Stefan Moraru <stefan@getlago.com>
2025-03-19 12:13:11 +0200

    feat: add variable to template / sh

--- .env.sh
+++ .env.sh
@@ -10,6 +10,7 @@ lago_oauth_proxy_url_value=$(echo $LAGO_OAUTH_PROXY_URL)
 lago_disable_signup_value=$(echo $LAGO_DISABLE_SIGNUP)
 nango_public_key=$(echo $NANGO_PUBLIC_KEY)
 sentry_dsn_value=$(echo $SENTRY_DSN)
+lago_disable_pdf_generation=$(echo $LAGO_DISABLE_PDF_GENERATION)
 
 echo "window.API_URL = \"$api_url_value\"" >> ./env-config.js
 echo "window.LAGO_DOMAIN = \"$domain_value\"" >> ./env-config.js
@@ -18,3 +19,4 @@ echo "window.LAGO_OAUTH_PROXY_URL = \"$lago_oauth_proxy_url_value\"" >> ./env-co
 echo "window.LAGO_DISABLE_SIGNUP = \"$lago_disable_signup_value\"" >> ./env-config.js
 echo "window.NANGO_PUBLIC_KEY = \"$nango_public_key\"" >> ./env-config.js
 echo "window.SENTRY_DSN = \"$sentry_dsn_value\"" >> ./env-config.js
+echo "window.LAGO_DISABLE_PDF_GENERATION = \"$lago_disable_pdf_generation\"" >> ./env-config.js
