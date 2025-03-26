a3244425adc588d1466102117b20185287a56b60
Kelly Phan <keelly.p@gmail.com>
2025-03-26 11:27:30 +0100

    refactor: migrate to tw

--- src/components/subscriptions/SubscriptionUsageTabContent.tsx
+++ src/components/subscriptions/SubscriptionUsageTabContent.tsx
@@ -1,27 +1,15 @@
 import { useParams } from 'react-router-dom'
-import styled from 'styled-components'
-
-import { theme } from '~/styles'
 
 import { SubscriptionCurrentUsageTable } from './SubscriptionCurrentUsageTable'
 import SubscriptionUsageLifetimeGraph from './SubscriptionUsageLifetimeGraph'
 
-const SubscriptionUsageTabContent = () => {
+export const SubscriptionUsageTabContent = () => {
   const { subscriptionId = '', customerId = '' } = useParams()
 
   return (
-    <Container>
+    <div className="flex flex-col gap-12 pt-8">
       <SubscriptionUsageLifetimeGraph customerId={customerId} subscriptionId={subscriptionId} />
       <SubscriptionCurrentUsageTable customerId={customerId} subscriptionId={subscriptionId} />
-    </Container>
+    </div>
   )
 }
-
-export default SubscriptionUsageTabContent
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: ${theme.spacing(12)};
-  padding-top: ${theme.spacing(8)};
-`
