1ce9eaa9e03f56c0f896d2648604cc48f44b66cb
Kelly Phan <keelly.p@gmail.com>
2025-04-14 15:31:23 +0200

    feat(lago-design-system): init package

---
+++ packages/design-system/playground/App.tsx
@@ -0,0 +1,38 @@
+import { ALL_ICONS, Icon, IconName } from '~/components'
+
+export default function App() {
+  return (
+    <div className="mx-auto max-w-screen-md p-4">
+      <div className="mb-4">Icons</div>
+      <div className="mb-6 flex flex-wrap gap-2">
+        {Object.keys(ALL_ICONS).map((iconName, i) => (
+          <Icon key={`icon-${i}`} name={iconName as IconName} />
+        ))}
+      </div>
+      <div className="mb-4">Colors</div>
+      <div className="mb-6 flex flex-wrap gap-2">
+        <Icon name="plug" color="success" />
+        <Icon name="plug" color="error" />
+        <Icon name="plug" color="warning" />
+        <Icon name="plug" color="info" />
+        <Icon name="plug" color="light" />
+        <Icon name="plug" color="dark" />
+        <Icon name="plug" color="skeleton" />
+        <Icon name="plug" color="disabled" />
+        <Icon name="plug" color="input" />
+        <Icon name="plug" color="primary" />
+      </div>
+      <div className="mb-4">Animation</div>
+      <div className="mb-6 flex flex-wrap gap-2">
+        <Icon name="processing" animation="spin" />
+        <Icon name="star-filled" animation="pulse" />
+      </div>
+      <div className="mb-4">Size</div>
+      <div className="mb-6 flex flex-wrap gap-2">
+        <Icon name="puzzle" size="small" />
+        <Icon name="puzzle" size="medium" />
+        <Icon name="puzzle" size="large" />
+      </div>
+    </div>
+  )
+}
---
+++ packages/design-system/src/components/Icon/mapping.tsx
@@ -0,0 +1,249 @@
+import Alphabet from '~/icons/alphabet.svg'
+import Apps from '~/icons/apps.svg'
+import ArrowBottom from '~/icons/arrow-bottom.svg'
+import ArrowDownCircleFilled from '~/icons/arrow-down-circle-filled.svg'
+import ArrowIndent from '~/icons/arrow-indent.svg'
+import ArrowLeftRight from '~/icons/arrow-left-right.svg'
+import ArrowLeft from '~/icons/arrow-left.svg'
+import ArrowRight from '~/icons/arrow-right.svg'
+import ArrowTop from '~/icons/arrow-top.svg'
+import ArrowUpCircleFilled from '~/icons/arrow-up-circle-filled.svg'
+import Ascending from '~/icons/ascending.svg'
+import At from '~/icons/at.svg'
+import AttachmentNa from '~/icons/attachment-na.svg'
+import Bank from '~/icons/bank.svg'
+import Bell from '~/icons/bell.svg'
+import Board from '~/icons/board.svg'
+import Book from '~/icons/book.svg'
+import Box from '~/icons/box.svg'
+import Bulb from '~/icons/bulb.svg'
+import Burger from '~/icons/burger.svg'
+import Calendar from '~/icons/calendar.svg'
+import ChartBar from '~/icons/chart-bar.svg'
+import Checkmark from '~/icons/checkmark.svg'
+import ChevronDownFilled from '~/icons/chevron-down-filled.svg'
+import ChevronDown from '~/icons/chevron-down.svg'
+import ChevronLeft from '~/icons/chevron-left.svg'
+import ChevronRightFilled from '~/icons/chevron-right-filled.svg'
+import ChevronRight from '~/icons/chevron-right.svg'
+import ChevronTop from '~/icons/chevron-top.svg'
+import ChevronUpDown from '~/icons/chevron-up-down.svg'
+import Clock from '~/icons/clock.svg'
+import CloseCircleFilled from '~/icons/close-circle-filled.svg'
+import CloseCircleUnfilled from '~/icons/close-circle-unfilled.svg'
+import Close from '~/icons/close.svg'
+import Code from '~/icons/code.svg'
+import CoinDollar from '~/icons/coin-dollar.svg'
+import Collect from '~/icons/collect.svg'
+import Command from '~/icons/command.svg'
+import Company from '~/icons/company.svg'
+import Condition from '~/icons/condition.svg'
+import ContentLeftAlign from '~/icons/content-left-align.svg'
+import Coupon from '~/icons/coupon.svg'
+import Descending from '~/icons/descending.svg'
+import Document from '~/icons/document.svg'
+import DotsHorizontal from '~/icons/dots-horizontal.svg'
+import Download from '~/icons/download.svg'
+import Duplicate from '~/icons/duplicate.svg'
+import ErrorFilled from '~/icons/error-filled.svg'
+import ErrorUnfilled from '~/icons/error-unfilled.svg'
+import EyeHidden from '~/icons/eye-hidden.svg'
+import Eye from '~/icons/eye.svg'
+import Filter from '~/icons/filter.svg'
+import FlashFilled from '~/icons/flash-filled.svg'
+import Flash from '~/icons/flash.svg'
+import Formulas from '~/icons/formulas.svg'
+import Globe from '~/icons/globe.svg'
+import Google from '~/icons/google.svg'
+import Handle from '~/icons/handle.svg'
+import Hashtag from '~/icons/hashtag.svg'
+import Heart from '~/icons/heart.svg'
+import Id from '~/icons/id.svg'
+import InfoCircle from '~/icons/info-circle.svg'
+import Key from '~/icons/key.svg'
+import KeyboardEnter from '~/icons/keyboard-enter.svg'
+import Laptop from '~/icons/laptop.svg'
+import Link from '~/icons/link.svg'
+import Lock from '~/icons/lock.svg'
+import Logout from '~/icons/log-out.svg'
+import MagnifyingGlass from '~/icons/magnifying-glass.svg'
+import Mail from '~/icons/mail.svg'
+import Map from '~/icons/map.svg'
+import Micro from '~/icons/micro.svg'
+import Minus from '~/icons/minus.svg'
+import Okta from '~/icons/okta.svg'
+import Outside from '~/icons/outside.svg'
+import Paperclip from '~/icons/paperclip.svg'
+import PartiallyFilled from '~/icons/partially-filled.svg'
+import PauseCircleFilled from '~/icons/pause-circle-filled.svg'
+import PauseCircleUnfilled from '~/icons/pause-circle-unfilled.svg'
+import Pen from '~/icons/pen.svg'
+import Percentage from '~/icons/percentage.svg'
+import Pivot from '~/icons/pivot.svg'
+import Play from '~/icons/play.svg'
+import Plug from '~/icons/plug.svg'
+import Plus from '~/icons/plus.svg'
+import Processing from '~/icons/processing.svg'
+import Pulse from '~/icons/pulse.svg'
+import Push from '~/icons/push.svg'
+import Puzzle from '~/icons/puzzle.svg'
+import QuestionCircle from '~/icons/question-circle.svg'
+import Receipt from '~/icons/receipt.svg'
+import Reload from '~/icons/reload.svg'
+import Requested from '~/icons/requested.svg'
+import ResizeExpand from '~/icons/resize-expand.svg'
+import ResizeReduce from '~/icons/resize-reduce.svg'
+import Rocket from '~/icons/rocket.svg'
+import Rotate from '~/icons/rotate.svg'
+import Schema from '~/icons/schema.svg'
+import Scissor from '~/icons/scissor.svg'
+import Settings from '~/icons/settings.svg'
+import Share from '~/icons/share.svg'
+import Smartphone from '~/icons/smartphone.svg'
+import Sparkles from '~/icons/sparkles.svg'
+import StarFilled from '~/icons/star-filled.svg'
+import StarOutlinedHidden from '~/icons/star-outlined-hidden.svg'
+import Stop from '~/icons/stop.svg'
+import SwitchOff from '~/icons/switch-off.svg'
+import Switch from '~/icons/switch.svg'
+import Sync from '~/icons/sync.svg'
+import TableHorizontale from '~/icons/table-horizontale.svg'
+import Table from '~/icons/table.svg'
+import Target from '~/icons/target.svg'
+import Text from '~/icons/text.svg'
+import Trash from '~/icons/trash.svg'
+import Unlock from '~/icons/unlock.svg'
+import UserAdd from '~/icons/user-add.svg'
+import UserMultiple from '~/icons/user-multiple.svg'
+import User from '~/icons/user.svg'
+import ValidateFilled from '~/icons/validate-filled.svg'
+import ValidateUnfilled from '~/icons/validate-unfilled.svg'
+import Wallet from '~/icons/wallet.svg'
+import WarningFilled from '~/icons/warning-filled.svg'
+import WarningUnfilled from '~/icons/warning-unfilled.svg'
+
+export const ALL_ICONS = {
+  at: At,
+  alphabet: Alphabet,
+  apps: Apps,
+  'arrow-bottom': ArrowBottom,
+  'arrow-left': ArrowLeft,
+  'arrow-left-right': ArrowLeftRight,
+  'arrow-right': ArrowRight,
+  'arrow-top': ArrowTop,
+  'arrow-up-circle-filled': ArrowUpCircleFilled,
+  'arrow-down-circle-filled': ArrowDownCircleFilled,
+  'arrow-indent': ArrowIndent,
+  ascending: Ascending,
+  'attachment-na': AttachmentNa,
+  bank: Bank,
+  bell: Bell,
+  board: Board,
+  book: Book,
+  bulb: Bulb,
+  burger: Burger,
+  box: Box,
+  calendar: Calendar,
+  'chart-bar': ChartBar,
+  checkmark: Checkmark,
+  'chevron-down': ChevronDown,
+  'chevron-down-filled': ChevronDownFilled,
+  'chevron-left': ChevronLeft,
+  'chevron-right': ChevronRight,
+  'chevron-right-filled': ChevronRightFilled,
+  'chevron-top': ChevronTop,
+  'chevron-up-down': ChevronUpDown,
+  clock: Clock,
+  'close-circle-filled': CloseCircleFilled,
+  'close-circle-unfilled': CloseCircleUnfilled,
+  close: Close,
+  code: Code,
+  'coin-dollar': CoinDollar,
+  collect: Collect,
+  command: Command,
+  company: Company,
+  condition: Condition,
+  'content-left-align': ContentLeftAlign,
+  coupon: Coupon,
+  descending: Descending,
+  document: Document,
+  'dots-horizontal': DotsHorizontal,
+  download: Download,
+  duplicate: Duplicate,
+  'error-filled': ErrorFilled,
+  'error-unfilled': ErrorUnfilled,
+  'eye-hidden': EyeHidden,
+  eye: Eye,
+  filter: Filter,
+  flash: Flash,
+  'flash-filled': FlashFilled,
+  formulas: Formulas,
+  globe: Globe,
+  google: Google,
+  handle: Handle,
+  hashtag: Hashtag,
+  heart: Heart,
+  id: Id,
+  'info-circle': InfoCircle,
+  'keyboard-enter': KeyboardEnter,
+  'magnifying-glass': MagnifyingGlass,
+  key: Key,
+  laptop: Laptop,
+  link: Link,
+  lock: Lock,
+  logout: Logout,
+  mail: Mail,
+  map: Map,
+  micro: Micro,
+  minus: Minus,
+  okta: Okta,
+  outside: Outside,
+  paperclip: Paperclip,
+  'partially-filled': PartiallyFilled,
+  'pause-circle-filled': PauseCircleFilled,
+  'pause-circle-unfilled': PauseCircleUnfilled,
+  pen: Pen,
+  percentage: Percentage,
+  pivot: Pivot,
+  play: Play,
+  plug: Plug,
+  plus: Plus,
+  processing: Processing,
+  pulse: Pulse,
+  push: Push,
+  puzzle: Puzzle,
+  'question-circle': QuestionCircle,
+  receipt: Receipt,
+  reload: Reload,
+  requested: Requested,
+  'resize-expand': ResizeExpand,
+  'resize-reduce': ResizeReduce,
+  rocket: Rocket,
+  rotate: Rotate,
+  scissor: Scissor,
+  schema: Schema,
+  settings: Settings,
+  share: Share,
+  smartphone: Smartphone,
+  sparkles: Sparkles,
+  'star-filled': StarFilled,
+  'star-outlined-hidden': StarOutlinedHidden,
+  stop: Stop,
+  switch: Switch,
+  'switch-off': SwitchOff,
+  sync: Sync,
+  table: Table,
+  'table-horizontale': TableHorizontale,
+  target: Target,
+  text: Text,
+  trash: Trash,
+  unlock: Unlock,
+  'user-add': UserAdd,
+  user: User,
+  'user-multiple': UserMultiple,
+  'validate-filled': ValidateFilled,
+  'validate-unfilled': ValidateUnfilled,
+  wallet: Wallet,
+  'warning-filled': WarningFilled,
+  'warning-unfilled': WarningUnfilled,
+}
