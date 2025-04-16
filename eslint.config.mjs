c13288122e57d48a26f0503e5eaacdfe4994f5b3
Kelly Phan <keelly.p@gmail.com>
2025-04-16 11:47:47 +0200

    chore: bump eslint (#2126)
    * chore: bump eslint deps
    
    * feat: whitelist eslint folder

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,13 @@
+import { defineConfig } from 'eslint/config'
 import config from 'lago-configs/eslint'
 
-export default config
+export default defineConfig([
+  {
+    files: [
+      'src/**/*.{js,ts,jsx,tsx}',
+      'scripts/**/*.{js,ts,jsx,tsx}',
+      'cypress/**/*.{js,ts,jsx,tsx}',
+    ],
+    extends: [config],
+  },
+])
