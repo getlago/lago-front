410c357ec3b88b5c4ba9d611f583e75663bceb3f
Kelly Phan <keelly.p@gmail.com>
2025-04-16 18:04:19 +0200

    refactor: update playground

--- packages/design-system/playground/App.tsx
+++ packages/design-system/playground/App.tsx
@@ -1,37 +1,10 @@
-import { ALL_ICONS, Icon, IconName } from '~/components'
+import { IconsSection } from './components/IconsSection'
 
 export default function App() {
   return (
-    <div className="mx-auto max-w-screen-md p-4">
-      <div className="mb-4">Icons</div>
-      <div className="mb-6 flex flex-wrap gap-2">
-        {Object.keys(ALL_ICONS).map((iconName) => (
-          <Icon key={iconName} name={iconName as IconName} />
-        ))}
-      </div>
-      <div className="mb-4">Colors</div>
-      <div className="mb-6 flex flex-wrap gap-2">
-        <Icon name="plug" color="success" />
-        <Icon name="plug" color="error" />
-        <Icon name="plug" color="warning" />
-        <Icon name="plug" color="info" />
-        <Icon name="plug" color="light" />
-        <Icon name="plug" color="dark" />
-        <Icon name="plug" color="skeleton" />
-        <Icon name="plug" color="disabled" />
-        <Icon name="plug" color="input" />
-        <Icon name="plug" color="primary" />
-      </div>
-      <div className="mb-4">Animation</div>
-      <div className="mb-6 flex flex-wrap gap-2">
-        <Icon name="processing" animation="spin" />
-        <Icon name="star-filled" animation="pulse" />
-      </div>
-      <div className="mb-4">Size</div>
-      <div className="mb-6 flex flex-wrap gap-2">
-        <Icon name="puzzle" size="small" />
-        <Icon name="puzzle" size="medium" />
-        <Icon name="puzzle" size="large" />
+    <div className="min-h-screen bg-grey-100 pb-20 pt-8">
+      <div className="mx-auto flex max-w-screen-lg flex-col gap-20">
+        <IconsSection />
       </div>
     </div>
   )
--- packages/design-system/playground/main.tsx
+++ packages/design-system/playground/main.tsx
@@ -1,6 +1,8 @@
+import { StyledEngineProvider, ThemeProvider } from '@mui/material'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { theme } from '~/lib'
 import '~/style.css'
 
 import App from './App'
@@ -8,6 +10,10 @@ import App from './App'
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </StyledEngineProvider>
   </React.StrictMode>,
 )
