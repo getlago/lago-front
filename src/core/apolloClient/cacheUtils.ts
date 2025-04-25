1bad213c5361a37c2fb70292822fa9ccac10d65a
Alexandre Monjol <alexandre.monjol@gmail.com>
2025-04-25 11:20:59 +0200

    misc: fix some linter warning

--- src/core/apolloClient/cacheUtils.ts
+++ src/core/apolloClient/cacheUtils.ts
@@ -88,7 +88,7 @@ export const onAccessCustomerPortal = (token?: string) => {
 const omitDeepArrayWalk = (arr: Array<unknown>, key: string): unknown => {
   return arr.map((val) => {
     if (Array.isArray(val)) return omitDeepArrayWalk(val, key)
-    // @ts-expect-error
+    // @ts-expect-error: val could be null which would cause type error when passing to omitDeep
     else if (typeof val === 'object') return omitDeep(val, key)
     return val
   })
@@ -105,7 +105,7 @@ export const omitDeep = (obj: Record<string | number, unknown>, key: string) => 
       if (val instanceof Date) newObj[i] = val
       else if (Array.isArray(val)) newObj[i] = omitDeepArrayWalk(val, key)
       else if (typeof val === 'object' && val !== null)
-        // @ts-expect-error
+        // @ts-expect-error: val could be any object type which would cause type error when passing to omitDeep
         newObj[i] = omitDeep(val, key)
       else newObj[i] = val
     }
