b90892a6bae0fbd6dac65fc599a20334d6bafa88
Kelly Phan <keelly.p@gmail.com>
2025-04-30 17:13:42 +0200

    feat: improve console

--- src/components/developers/DevtoolsView.tsx
+++ src/components/developers/DevtoolsView.tsx
@@ -1,9 +1,13 @@
 import { FC, useRef, useState } from 'react'
 import { ImperativePanelHandle, Panel, PanelResizeHandle } from 'react-resizable-panels'
+import { useLocation } from 'react-router-dom'
 
 import { Button, NavigationTab, TabManagedBy, Tooltip } from '~/components/designSystem'
+import { devToolsNavigationMapping, DevtoolsRouter } from '~/components/developers/DevtoolsRouter'
+import { copyToClipboard } from '~/core/utils/copyToClipboard'
 import { useInternationalization } from '~/hooks/core/useInternationalization'
-import { useDeveloperTool } from '~/hooks/useDeveloperTool'
+import { DEVTOOL_TAB_PARAMS, useDeveloperTool } from '~/hooks/useDeveloperTool'
+import { usePermissions } from '~/hooks/usePermissions'
 
 const MAX_RESIZABLE_HEIGHT = 88
 const MIN_RESIZABLE_HEIGHT = 20
@@ -14,7 +18,9 @@ export const DevtoolsView: FC = () => {
   const { isOpen, close } = useDeveloperTool()
   const [isFullscreen, setIsFullscreen] = useState(false)
   const { translate } = useInternationalization()
+  const { pathname } = useLocation()
   const panel = useRef<ImperativePanelHandle>(null)
+  const { hasPermissions } = usePermissions()
 
   const expandPanel = () => {
     let isLocalFullscreen = false
@@ -27,7 +33,6 @@ export const DevtoolsView: FC = () => {
       isLocalFullscreen = true
       height = FULLSCREEN
     }
-
     setIsFullscreen(isLocalFullscreen)
     requestAnimationFrame(() => {
       panel.current?.resize(height)
@@ -37,6 +42,14 @@ export const DevtoolsView: FC = () => {
   const closePanel = () => {
     setIsFullscreen(false)
     close()
+  }
+
+  const copyInspectorLink = () => {
+    const url = new URL(window.location.href)
+    const encodedPathname = encodeURIComponent(pathname)
+
+    url.searchParams.set(DEVTOOL_TAB_PARAMS, encodedPathname)
+    copyToClipboard(url.toString())
   }
 
   if (!isOpen) return null
@@ -53,6 +66,8 @@ export const DevtoolsView: FC = () => {
         <div className="mx-auto h-2 w-20 translate-y-2 rounded-full bg-grey-300" />
       </PanelResizeHandle>
       <Panel
+        id="devtools-panel"
+        order={2}
         ref={panel}
         defaultSize={DEFAULT_RESIZABLE_HEIGHT}
         minSize={MIN_RESIZABLE_HEIGHT}
@@ -68,41 +83,39 @@ export const DevtoolsView: FC = () => {
       >
         <div className="relative w-full">
           <NavigationTab
-            managedBy={TabManagedBy.INDEX}
-            className="px-4"
-            tabs={[
-              {
-                title: 'API keys & ID',
-                component: <div>API keys & ID</div>,
-              },
-              {
-                title: 'Webhooks',
-                component: <div>Webhooks</div>,
-              },
-              {
-                title: 'Events',
-                component: <div>Events</div>,
-              },
-            ]}
-          />
-          <div className="absolute right-4 top-[14px] flex gap-3">
-            <Tooltip
-              title={translate(
-                isFullscreen ? 'text_1746019984781u1ftea09d0b' : 'text_1746019984781hsxx9jjjska',
-              )}
-              placement="top"
-            >
+            name="devtools"
+            managedBy={TabManagedBy.URL}
+            className="sticky top-0 z-navBar bg-white px-4"
+            tabs={devToolsNavigationMapping(translate, hasPermissions)}
+          >
+            <div className="ml-auto flex items-center gap-3">
               <Button
+                startIcon="link"
                 size="small"
-                icon={isFullscreen ? 'resize-reduce' : 'resize-expand'}
                 variant="quaternary"
-                onClick={expandPanel}
-              />
-            </Tooltip>
-            <Tooltip title={translate('text_62f50d26c989ab03196884ae')} placement="top">
-              <Button size="small" icon="close" variant="quaternary" onClick={closePanel} />
-            </Tooltip>
-          </div>
+                onClick={() => copyInspectorLink()}
+              >
+                {translate('text_17460208605597iyd249v26z')}
+              </Button>
+              <Tooltip
+                title={translate(
+                  isFullscreen ? 'text_1746019984781u1ftea09d0b' : 'text_1746019984781hsxx9jjjska',
+                )}
+                placement="top"
+              >
+                <Button
+                  size="small"
+                  icon={isFullscreen ? 'resize-reduce' : 'resize-expand'}
+                  variant="quaternary"
+                  onClick={expandPanel}
+                />
+              </Tooltip>
+              <Tooltip title={translate('text_62f50d26c989ab03196884ae')} placement="top">
+                <Button size="small" icon="close" variant="quaternary" onClick={closePanel} />
+              </Tooltip>
+            </div>
+          </NavigationTab>
+          <DevtoolsRouter />
         </div>
       </Panel>
     </>
