3600437a1d88498f0aff4c997b75ee5946886c93
Kelly Phan <keelly.p@gmail.com>
2025-04-30 15:43:25 +0200

    feat: init new console
    fix: remove ff

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import { loadDevMessages, loadErrorMessages } from '@apollo/client/dev'
 import { StyledEngineProvider, ThemeProvider } from '@mui/material'
 import { DesignSystemProvider } from 'lago-design-system'
 import { useEffect, useState } from 'react'
+import { Panel, PanelGroup } from 'react-resizable-panels'
 import { BrowserRouter } from 'react-router-dom'
 
 import { ToastContainer } from '~/components/designSystem/Toasts'
+import { DevtoolsView } from '~/components/developers/DevtoolsView'
 import { ErrorBoundary } from '~/components/ErrorBoundary'
 import { RouteWrapper } from '~/components/RouteWrapper'
 import { UserIdentifier } from '~/components/UserIdentifier'
 import { envGlobalVar, initializeApolloClient, initializeTranslations } from '~/core/apolloClient'
 import { AppEnvEnum } from '~/core/constants/globalTypes'
 import { initializeYup } from '~/formValidation/initializeYup'
+import { DeveloperToolProvider } from '~/hooks/useDeveloperTool'
 import { theme } from '~/styles'
 
 const App = () => {
@@ -45,7 +48,15 @@ const App = () => {
           <ThemeProvider theme={theme}>
             <DesignSystemProvider>
               <ErrorBoundary>
-                <RouteWrapper />
+                <DeveloperToolProvider>
+                  <PanelGroup direction="vertical" autoSaveId="devtools">
+                    <Panel id="app-panel" order={1}>
+                      <RouteWrapper />
+                    </Panel>
+
+                    <DevtoolsView />
+                  </PanelGroup>
+                </DeveloperToolProvider>
               </ErrorBoundary>
               <UserIdentifier />
               <ToastContainer />
