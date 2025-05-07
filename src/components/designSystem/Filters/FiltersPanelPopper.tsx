74501f5df4d979fb7cc06cb77728c32460aaedfb
Kelly Phan <keelly.p@gmail.com>
2025-05-07 16:36:36 +0200

    feat: migrate webhook single log

--- src/components/designSystem/Filters/FiltersPanelPopper.tsx
+++ src/components/designSystem/Filters/FiltersPanelPopper.tsx
@@ -12,7 +12,7 @@ import { FiltersPanelItemTypeSwitch } from './FiltersPanelItemTypeSwitch'
 import { AvailableFiltersEnum, FiltersFormValues, mapFilterToTranslationKey } from './types'
 import { useFilters } from './useFilters'
 
-export const FiltersPanelPopper = ({ displayInDialog }: { displayInDialog?: boolean }) => {
+export const FiltersPanelPopper = () => {
   const { translate } = useInternationalization()
   const {
     availableFilters,
