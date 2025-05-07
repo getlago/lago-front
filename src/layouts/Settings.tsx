ebb042556e18cc423d1349f9dc19c7f45366e595
Stefan Moraru <stefan@getlago.com>
2025-05-07 17:34:27 +0300

    redo after rebase

--- src/layouts/Settings.tsx
+++ src/layouts/Settings.tsx
@@ -1,41 +1,105 @@
 import { ClickAwayListener } from '@mui/material'
+import { ButtonLink, Icon, Skeleton } from 'lago-design-system'
 import { useEffect, useRef, useState } from 'react'
-import { generatePath, Location, Outlet, useLocation } from 'react-router-dom'
+import { generatePath, Location, Outlet, useLocation, useParams } from 'react-router-dom'
 
 import { Button, Typography, VerticalMenu } from '~/components/designSystem'
+import { PremiumWarningDialog, PremiumWarningDialogRef } from '~/components/PremiumWarningDialog'
 import { IntegrationsTabsOptionsEnum } from '~/core/constants/tabsOptions'
 import {
   AUTHENTICATION_ROUTE,
+  BILLING_ENTITY_CREATE_ROUTE,
+  BILLING_ENTITY_ROUTE,
+  BILLING_ENTITY_UPDATE_ROUTE,
   CREATE_DUNNING_ROUTE,
   CREATE_INVOICE_CUSTOM_SECTION,
   CREATE_TAX_ROUTE,
   DUNNINGS_SETTINGS_ROUTE,
   EDIT_INVOICE_CUSTOM_SECTION,
-  EMAILS_SETTINGS_ROUTE,
+  FULL_INTEGRATIONS_ROUTE,
+  FULL_INTEGRATIONS_ROUTE_ID,
   HOME_ROUTE,
   INTEGRATIONS_ROUTE,
   INVOICE_SETTINGS_ROUTE,
   MEMBERS_ROUTE,
-  ORGANIZATION_INFORMATIONS_ROUTE,
   settingRoutes,
-  SETTINGS_ROUTE,
   TAXES_SETTINGS_ROUTE,
   UPDATE_DUNNING_ROUTE,
   UPDATE_TAX_ROUTE,
 } from '~/core/router'
-import { useInternationalization } from '~/hooks/core/useInternationalization'
+import { useGetBillingEntitiesQuery } from '~/generated/graphql'
+import { TranslateFunc, useInternationalization } from '~/hooks/core/useInternationalization'
 import { useLocationHistory } from '~/hooks/core/useLocationHistory'
-import { usePermissions } from '~/hooks/usePermissions'
+import { useOrganizationInfos } from '~/hooks/useOrganizationInfos'
+import { TMembershipPermissions, usePermissions } from '~/hooks/usePermissions'
 import { tw } from '~/styles/utils'
+
+const generateTabs = ({
+  translate,
+  hasPermissions,
+}: {
+  translate: TranslateFunc
+  hasPermissions: (permissionsToCheck: Array<keyof TMembershipPermissions>) => boolean
+}) => [
+  {
+    title: translate('text_664c732c264d7eed1c74fd96'),
+    link: AUTHENTICATION_ROUTE,
+    hidden: !hasPermissions(['organizationIntegrationsView']),
+  },
+  {
+    title: translate('text_62b1edddbf5f461ab9712733'),
+    link: generatePath(INTEGRATIONS_ROUTE, {
+      integrationGroup: IntegrationsTabsOptionsEnum.Lago,
+    }),
+    match: [FULL_INTEGRATIONS_ROUTE, FULL_INTEGRATIONS_ROUTE_ID],
+    hidden: !hasPermissions(['organizationIntegrationsView']),
+  },
+  {
+    title: translate('text_63208b630aaf8df6bbfb2655'),
+    link: MEMBERS_ROUTE,
+    hidden: !hasPermissions(['organizationMembersView']),
+  },
+  {
+    title: translate('text_1742230191028qkwly181six'),
+    link: INVOICE_SETTINGS_ROUTE,
+    hidden: !hasPermissions(['organizationInvoicesView']),
+  },
+  {
+    title: translate('text_17285747264958mqbtws3em8'),
+    link: DUNNINGS_SETTINGS_ROUTE,
+    match: [DUNNINGS_SETTINGS_ROUTE],
+    hidden: !hasPermissions(['dunningCampaignsView']),
+  },
+  {
+    title: translate('text_645bb193927b375079d28a8f'),
+    link: TAXES_SETTINGS_ROUTE,
+    match: [TAXES_SETTINGS_ROUTE],
+    hidden: !hasPermissions(['organizationTaxesView']),
+  },
+]
+
+const isEntityActive = (code: string, current: string) => code === current
 
 const Settings = () => {
   const location = useLocation()
   const { translate } = useInternationalization()
   const { goBack } = useLocationHistory()
   const { hasPermissions } = usePermissions()
+  const { billingEntityCode } = useParams()
+
+  const { organization: { canCreateBillingEntity } = {} } = useOrganizationInfos()
   const contentRef = useRef<HTMLDivElement>(null)
 
   const [open, setOpen] = useState(false)
+
+  const { data: billingEntities, loading: billingEntitiesLoading } = useGetBillingEntitiesQuery({})
+
+  const premiumWarningDialogRef = useRef<PremiumWarningDialogRef>(null)
+
+  const TABS_ORGANIZATION = generateTabs({
+    translate,
+    hasPermissions,
+  })
 
   const { pathname, state } = location as Location & { state: { disableScrollTop?: boolean } }
 
@@ -63,6 +127,8 @@ const Settings = () => {
       UPDATE_DUNNING_ROUTE,
       CREATE_INVOICE_CUSTOM_SECTION,
       EDIT_INVOICE_CUSTOM_SECTION,
+      BILLING_ENTITY_CREATE_ROUTE,
+      BILLING_ENTITY_UPDATE_ROUTE,
     ],
   )
 
@@ -99,68 +165,91 @@ const Settings = () => {
                   })
                 }}
               >
-                <Typography variant="body" color="textSecondary" noWrap>
+                <Typography className="text-nowrap text-grey-600">
                   {translate('text_65df4fc6314ffd006ce0a537')}
                 </Typography>
               </Button>
             </div>
 
-            <VerticalMenu
-              onClick={() => {
-                setOpen(false)
-              }}
-              tabs={[
-                {
-                  title: translate('text_62ab2d0396dd6b0361614d1c'),
-                  link: ORGANIZATION_INFORMATIONS_ROUTE,
-                  match: [ORGANIZATION_INFORMATIONS_ROUTE, SETTINGS_ROUTE],
-                  hidden: !hasPermissions(['organizationView']),
-                },
-                {
-                  title: translate('text_62bb10ad2a10bd182d00202d'),
-                  link: INVOICE_SETTINGS_ROUTE,
-                  match: [INVOICE_SETTINGS_ROUTE],
-                  hidden: !hasPermissions(['organizationInvoicesView']),
-                },
-                {
-                  title: translate('text_645bb193927b375079d28a8f'),
-                  link: TAXES_SETTINGS_ROUTE,
-                  match: [TAXES_SETTINGS_ROUTE],
-                  hidden: !hasPermissions(['organizationTaxesView']),
-                },
-                {
-                  title: translate('text_17285747264958mqbtws3em8'),
-                  link: DUNNINGS_SETTINGS_ROUTE,
-                  match: [DUNNINGS_SETTINGS_ROUTE],
-                  hidden: !hasPermissions(['dunningCampaignsView']),
-                },
-                {
-                  title: translate('text_6407684eaf41130074c4b2a1'),
-                  link: EMAILS_SETTINGS_ROUTE,
-                  hidden: !hasPermissions(['organizationEmailsView']),
-                },
-                {
-                  title: translate('text_62b1edddbf5f461ab9712733'),
-                  link: generatePath(INTEGRATIONS_ROUTE, {
-                    integrationGroup: IntegrationsTabsOptionsEnum.Lago,
-                  }),
-                  hidden: !hasPermissions(['organizationIntegrationsView']),
-                },
-                {
-                  title: translate('text_664c732c264d7eed1c74fd96'),
-                  link: AUTHENTICATION_ROUTE,
-                  hidden: !hasPermissions(['organizationIntegrationsView']),
-                },
-                {
-                  title: translate('text_63208b630aaf8df6bbfb2655'),
-                  link: MEMBERS_ROUTE,
-                  hidden: !hasPermissions(['organizationMembersView']),
-                },
-              ]}
-            />
+            <div>
+              <div className="mb-1 flex items-center gap-2 px-3 py-1">
+                <Icon name="company" size="small" />
+
+                <Typography className="text-sm font-medium text-grey-600">
+                  {translate('text_1742230191028y9ffl7i1dhe')}
+                </Typography>
+              </div>
+
+              <div className="flex flex-col gap-1">
+                {billingEntitiesLoading && <Skeleton className="w-full px-3" variant="text" />}
+
+                {!billingEntitiesLoading &&
+                  billingEntities?.billingEntities?.collection?.map((entity, index) => (
+                    <ButtonLink
+                      key={`${index}-${entity.code}`}
+                      title={entity.name}
+                      to={generatePath(BILLING_ENTITY_ROUTE, {
+                        billingEntityCode: entity.code,
+                      })}
+                      type="tab"
+                      active={isEntityActive(entity.code, billingEntityCode || '')}
+                      canBeClickedOnActive={true}
+                    >
+                      <div className="flex w-full flex-row items-center justify-between">
+                        <div className="flex items-baseline gap-2">
+                          <Typography variant="body" color="inherit" noWrap>
+                            {entity.name}
+                          </Typography>
+                        </div>
+                      </div>
+                    </ButtonLink>
+                  ))}
+
+                <ButtonLink
+                  title={translate('text_1742367266660p3a701mnvli')}
+                  to={canCreateBillingEntity ? generatePath(BILLING_ENTITY_CREATE_ROUTE) : '#'}
+                  type="tab"
+                  onClick={() =>
+                    !canCreateBillingEntity && premiumWarningDialogRef.current?.openDialog()
+                  }
+                >
+                  <div className="flex w-full flex-row items-center justify-between">
+                    <div className="flex items-baseline gap-2">
+                      <Icon name="plus" size="small" />
+
+                      <Typography variant="body" color="inherit" noWrap>
+                        {translate('text_1742367266660p3a701mnvli')}
+                      </Typography>
+                    </div>
+
+                    {!canCreateBillingEntity && <Icon name="sparkles" />}
+                  </div>
+                </ButtonLink>
+              </div>
+            </div>
+
+            <div>
+              <div className="mb-1 mt-4 flex items-center gap-2 px-3 py-1">
+                <Icon name="globe" size="small" />
+
+                <Typography className="text-sm font-medium text-grey-600">
+                  {translate('text_1742230191028ts64cxrgwdj')}
+                </Typography>
+              </div>
+
+              <VerticalMenu
+                onClick={() => {
+                  setOpen(false)
+                }}
+                tabs={TABS_ORGANIZATION}
+              />
+            </div>
           </div>
         </nav>
       </ClickAwayListener>
+
+      <PremiumWarningDialog ref={premiumWarningDialogRef} />
+
       <div className="flex-1 overflow-y-auto" ref={contentRef}>
         <Outlet />
       </div>
