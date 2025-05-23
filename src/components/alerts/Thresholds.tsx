47a650813d1d75e723e74cddbd2973ab950574e4
Alexandre Monjol <alexandre.monjol@gmail.com>
2025-05-23 11:34:15 +0200

    feat: complete alert form with thresholds

---
+++ src/components/alerts/Thresholds.tsx
@@ -0,0 +1,263 @@
+import { InputAdornment } from '@mui/material'
+import { Button, Tooltip, Typography } from 'lago-design-system'
+import { useMemo } from 'react'
+
+import { ChargeTable } from '~/components/designSystem'
+import { AmountInput, Switch, TextInput } from '~/components/form'
+import { getCurrencySymbol } from '~/core/formats/intlFormatNumber'
+import { CurrencyEnum, ThresholdInput } from '~/generated/graphql'
+import { useInternationalization } from '~/hooks/core/useInternationalization'
+
+export const isThresholdValueValid = (
+  index: number,
+  value: string,
+  previousThreshold: ThresholdInput[],
+) => {
+  return index > 0 && value !== '' && Number(value) <= Number(previousThreshold[index - 1]?.value)
+}
+
+const AlertThresholds = ({
+  thresholds,
+  setThresholds,
+  setThresholdValue,
+  currency,
+}: {
+  thresholds: ThresholdInput[]
+  setThresholds: (thresholds: ThresholdInput[]) => void
+  setThresholdValue: ({
+    index,
+    key,
+    newValue,
+  }: {
+    index: number
+    key: keyof ThresholdInput
+    newValue: unknown
+  }) => void
+  currency: CurrencyEnum
+}) => {
+  const { translate } = useInternationalization()
+
+  const recurringIndex = thresholds.findIndex((t) => t.recurring)
+  const hasRecurringThreshold = recurringIndex !== -1
+
+  const addThreshold = ({ recurring = false }: { recurring?: boolean } = {}) => {
+    const thresholdsToUpdate = [...thresholds]
+    const newThreshold: ThresholdInput = {
+      code: '',
+      value: '',
+      recurring,
+    }
+
+    if (!recurring && hasRecurringThreshold && recurringIndex > 0) {
+      thresholdsToUpdate.splice(recurringIndex, 0, newThreshold)
+    } else {
+      thresholdsToUpdate.push(newThreshold)
+    }
+
+    setThresholds(thresholdsToUpdate)
+  }
+
+  const deleteThreshold = (index: number) => {
+    const newThresholds = [...thresholds].filter((_, i) => i !== index)
+
+    setThresholds(newThresholds)
+  }
+
+  const onSwitchClick = () => {
+    if (!hasRecurringThreshold) {
+      addThreshold({ recurring: true })
+    } else {
+      if (recurringIndex !== -1) {
+        deleteThreshold(recurringIndex)
+      }
+    }
+  }
+
+  const nonRecurringThresholds = useMemo(
+    () => thresholds.filter((threshold) => !threshold.recurring),
+    [thresholds],
+  )
+
+  return (
+    <div className="flex flex-col gap-6">
+      <div className="flex flex-col">
+        <Button
+          className="mb-2 ml-auto"
+          startIcon="plus"
+          variant="quaternary"
+          onClick={() => addThreshold()}
+          data-test="add-new-non-recurring-threshold-button"
+        >
+          {translate('text_1724233213997l2ksi40t8q6')}
+        </Button>
+        <div className="-mx-4 -my-1 overflow-auto px-4 py-1">
+          <ChargeTable
+            name="graduated-percentage-charge-table"
+            data={nonRecurringThresholds.map((localData, i) => ({
+              ...localData,
+              disabledDelete: i === 0,
+            }))}
+            onDeleteRow={(_, i) => {
+              // Recurring threshold will always be at the end of the array
+              // So we can use the index to delete the non-recurring threshold
+              deleteThreshold(i)
+            }}
+            deleteTooltipContent={translate('text_17242522324608198c2vblmw')}
+            columns={[
+              {
+                size: 175,
+                content: (_, i) => (
+                  <Typography className="px-4" variant="captionHl" noWrap>
+                    {translate(
+                      i === 0 ? 'text_1747921119080dk04aab1ecy' : 'text_1724179887723917j8ezkd9v',
+                    )}
+                  </Typography>
+                ),
+              },
+              {
+                size: 250,
+                title: (
+                  <Typography className="px-4" variant="captionHl">
+                    {translate('text_1724179887723eh12a0kqbdw')}
+                  </Typography>
+                ),
+                content: (row, i) => {
+                  const shouldDisplayError = isThresholdValueValid(
+                    i,
+                    row.value,
+                    nonRecurringThresholds,
+                  )
+
+                  return (
+                    <Tooltip
+                      placement="top"
+                      title={translate('text_1724252232460i4tv7384iiy', {
+                        value: nonRecurringThresholds[i - 1]?.value,
+                      })}
+                      disableHoverListener={!shouldDisplayError}
+                    >
+                      <AmountInput
+                        variant="outlined"
+                        error={shouldDisplayError}
+                        beforeChangeFormatter={['positiveNumber']}
+                        currency={currency}
+                        value={row.value}
+                        onChange={(value) => {
+                          setThresholdValue({
+                            index: i,
+                            key: 'value',
+                            newValue: String(value) || undefined,
+                          })
+                        }}
+                        InputProps={{
+                          startAdornment: (
+                            <InputAdornment position="start">
+                              {getCurrencySymbol(currency)}
+                            </InputAdornment>
+                          ),
+                        }}
+                      />
+                    </Tooltip>
+                  )
+                },
+              },
+              {
+                size: 250,
+                title: (
+                  <Typography className="px-4" variant="captionHl">
+                    {translate('text_1748334091887k4p2p41jdou')}
+                  </Typography>
+                ),
+                content: (row, i) => (
+                  <TextInput
+                    variant="outlined"
+                    placeholder={translate('text_1747921154885jqsl0c9xhro')}
+                    value={row.code ?? ''}
+                    onChange={(value) => {
+                      setThresholdValue({
+                        index: i,
+                        key: 'code',
+                        newValue: value === '' ? undefined : value,
+                      })
+                    }}
+                  />
+                ),
+              },
+            ]}
+          />
+        </div>
+      </div>
+      <Switch
+        name="recurring-threshold"
+        className="w-fit cursor-pointer"
+        data-test="add-new-recurring-threshold-switch"
+        checked={hasRecurringThreshold}
+        onChange={onSwitchClick}
+        label={translate('text_1724234174945ztq15pvmty3')}
+        subLabel={translate('text_172423417494563qf45qet2d')}
+      />
+      {hasRecurringThreshold && (
+        <div className="-mx-4 -my-1 overflow-auto px-4 py-1">
+          <ChargeTable
+            name={'progressive-billing-recurring'}
+            data={thresholds.filter((threshold) => threshold.recurring) || []}
+            columns={[
+              {
+                size: 175,
+                content: () => (
+                  <Typography className="px-4" variant="captionHl" noWrap>
+                    {translate('text_17241798877230y851fdxzqu')}
+                  </Typography>
+                ),
+              },
+              {
+                size: 250,
+                content: (row) => (
+                  <AmountInput
+                    variant="outlined"
+                    beforeChangeFormatter={['positiveNumber']}
+                    currency={currency}
+                    value={row.value}
+                    onChange={(value) => {
+                      setThresholdValue({
+                        index: recurringIndex,
+                        key: 'value',
+                        newValue: String(value) || undefined,
+                      })
+                    }}
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          {getCurrencySymbol(currency)}
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+                ),
+              },
+              {
+                size: 250,
+                content: (row) => (
+                  <TextInput
+                    variant="outlined"
+                    placeholder={translate('text_1747921154885jqsl0c9xhro')}
+                    value={row.code ?? ''}
+                    onChange={(value) => {
+                      setThresholdValue({
+                        index: recurringIndex,
+                        key: 'code',
+                        newValue: value === '' ? undefined : value,
+                      })
+                    }}
+                  />
+                ),
+              },
+            ]}
+          />
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default AlertThresholds
--- src/pages/AlertForm.tsx
+++ src/pages/AlertForm.tsx
@@ -1,32 +1,66 @@
 import { gql } from '@apollo/client'
 import { useFormik } from 'formik'
-import { useCallback, useMemo, useRef } from 'react'
+import { useCallback, useEffect, useMemo, useRef } from 'react'
 import { generatePath, useNavigate, useParams } from 'react-router-dom'
 import { array, boolean, number, object, string } from 'yup'
 
+import AlertThresholds, { isThresholdValueValid } from '~/components/alerts/Thresholds'
 import { Button, Typography } from '~/components/designSystem'
-import { ComboBoxField, TextInputField } from '~/components/form'
+import { ComboBoxField, ComboboxItem, TextInputField } from '~/components/form'
 import { CenteredPage } from '~/components/layouts/CenteredPage'
 import { WarningDialog, WarningDialogRef } from '~/components/WarningDialog'
-import { addToast } from '~/core/apolloClient'
+import { addToast, hasDefinedGQLError } from '~/core/apolloClient'
 import { CustomerSubscriptionDetailsTabsOptionsEnum } from '~/core/constants/tabsOptions'
 import { CUSTOMER_SUBSCRIPTION_DETAILS_ROUTE, PLAN_SUBSCRIPTION_DETAILS_ROUTE } from '~/core/router'
+import { deserializeAmount, serializeAmount } from '~/core/serializers/serializeAmount'
 import {
+  AlertThreshold,
   AlertTypeEnum,
   CreateSubscriptionAlertInput,
+  CurrencyEnum,
+  ThresholdInput,
   useCreateSubscriptionAlertMutation,
+  useGetExistingAlertsOfSubscriptionQuery,
   useGetSubscriptionAlertToEditQuery,
+  useGetSubscriptionBillableMetricsQuery,
   useGetSubscriptionInfosQuery,
   useUpdateSubscriptionAlertMutation,
 } from '~/generated/graphql'
 import { useInternationalization } from '~/hooks/core/useInternationalization'
 import { FormLoadingSkeleton } from '~/styles/mainObjectsForm'
 
+const sortAndFormatThresholds = (
+  thresholds: AlertThreshold[],
+  currency: CurrencyEnum,
+): AlertThreshold[] => {
+  const formattedThresholds = thresholds.map((threshold) => ({
+    ...threshold,
+    value: String(deserializeAmount(threshold.value, currency)),
+  }))
+
+  const recurringThreshold = formattedThresholds.find((threshold) => threshold.recurring)
+  const nonRecurringThresholds = formattedThresholds.filter((threshold) => !threshold.recurring)
+
+  // Sort the non-recurring thresholds by value
+  const sortedNonRecurringThresholds = nonRecurringThresholds.sort((a, b) => {
+    if (a.value && !b.value) return -1
+    if (!a.value && b.value) return 1
+    return 0
+  })
+
+  // Combine the recurring threshold with the sorted non-recurring thresholds
+  return [...sortedNonRecurringThresholds, ...(!!recurringThreshold ? [recurringThreshold] : [])]
+}
+
 gql`
   query getSubscriptionInfos($id: ID!) {
     subscription(id: $id) {
       id
       externalId
+      plan {
+        id
+        amountCurrency
+      }
     }
   }
 
@@ -45,6 +79,26 @@ gql`
         code
         recurring
         value
+      }
+    }
+  }
+
+  query getExistingAlertsOfSubscription($subscriptionExternalId: String!, $limit: Int) {
+    alerts(subscriptionExternalId: $subscriptionExternalId, limit: $limit) {
+      collection {
+        id
+        alertType
+        billableMetricId
+      }
+    }
+  }
+
+  query getSubscriptionBillableMetrics($page: Int, $limit: Int, $searchTerm: String, $planId: ID) {
+    billableMetrics(page: $page, limit: $limit, searchTerm: $searchTerm, planId: $planId) {
+      collection {
+        id
+        code
+        name
       }
     }
   }
@@ -70,17 +124,45 @@ const AlertForm = () => {
   const isEdition = !!alertId
 
   const { data: subscriptionData, loading: subscriptionLoading } = useGetSubscriptionInfosQuery({
-    variables: { id: alertId },
-    skip: !isEdition,
-    fetchPolicy: 'network-only',
+    variables: { id: subscriptionId },
   })
 
   const { data: alertData, loading: alertLoading } = useGetSubscriptionAlertToEditQuery({
     variables: { id: alertId },
     skip: !isEdition,
-    fetchPolicy: 'network-only',
   })
+
+  const { data: existingAlertsData, loading: existingAlertsLoading } =
+    useGetExistingAlertsOfSubscriptionQuery({
+      variables: {
+        subscriptionExternalId: subscriptionData?.subscription?.externalId || '',
+        limit: 99999,
+      },
+      skip: isEdition || !subscriptionData?.subscription?.externalId,
+      fetchPolicy: 'network-only',
+    })
+
+  const { data: subscriptionBillableMetricsData, loading: subscriptionBillableMetricsLoading } =
+    useGetSubscriptionBillableMetricsQuery({
+      variables: {
+        page: 1,
+        limit: 20,
+        searchTerm: '',
+        planId: subscriptionData?.subscription?.plan?.id,
+      },
+      skip:
+        !subscriptionData?.subscription?.plan?.id ||
+        (isEdition && alertData?.alert?.alertType === AlertTypeEnum.UsageAmount),
+    })
+
+  const isLoading =
+    subscriptionLoading ||
+    alertLoading ||
+    existingAlertsLoading ||
+    subscriptionBillableMetricsLoading
+
   const existingAlert = alertData?.alert
+  const currency = subscriptionData?.subscription?.plan?.amountCurrency || CurrencyEnum.Usd
 
   const onLeave = useCallback(() => {
     if (!!customerId) {
@@ -102,7 +184,7 @@ const AlertForm = () => {
     }
   }, [customerId, navigate, planId, subscriptionId])
 
-  const [updateAlert] = useUpdateSubscriptionAlertMutation({
+  const [updateAlert, { error: updateError }] = useUpdateSubscriptionAlertMutation({
     onCompleted({ updateSubscriptionAlert }) {
       if (!!updateSubscriptionAlert?.id) {
         addToast({
@@ -115,7 +197,7 @@ const AlertForm = () => {
     },
   })
 
-  const [createAlert] = useCreateSubscriptionAlertMutation({
+  const [createAlert, { error: createError }] = useCreateSubscriptionAlertMutation({
     onCompleted({ createSubscriptionAlert }) {
       if (!!createSubscriptionAlert?.id) {
         addToast({
@@ -135,7 +217,17 @@ const AlertForm = () => {
       // @ts-expect-error alertType is mandatory but default value should be empty string
       alertType: existingAlert?.alertType || '',
       billableMetricId: existingAlert?.billableMetric?.id || '',
-      thresholds: existingAlert?.thresholds || [],
+      // Note: we need to sort the thresholds by value and recuring last.
+      // We don't really know how the backend will return the thresholds as we don't check the order if they are saved via API
+      thresholds: !!existingAlert?.thresholds?.length
+        ? sortAndFormatThresholds(existingAlert?.thresholds, currency)
+        : [
+            {
+              code: '',
+              recurring: false,
+              value: '',
+            },
+          ],
     },
     validationSchema: object().shape({
       name: string(),
@@ -145,7 +237,7 @@ const AlertForm = () => {
       thresholds: array()
         .of(
           object().shape({
-            code: string().required(''),
+            code: string(),
             recurring: boolean().required(''),
             value: number().required(''),
           }),
@@ -154,23 +246,52 @@ const AlertForm = () => {
     }),
     enableReinitialize: true,
     validateOnMount: true,
-    onSubmit: async (values) => {
+    onSubmit: async ({ billableMetricId, alertType, thresholds, ...values }) => {
+      const formattedThresholds = thresholds?.map((threshold) => ({
+        ...threshold,
+        value: String(serializeAmount(threshold.value, currency)),
+      }))
+
       // Edition
       if (!!existingAlert?.id) {
         await updateAlert({
           variables: {
-            input: { ...values, id: existingAlert.id },
+            input: {
+              ...values,
+              id: existingAlert.id,
+              billableMetricId: billableMetricId || undefined,
+              thresholds: formattedThresholds || undefined,
+            },
           },
         })
       } else {
         await createAlert({
           variables: {
-            input: { ...values, subscriptionId: subscriptionData?.subscription?.externalId || '' },
+            input: {
+              ...values,
+              alertType,
+              subscriptionId: subscriptionId,
+              billableMetricId: billableMetricId || undefined,
+              thresholds: formattedThresholds || undefined,
+            },
           },
         })
       }
     },
   })
+
+  useEffect(() => {
+    if (hasDefinedGQLError('ValueAlreadyExist', createError || updateError)) {
+      formikProps.setFieldError('code', 'text_632a2d437e341dcc76817556')
+      const rootElement = document.getElementById('root')
+
+      if (!rootElement) return
+      rootElement.scrollTo({ top: 0 })
+    }
+
+    return undefined
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [createError, formikProps.setFieldError, updateError])
 
   const showThresholdTable = useMemo(
     () =>
@@ -179,6 +300,75 @@ const AlertForm = () => {
         !!formikProps.values.billableMetricId),
     [formikProps.values.alertType, formikProps.values.billableMetricId],
   )
+
+  const comboboxData = useMemo(() => {
+    return (subscriptionBillableMetricsData?.billableMetrics?.collection || []).map((item) => {
+      const { id, code, name } = item
+
+      const hasAlertOnBillableMetric = existingAlertsData?.alerts.collection.some(
+        (alert) => alert.billableMetricId === id,
+      )
+
+      return {
+        label: `${name} (${code})`,
+        value: id,
+        disabled: hasAlertOnBillableMetric,
+        labelNode: (
+          <ComboboxItem className="flex flex-col items-start">
+            <Typography color="grey700" noWrap>
+              {name}
+            </Typography>
+            <Typography color="textPrimary" noWrap>
+              ({code})
+            </Typography>
+          </ComboboxItem>
+        ),
+      }
+    })
+  }, [
+    subscriptionBillableMetricsData?.billableMetrics?.collection,
+    existingAlertsData?.alerts.collection,
+  ])
+
+  const { hasUsageAmountAlert } = useMemo(() => {
+    if (!existingAlertsData?.alerts.collection.length) {
+      return { hasUsageAmountAlert: false }
+    }
+
+    const localHasUsageAmountAlert = existingAlertsData?.alerts.collection.some(
+      (alert) => alert.alertType === AlertTypeEnum.UsageAmount,
+    )
+
+    return {
+      hasUsageAmountAlert: localHasUsageAmountAlert,
+    }
+  }, [existingAlertsData?.alerts.collection])
+
+  const hasAnyNonRecurringThresholdError = useMemo(() => {
+    const localNonRecurringThresholds = formikProps.values.thresholds.filter(
+      (threshold) => !threshold.recurring,
+    )
+
+    return localNonRecurringThresholds.some((threshold, i) =>
+      isThresholdValueValid(i, threshold.value, localNonRecurringThresholds),
+    )
+  }, [formikProps.values.thresholds])
+
+  const setThresholds = (thresholds: ThresholdInput[]) => {
+    formikProps.setFieldValue('thresholds', thresholds)
+  }
+
+  const setThresholdValue = ({
+    index,
+    key,
+    newValue,
+  }: {
+    index: number
+    key: keyof ThresholdInput
+    newValue: unknown
+  }) => {
+    formikProps.setFieldValue(`thresholds.${index}.${key}`, newValue)
+  }
 
   return (
     <>
@@ -199,8 +389,8 @@ const AlertForm = () => {
         </CenteredPage.Header>
 
         <CenteredPage.Container>
-          {alertLoading && <FormLoadingSkeleton id="create-alert" />}
-          {!alertLoading && (
+          {isLoading && <FormLoadingSkeleton id="create-alert" />}
+          {!isLoading && (
             <>
               <div className="not-last-child:mb-1">
                 <Typography variant="headline" color="grey700">
@@ -233,6 +423,7 @@ const AlertForm = () => {
                       label={translate('text_62876e85e32e0300e1803127')}
                       placeholder={translate('text_623b42ff8ee4e000ba87d0c4')}
                       formikProps={formikProps}
+                      error={formikProps.errors.code}
                     />
                   </div>
                 </section>
@@ -251,6 +442,8 @@ const AlertForm = () => {
                       name="alertType"
                       label={translate('text_1746631350478jqk347d5dy4')}
                       placeholder={translate('text_1746631350478bwa1swfpwky')}
+                      disabled={isEdition}
+                      disableClearable={isEdition}
                       data={[
                         {
                           label: translate('text_1746631350478l8lfdopffh1'),
@@ -259,29 +452,32 @@ const AlertForm = () => {
                         {
                           label: translate('text_1746631350478bwa1swfpwkw'),
                           value: AlertTypeEnum.UsageAmount,
+                          disabled: hasUsageAmountAlert,
                         },
                       ]}
                       formikProps={formikProps}
                     />
 
-                    {!!formikProps.values.alertType && (
+                    {formikProps.values.alertType === AlertTypeEnum.BillableMetricUsageAmount && (
                       <>
-                        {formikProps.values.alertType ===
-                          AlertTypeEnum.BillableMetricUsageAmount && (
-                          <>
-                            <ComboBoxField
-                              name="billableMetricId"
-                              label={translate('text_1746780648463scppfjbhd1b')}
-                              placeholder={translate('text_1746780648463n39xfvr772k')}
-                              // TODO: Add data
-                              data={[]}
-                              formikProps={formikProps}
-                            />
-                          </>
-                        )}
-
-                        {showThresholdTable && <>TODO: Show threeshold table</>}
+                        <ComboBoxField
+                          name="billableMetricId"
+                          label={translate('text_1746780648463scppfjbhd1b')}
+                          placeholder={translate('text_1746780648463n39xfvr772k')}
+                          disabled={isEdition}
+                          data={comboboxData}
+                          formikProps={formikProps}
+                        />
                       </>
+                    )}
+
+                    {showThresholdTable && (
+                      <AlertThresholds
+                        thresholds={formikProps.values.thresholds}
+                        setThresholds={setThresholds}
+                        setThresholdValue={setThresholdValue}
+                        currency={currency}
+                      />
                     )}
                   </div>
                 </section>
@@ -303,10 +499,17 @@ const AlertForm = () => {
           <Button
             variant="primary"
             size="large"
-            disabled={!formikProps.isValid || !formikProps.dirty || subscriptionLoading}
+            disabled={
+              !formikProps.isValid ||
+              !formikProps.dirty ||
+              isLoading ||
+              hasAnyNonRecurringThresholdError
+            }
             onClick={formikProps.submitForm}
           >
-            {translate('text_1737473550277wkq2gsbaiab')}
+            {translate(
+              isEdition ? 'text_17432414198706rdwf76ek3u' : 'text_1747917472538el8fg31n3i8',
+            )}
           </Button>
         </CenteredPage.StickyFooter>
       </CenteredPage.Wrapper>
