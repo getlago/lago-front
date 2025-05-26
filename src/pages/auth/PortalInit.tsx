efff77f9d5dccb6720b32032296ae40a516b5a6b
Alexandre Monjol <alexandre.monjol@gmail.com>
2025-05-26 10:40:45 +0200

    bug(customerPortal): prevent unexpected calls to be triggered

--- src/pages/auth/PortalInit.tsx
+++ src/pages/auth/PortalInit.tsx
@@ -1,17 +1,35 @@
+import { useApolloClient } from '@apollo/client'
+import { Icon } from 'lago-design-system'
 import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
-import { onAccessCustomerPortal } from '~/core/apolloClient'
+import {
+  CUSTOMER_PORTAL_TOKEN_LS_KEY,
+  getItemFromLS,
+  onAccessCustomerPortal,
+} from '~/core/apolloClient'
 import CustomerPortal from '~/pages/customerPortal/CustomerPortal'
 
 const PortalInit = () => {
   const { token } = useParams()
+  const client = useApolloClient()
 
   useEffect(() => {
     if (token) {
+      // Prevent active queries from "main" app to re-fetch
+      client.clearStore()
+
       onAccessCustomerPortal(token)
     }
-  }, [token])
+  }, [client, token])
+
+  if (!token || !getItemFromLS(CUSTOMER_PORTAL_TOKEN_LS_KEY)) {
+    return (
+      <div className="flex size-full items-center justify-center">
+        <Icon name="processing" color="info" size="large" animation="spin" />
+      </div>
+    )
+  }
 
   return <CustomerPortal />
 }
