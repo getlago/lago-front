17b86da311e9540c0adde33162fc3aef0a993fbe
Alexandre Monjol <alexandre.monjol@gmail.com>
2025-06-02 17:51:35 +0200

    bug: wrong billing entity fetched in settings

--- src/pages/settings/SettingsHomePage.tsx
+++ src/pages/settings/SettingsHomePage.tsx
@@ -7,7 +7,12 @@ import { useGetBillingEntitiesQuery } from '~/generated/graphql'
 const SettingsHomePage = () => {
   const navigate = useNavigate()
 
-  const { data: billingEntitiesData } = useGetBillingEntitiesQuery()
+  const { data: billingEntitiesData } = useGetBillingEntitiesQuery({
+    // This endpoint is not cached to prevent error after logout + organization switch
+    // https://github.com/getlago/lago-front/pull/2233/files
+    fetchPolicy: 'no-cache',
+    nextFetchPolicy: 'no-cache',
+  })
 
   useEffect(() => {
     if (!billingEntitiesData?.billingEntities?.collection?.length) {
