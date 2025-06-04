4fe5d8d7edc54a25155777d3eef07edad685e2e1
Alexandre Monjol <alexandre.monjol@gmail.com>
2025-06-04 11:16:52 +0200

    misc: remove styled components and turn into FC if needed

--- src/styles/designSystem/PopperComponents.tsx
+++ src/styles/designSystem/PopperComponents.tsx
@@ -6,3 +6,10 @@ export const MenuPopper: FC<PropsWithChildren<{ className?: string }>> = ({
   className,
   children,
 }) => <div className={tw('flex flex-col gap-1 p-2', className)}>{children}</div>
+
+export const PopperOpener: FC<PropsWithChildren<{ className?: string }>> = ({
+  children,
+  className,
+}) => {
+  return <div className={tw('absolute right-12 top-4 z-[1] md:right-4', className)}>{children}</div>
+}
