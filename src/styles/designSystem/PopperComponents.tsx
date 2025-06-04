1adc6ef6b3656ed7e8b084eef9cc3f20c16f08c8
Alexandre Monjol <alexandre.monjol@gmail.com>
2025-06-04 14:44:54 +0200

    misc: forward ref and props to fix placement

--- src/styles/designSystem/PopperComponents.tsx
+++ src/styles/designSystem/PopperComponents.tsx
@@ -1,4 +1,4 @@
-import { FC, PropsWithChildren } from 'react'
+import { FC, forwardRef, PropsWithChildren } from 'react'
 
 import { tw } from '~/styles/utils'
 
@@ -7,9 +7,20 @@ export const MenuPopper: FC<PropsWithChildren<{ className?: string }>> = ({
   children,
 }) => <div className={tw('flex flex-col gap-1 p-2', className)}>{children}</div>
 
-export const PopperOpener: FC<PropsWithChildren<{ className?: string }>> = ({
-  children,
-  className,
-}) => {
-  return <div className={tw('absolute right-12 top-4 z-[1] md:right-4', className)}>{children}</div>
-}
+// Note: we must use forwardRef and props spread because some invocations use them to place the element in the parent
+// E.g. in the Table component, the Popper passes those props implicitely to the PopperOpener.
+export const PopperOpener = forwardRef<HTMLDivElement, PropsWithChildren<{ className?: string }>>(
+  ({ children, className, ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className={tw('absolute right-12 top-4 z-10 md:right-4', className)}
+        {...props}
+      >
+        {children}
+      </div>
+    )
+  },
+)
+
+PopperOpener.displayName = 'PopperOpener'
