ba83a2ec27aacb38d418e66d91d52b49081a37e8
Alexandre Monjol <alexandre.monjol@gmail.com>
2025-07-09 17:10:44 -0300

    misc: handle logo removal in email scenario settings

--- src/components/settings/emails/UpdateBillingEntityLogoDialog.tsx
+++ src/components/settings/emails/UpdateBillingEntityLogoDialog.tsx
@@ -18,54 +18,64 @@ gql`
 
 export type UpdateBillingEntityLogoDialogRef = DialogRef
 
-export const UpdateBillingEntityLogoDialog = forwardRef<UpdateBillingEntityLogoDialogRef>(
-  (_, ref) => {
-    const { billingEntityCode } = useParams<string>()
-    const { translate } = useInternationalization()
-    const [logo, setLogo] = useState<string>()
-    const [updateLogo] = useUpdateBillingEntityLogoMutation()
+type TUpdateBillingEntityLogoDialog = {
+  existingLogoUrl: string | null | undefined
+}
 
-    const { data: billingEntityData } = useGetBillingEntityQuery({
-      variables: {
-        code: billingEntityCode as string,
-      },
-      skip: !billingEntityCode,
-    })
+export const UpdateBillingEntityLogoDialog = forwardRef<
+  UpdateBillingEntityLogoDialogRef,
+  TUpdateBillingEntityLogoDialog
+>(({ existingLogoUrl }, ref) => {
+  const { billingEntityCode } = useParams<string>()
+  const { translate } = useInternationalization()
+  const [logo, setLogo] = useState<string | null>()
+  const [updateLogo] = useUpdateBillingEntityLogoMutation()
 
-    const billingEntity = billingEntityData?.billingEntity
+  const { data: billingEntityData } = useGetBillingEntityQuery({
+    variables: {
+      code: billingEntityCode as string,
+    },
+    skip: !billingEntityCode,
+  })
 
-    return (
-      <Dialog
-        ref={ref}
-        title={translate('text_6411e69b9bda18008db7ad51')}
-        description={translate('text_6411e6a2de0b3f00b25ae488')}
-        onClose={() => {
-          setLogo(undefined)
-        }}
-        actions={({ closeDialog }) => (
-          <>
-            <Button variant="quaternary" onClick={closeDialog}>
-              {translate('text_6411e6b530cb47007488b027')}
-            </Button>
-            <Button
-              variant="primary"
-              disabled={!logo}
-              onClick={async () => {
-                await updateLogo({
-                  variables: { input: { id: billingEntity?.id as string, logo } },
-                })
-                closeDialog()
-              }}
-            >
-              {translate('text_6411e6ac9a8c9700a7570a4e')}
-            </Button>
-          </>
-        )}
-      >
-        <LogoPicker className="mb-8" logoValue={logo} onChange={(value) => setLogo(value)} />
-      </Dialog>
-    )
-  },
-)
+  const billingEntity = billingEntityData?.billingEntity
+
+  return (
+    <Dialog
+      ref={ref}
+      title={translate('text_6411e69b9bda18008db7ad51')}
+      description={translate('text_6411e6a2de0b3f00b25ae488')}
+      onClose={() => {
+        setLogo(undefined)
+      }}
+      actions={({ closeDialog }) => (
+        <>
+          <Button variant="quaternary" onClick={closeDialog}>
+            {translate('text_6411e6b530cb47007488b027')}
+          </Button>
+          <Button
+            variant="primary"
+            disabled={logo === undefined}
+            onClick={async () => {
+              await updateLogo({
+                variables: { input: { id: billingEntity?.id as string, logo } },
+              })
+              closeDialog()
+            }}
+          >
+            {translate('text_6411e6ac9a8c9700a7570a4e')}
+          </Button>
+        </>
+      )}
+    >
+      <LogoPicker
+        className="mb-8"
+        logoValue={logo}
+        onChange={(value) => setLogo(value)}
+        logoUrl={existingLogoUrl}
+      />
+    </Dialog>
+  )
+})
 
 UpdateBillingEntityLogoDialog.displayName = 'UpdateBillingEntityLogoDialog'
