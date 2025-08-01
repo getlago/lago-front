63861d7212383416936184de5bdc8309ee0da8ce
Stefan M <stefan@getlago.com>
2025-08-01 11:28:06 +0300

    feat(analytics-usage): show daily / last 30 days by default

--- src/components/analytics/usage/useUsageAnalyticsOverview.ts
+++ src/components/analytics/usage/useUsageAnalyticsOverview.ts
@@ -78,16 +78,12 @@ export const useUsageAnalyticsOverview = () => {
       return `${now.minus({ month: 1 }).startOf('day').toISO()},${now.endOf('day').toISO()}`
     }
 
-    return `${now.minus({ month: 12 }).startOf('day').toISO()},${now.endOf('day').toISO()}`
+    return `${now.minus({ days: 30 }).startOf('day').toISO()},${now.endOf('day').toISO()}`
   }, [hasAccessToAnalyticsDashboardsFeature])
 
   const getDefaultStaticTimeGranularityFilter = useCallback((): string => {
-    if (!hasAccessToAnalyticsDashboardsFeature) {
-      return TimeGranularityEnum.Daily
-    }
-
-    return TimeGranularityEnum.Monthly
-  }, [hasAccessToAnalyticsDashboardsFeature])
+    return TimeGranularityEnum.Daily
+  }, [])
 
   const filtersForUsageOverviewQuery = useMemo(() => {
     if (!hasAccessToAnalyticsDashboardsFeature) {
