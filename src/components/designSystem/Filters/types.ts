91418f97c35e8eaf2474cbaf28be9f67d8e00f26
Stefan M <stefan@getlago.com>
2025-09-05 16:53:36 +0300

    feat: connect to data-api

--- src/components/designSystem/Filters/types.ts
+++ src/components/designSystem/Filters/types.ts
@@ -211,7 +211,9 @@ export const UsageBillableMetricAvailableFilters = [
 ]
 
 export const ForecastsAvailableFilters = [
+  AvailableFiltersEnum.billingEntityCode,
   AvailableFiltersEnum.country,
+  AvailableFiltersEnum.currency,
   AvailableFiltersEnum.customerExternalId,
   AvailableFiltersEnum.customerAccountType,
   AvailableFiltersEnum.isCustomerTinEmpty,
