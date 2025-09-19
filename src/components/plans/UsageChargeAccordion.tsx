ce8c8de8e65711506e30d44c7f499ad183bcb451
Alexandre Monjol <alexandre.monjol@gmail.com>
2025-09-19 14:26:09 +0200

    misc: compose handleUpdateCharges usage

--- src/components/plans/UsageChargeAccordion.tsx
+++ src/components/plans/UsageChargeAccordion.tsx
@@ -15,6 +15,10 @@ import {
   TextInput,
 } from '~/components/form'
 import { EditInvoiceDisplayNameRef } from '~/components/invoices/EditInvoiceDisplayName'
+import {
+  handleUpdateCharges,
+  HandleUpdateChargesProps,
+} from '~/components/plans/chargeAccordion/utils'
 import { ChargeBillingRadioGroup } from '~/components/plans/ChargeBillingRadioGroup'
 import { PremiumWarningDialogRef } from '~/components/PremiumWarningDialog'
 import { useDuplicatePlanVar } from '~/core/apolloClient'
@@ -269,38 +273,16 @@ export const UsageChargeAccordion = memo(
     }, [initialLocalCharge?.minAmountCents])
 
     const handleUpdate = useCallback(
-      (name: string, value: unknown) => {
-        // IMPORTANT: This check should stay first in this function
-        // If user is not premium and try to switch to graduated percentage pricing
-        // We should show the premium modal and prevent any formik value change
-        if (name === 'chargeModel' && !isPremium && value === ChargeModelEnum.GraduatedPercentage) {
-          premiumWarningDialogRef?.current?.openDialog()
-          return
-        }
-
-        // NOTE: We prevent going further if the change is about the charge model and the value remain the same
-        // It prevents fixing the properties to be wrongly reset to default on 2nd select.
-        if (name === 'chargeModel' && value === localCharge.chargeModel) return
-
-        let currentChargeValues: LocalChargeInput = {
-          ...localCharge,
-          [name]: value,
-        }
-
-        if (name === 'chargeModel') {
-          // Reset charge data to default when switching charge model
-          currentChargeValues = {
-            ...currentChargeValues,
-            payInAdvance: false,
-            prorated: false,
-            invoiceable: true,
-            properties: getPropertyShape({}),
-            filters: [],
-            taxes: [],
-          }
-        }
-
-        formikProps.setFieldValue(`charges.${index}`, currentChargeValues)
+      (name: HandleUpdateChargesProps['name'], value: HandleUpdateChargesProps['value']) => {
+        handleUpdateCharges({
+          formikProps,
+          index,
+          isPremium,
+          localCharge,
+          name,
+          premiumWarningDialogRef,
+          value,
+        })
       },
 
       // eslint-disable-next-line react-hooks/exhaustive-deps
