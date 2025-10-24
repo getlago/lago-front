4f59190c77e0caa1f7da1c8fcabe473feed9bfac
Allan Michay <allan.michay@gmail.com>
2025-10-24 16:23:13 +0200

    misc: clean NetsuiteIntegration components

--- src/components/settings/integrations/NetsuiteIntegrationItemsListAddons.tsx
+++ src/components/settings/integrations/NetsuiteIntegrationItemsListAddons.tsx
@@ -6,10 +6,12 @@ import { useNavigate } from 'react-router-dom'
 import { InfiniteScroll } from '~/components/designSystem'
 import { GenericPlaceholder } from '~/components/GenericPlaceholder'
 import { CREATE_ADD_ON_ROUTE } from '~/core/router'
+import { createRangeArray } from '~/core/utils/createRangeArray'
 import {
   GetAddOnsForNetsuiteItemsListQuery,
   InputMaybe,
   MappableTypeEnum,
+  useGetAddOnsForNetsuiteItemsListLazyQuery,
 } from '~/generated/graphql'
 import { useInternationalization } from '~/hooks/core/useInternationalization'
 import {
@@ -38,7 +40,7 @@ gql`
 
 type NetsuiteIntegrationItemsListAddonsProps = {
   data: GetAddOnsForNetsuiteItemsListQuery | undefined
-  fetchMoreAddons: Function
+  fetchMoreAddons: ReturnType<typeof useGetAddOnsForNetsuiteItemsListLazyQuery>[1]['fetchMore']
   hasError: boolean
   integrationId: string
   searchTerm: InputMaybe<string> | undefined
@@ -59,12 +61,11 @@ const NetsuiteIntegrationItemsListAddons = ({
   const { translate } = useInternationalization()
   const addons = data?.addOns?.collection || []
 
-  return (
-    <Stack>
-      <IntegrationItemHeader columnName={translate('text_6630ea71a6c2ef00bc63006f')} />
-      {!!isLoading && !addons.length && searchTerm ? (
+  const displayCorrectState = () => {
+    if (isLoading && !addons.length && !!searchTerm) {
+      return (
         <>
-          {[0, 1, 2].map((i) => (
+          {createRangeArray(3).map((i) => (
             <IntegrationItemLine
               key={`addon-item-skeleton-${i}`}
               icon="puzzle"
@@ -74,106 +75,115 @@ const NetsuiteIntegrationItemsListAddons = ({
             />
           ))}
         </>
-      ) : !isLoading && !!hasError ? (
-        <>
-          {!!searchTerm ? (
-            <GenericPlaceholder
-              title={translate('text_623b53fea66c76017eaebb6e')}
-              subtitle={translate('text_63bab307a61c62af497e0599')}
-              image={<ErrorImage width="136" height="104" />}
-            />
-          ) : (
-            <GenericPlaceholder
-              title={translate('text_629728388c4d2300e2d380d5')}
-              subtitle={translate('text_629728388c4d2300e2d380eb')}
-              buttonTitle={translate('text_629728388c4d2300e2d38110')}
-              buttonVariant="primary"
-              buttonAction={() => location.reload()}
-              image={<ErrorImage width="136" height="104" />}
-            />
-          )}
-        </>
-      ) : !isLoading && (!addons || !addons.length) ? (
-        <>
-          {!!searchTerm ? (
-            <GenericPlaceholder
-              title={translate('text_63bee4e10e2d53912bfe4da5')}
-              subtitle={translate('text_63bee4e10e2d53912bfe4da7')}
-              image={<EmptyImage width="136" height="104" />}
-            />
-          ) : (
-            <GenericPlaceholder
-              title={translate('text_629728388c4d2300e2d380c9')}
-              subtitle={translate('text_629728388c4d2300e2d380df')}
-              buttonTitle={translate('text_629728388c4d2300e2d3810f')}
-              buttonVariant="primary"
-              buttonAction={() => navigate(CREATE_ADD_ON_ROUTE)}
-              image={<EmptyImage width="136" height="104" />}
-            />
-          )}
-        </>
+      )
+    }
+
+    if (!isLoading && hasError) {
+      return !!searchTerm ? (
+        <GenericPlaceholder
+          title={translate('text_623b53fea66c76017eaebb6e')}
+          subtitle={translate('text_63bab307a61c62af497e0599')}
+          image={<ErrorImage width="136" height="104" />}
+        />
       ) : (
-        <InfiniteScroll
-          onBottom={() => {
-            const { currentPage = 0, totalPages = 0 } = data?.addOns?.metadata || {}
+        <GenericPlaceholder
+          title={translate('text_629728388c4d2300e2d380d5')}
+          subtitle={translate('text_629728388c4d2300e2d380eb')}
+          buttonTitle={translate('text_629728388c4d2300e2d38110')}
+          buttonVariant="primary"
+          buttonAction={() => location.reload()}
+          image={<ErrorImage width="136" height="104" />}
+        />
+      )
+    }
 
-            currentPage < totalPages &&
-              !isLoading &&
-              fetchMoreAddons({
-                variables: { page: currentPage + 1 },
-              })
-          }}
-        >
-          <>
-            {!!addons.length &&
-              addons.map((addOn) => {
-                const addonMapping = addOn?.integrationMappings?.find(
-                  (i) => i.mappableType === MappableTypeEnum.AddOn,
-                )
+    if (!isLoading && (!addons || !addons.length)) {
+      return !!searchTerm ? (
+        <GenericPlaceholder
+          title={translate('text_63bee4e10e2d53912bfe4da5')}
+          subtitle={translate('text_63bee4e10e2d53912bfe4da7')}
+          image={<EmptyImage width="136" height="104" />}
+        />
+      ) : (
+        <GenericPlaceholder
+          title={translate('text_629728388c4d2300e2d380c9')}
+          subtitle={translate('text_629728388c4d2300e2d380df')}
+          buttonTitle={translate('text_629728388c4d2300e2d3810f')}
+          buttonVariant="primary"
+          buttonAction={() => navigate(CREATE_ADD_ON_ROUTE)}
+          image={<EmptyImage width="136" height="104" />}
+        />
+      )
+    }
 
-                return (
-                  <IntegrationItemLine
-                    key={`addon-item-${addOn.id}`}
-                    icon="puzzle"
-                    label={addOn.name}
-                    description={addOn.code}
-                    loading={false}
-                    onMappingClick={() => {
-                      netsuiteIntegrationMapItemDialogRef.current?.openDialog({
-                        integrationId,
-                        type: MappableTypeEnum.AddOn,
-                        itemId: addonMapping?.id,
-                        itemExternalId: addonMapping?.externalId,
-                        itemExternalCode: addonMapping?.externalAccountCode || undefined,
-                        itemExternalName: addonMapping?.externalName || undefined,
-                        lagoMappableId: addOn.id,
-                        lagoMappableName: addOn.name,
-                      })
-                    }}
-                    mappingInfos={
-                      !!addonMapping?.id
-                        ? {
-                            id: addonMapping.externalId,
-                            name: addonMapping.externalName || '',
-                          }
-                        : undefined
-                    }
-                  />
-                )
-              })}
-            {isLoading &&
-              [0, 1, 2].map((i) => (
+    return (
+      <InfiniteScroll
+        onBottom={() => {
+          const { currentPage = 0, totalPages = 0 } = data?.addOns?.metadata || {}
+
+          currentPage < totalPages &&
+            !isLoading &&
+            fetchMoreAddons({
+              variables: { page: currentPage + 1 },
+            })
+        }}
+      >
+        <>
+          {!!addons.length &&
+            addons.map((addOn) => {
+              const addonMapping = addOn?.integrationMappings?.find(
+                (i) => i.mappableType === MappableTypeEnum.AddOn,
+              )
+
+              return (
                 <IntegrationItemLine
-                  key={`addon-item-skeleton-${i}`}
+                  key={`addon-item-${addOn.id}`}
                   icon="puzzle"
-                  label={''}
-                  description={''}
-                  loading={true}
+                  label={addOn.name}
+                  description={addOn.code}
+                  loading={false}
+                  onMappingClick={() => {
+                    netsuiteIntegrationMapItemDialogRef.current?.openDialog({
+                      integrationId,
+                      type: MappableTypeEnum.AddOn,
+                      itemId: addonMapping?.id,
+                      itemExternalId: addonMapping?.externalId,
+                      itemExternalCode: addonMapping?.externalAccountCode || undefined,
+                      itemExternalName: addonMapping?.externalName || undefined,
+                      lagoMappableId: addOn.id,
+                      lagoMappableName: addOn.name,
+                    })
+                  }}
+                  mappingInfos={
+                    !!addonMapping?.id
+                      ? {
+                          id: addonMapping.externalId,
+                          name: addonMapping.externalName || '',
+                        }
+                      : undefined
+                  }
                 />
-              ))}
-          </>
-        </InfiniteScroll>
-      )}
+              )
+            })}
+          {isLoading &&
+            createRangeArray(3).map((i) => (
+              <IntegrationItemLine
+                key={`addon-item-skeleton-${i}`}
+                icon="puzzle"
+                label={''}
+                description={''}
+                loading={true}
+              />
+            ))}
+        </>
+      </InfiniteScroll>
+    )
+  }
+
+  return (
+    <Stack>
+      <IntegrationItemHeader columnName={translate('text_6630ea71a6c2ef00bc63006f')} />
+      {displayCorrectState()}
     </Stack>
   )
 }
