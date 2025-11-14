ccbf56c55a1d3a934e9b8f793742da4e9d4479e7
Domenico Falco <mimmo.falco@gmail.com>
2025-11-14 10:58:47 +0100

    add tests

--- src/components/invoceCustomFooter/__tests__/factories/invoiceCustomSectionFactory.ts
+++ src/components/invoceCustomFooter/__tests__/factories/invoiceCustomSectionFactory.ts
@@ -18,30 +18,3 @@ export const createInvoiceCustomSection = ({
     code,
   }
 }
-
-export const createInvoiceCustomSections = (
-  count: number,
-  baseId: string = 'section',
-): InvoiceCustomSection[] => {
-  return Array.from({ length: count }, (_, index) => {
-    const num = index + 1
-
-    return createInvoiceCustomSection({
-      id: `${baseId}-${num}`,
-      name: `Section ${num}`,
-      code: `SECTION_${num}`,
-    })
-  })
-}
-
-export const mockOrgSections: InvoiceCustomSection[] = [
-  createInvoiceCustomSection({ id: 'section-1', name: 'Section 1', code: 'SECTION_1' }),
-  createInvoiceCustomSection({ id: 'section-2', name: 'Section 2', code: 'SECTION_2' }),
-  createInvoiceCustomSection({ id: 'section-3', name: 'Section 3', code: 'SECTION_3' }),
-]
-
-export const mockCustomerSections: InvoiceCustomSection[] = [
-  createInvoiceCustomSection({ id: 'section-2', name: 'Section 2', code: 'SECTION_2' }),
-  createInvoiceCustomSection({ id: 'section-3', name: 'Section 3', code: 'SECTION_3' }),
-  createInvoiceCustomSection({ id: 'section-4', name: 'Section 4', code: 'SECTION_4' }),
-]
