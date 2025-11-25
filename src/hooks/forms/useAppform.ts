f36486d93df1efb0ba99ceb396edd5d219fdee35
Domenico Falco <mimmo.falco@gmail.com>
2025-11-25 11:08:29 +0100

    perf(lazyComponent): Add lazyComponent utils + suspense High Order Component
    ## Context
    
    Creation of a HOC that wraps elements around the <Suspense /> component from react out of the box
    
    <!-- Linear link -->
    Fixes ISSUE-1280

--- src/hooks/forms/useAppform.ts
+++ src/hooks/forms/useAppform.ts
@@ -1,16 +1,24 @@
 import { createFormHook } from '@tanstack/react-form'
 
-/**
- * TODO: lazy import them once the routing is fixed.
- * We currently have a strange Suspense recursively called in RouteWrapper.tsx line 53
- */
-import CheckboxField from '~/components/form/Checkbox/CheckboxFieldForTanstack.tsx'
-import ComboBoxField from '~/components/form/ComboBox/ComboBoxFieldForTanstack.tsx'
-import SubmitButton from '~/components/form/SubmitButton/SubmitButtonField.tsx'
-import SwitchField from '~/components/form/Switch/SwitchFieldForTanstack.tsx'
-import TextInputField from '~/components/form/TextInput/TextInputFieldForTanstack.tsx'
+import { lazyComponent } from '~/core/utils/lazyComponent.tsx'
 
 import { fieldContext, formContext } from './formContext.ts'
+
+const CheckboxField = lazyComponent(
+  () => import('~/components/form/Checkbox/CheckboxFieldForTanstack.tsx'),
+)
+const ComboBoxField = lazyComponent(
+  () => import('~/components/form/ComboBox/ComboBoxFieldForTanstack.tsx'),
+)
+const SwitchField = lazyComponent(
+  () => import('~/components/form/Switch/SwitchFieldForTanstack.tsx'),
+)
+const TextInputField = lazyComponent(
+  () => import('~/components/form/TextInput/TextInputFieldForTanstack.tsx'),
+)
+const SubmitButton = lazyComponent(
+  () => import('~/components/form/SubmitButton/SubmitButtonField.tsx'),
+)
 
 export const { useAppForm, withForm, withFieldGroup } = createFormHook({
   fieldComponents: {
