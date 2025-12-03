6a3e704ca12b777208969d2f4f6122bfb05ecfd5
Allan Michay <allan.michay@gmail.com>
2025-12-03 11:09:39 +0100

    fix: handle abort gracefully

--- src/pages/createCustomers/CreateCustomer.tsx
+++ src/pages/createCustomers/CreateCustomer.tsx
@@ -1,4 +1,4 @@
-import { revalidateLogic } from '@tanstack/react-form'
+import { revalidateLogic, useStore } from '@tanstack/react-form'
 import { Icon } from 'lago-design-system'
 import { useCallback, useMemo, useRef } from 'react'
 
@@ -109,9 +109,11 @@ const CreateCustomer = () => {
     },
   })
 
+  const isDirty = useStore(form.store, (state) => state.isDirty)
+
   const handleAbort = useCallback(() => {
-    form.state.isDirty ? warningDialogRef.current?.openDialog() : onClose()
-  }, [form.state.isDirty, onClose])
+    isDirty ? warningDialogRef.current?.openDialog() : onClose()
+  }, [isDirty, onClose])
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault()
