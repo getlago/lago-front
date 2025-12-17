5b3054dc7de28aecb39f5cb7a762c54727fc5454
Domenico Falco <mimmo.falco@gmail.com>
2025-12-17 11:53:41 +0100

    add multiple payment flow onto WalletTransactionTopUp

--- src/components/paymentMethodsInvoiceSettings/types.ts
+++ src/components/paymentMethodsInvoiceSettings/types.ts
@@ -1,6 +1,6 @@
 import { FormikProps } from 'formik'
 
-import { Customer, Maybe } from '~/generated/graphql'
+import { CreateCustomerWalletTransactionInput, Customer, Maybe } from '~/generated/graphql'
 import { SubscriptionFormInput } from '~/pages/subscriptions/types'
 import { TWalletDataForm } from '~/pages/wallet/types'
 
@@ -8,18 +8,21 @@ export enum ViewTypeEnum {
   Subscription = 'subscription',
   WalletTopUp = 'walletTopUp',
   WalletRecurringTopUp = 'walletRecurringTopUp',
+  WalletTransactionTopUp = 'walletTransactionTopUp',
 }
 
 export const VIEW_TYPE_TRANSLATION_KEYS: Record<ViewTypeEnum, string> = {
   [ViewTypeEnum.Subscription]: 'text_1764327933607nrezuuiheuc',
   [ViewTypeEnum.WalletTopUp]: 'text_1765895170354ovelm7g07o4',
   [ViewTypeEnum.WalletRecurringTopUp]: 'text_1765959116589recur1ngrul',
+  [ViewTypeEnum.WalletTransactionTopUp]: 'text_17659678187872em8xoix499',
 }
 
 type FormTypeMap = {
   [ViewTypeEnum.Subscription]: SubscriptionFormInput
   [ViewTypeEnum.WalletTopUp]: TWalletDataForm
   [ViewTypeEnum.WalletRecurringTopUp]: TWalletDataForm
+  [ViewTypeEnum.WalletTransactionTopUp]: Omit<CreateCustomerWalletTransactionInput, 'walletId'>
 }
 
 type CustomerForPaymentMethods = Maybe<Partial<Pick<Customer, 'id' | 'externalId'>>>
