f1db9502db48f0009736655cfd81887f1689a653
Allan Michay <allan.michay@gmail.com>
2026-01-08 10:11:45 +0100

    feat: update member list page

---
+++ src/pages/settings/members/common/MembersFilters.tsx
@@ -0,0 +1,129 @@
+import { Icon } from 'lago-design-system'
+import { useMemo } from 'react'
+import { useSearchParams } from 'react-router-dom'
+
+import { Button, Popper } from '~/components/designSystem'
+import { TextInput } from '~/components/form/TextInput/TextInput'
+import { MEMBERS_PAGE_ROLE_FILTER_KEY } from '~/core/constants/roles'
+import { useInternationalization } from '~/hooks/core/useInternationalization'
+import { useRoleDisplayInformation } from '~/hooks/useRoleDisplayInformation'
+import { useRolesList } from '~/hooks/useRolesList'
+import { MenuPopper } from '~/styles'
+
+export type MembersFiltersProps = {
+  searchQuery: string
+  setSearchQuery: (query: string) => void
+  type: 'members' | 'invitations'
+  openCreateInviteDialog: () => void
+}
+
+const CREATE_INVITE_BUTTON_TEST_ID = 'create-invite-button'
+
+const MembersFilters = ({
+  searchQuery,
+  setSearchQuery,
+  type,
+  openCreateInviteDialog,
+}: MembersFiltersProps) => {
+  const { translate } = useInternationalization()
+  const { roles } = useRolesList()
+  const [searchParams, setSearchParams] = useSearchParams()
+  const { getDisplayName } = useRoleDisplayInformation()
+
+  const selectedRole = useMemo(() => {
+    return searchParams.get(MEMBERS_PAGE_ROLE_FILTER_KEY)
+  }, [searchParams])
+
+  const getSearchPlaceholder = () => {
+    if (type === 'members') {
+      return translate('text_1767713872664devzn1r2wql')
+    }
+
+    return translate('text_1767713872664lwivpxg5xlb')
+  }
+
+  const handleRoleFilterChange = (newRole: string | null) => {
+    const newSearchParams = new URLSearchParams(searchParams)
+
+    if (!newRole) {
+      newSearchParams.delete(MEMBERS_PAGE_ROLE_FILTER_KEY)
+    } else {
+      newSearchParams.set(MEMBERS_PAGE_ROLE_FILTER_KEY, newRole)
+    }
+
+    setSearchParams(newSearchParams)
+  }
+
+  const getFilterRoleDisplayName = () => {
+    if (!selectedRole) {
+      return translate('text_1767710145806r6ewupk6dr8')
+    }
+
+    return getDisplayName({ name: selectedRole })
+  }
+
+  return (
+    <>
+      <div className="flex h-16 items-center justify-between gap-3 shadow-b">
+        <div className="flex items-center gap-3">
+          <TextInput
+            cleanable
+            placeholder={getSearchPlaceholder()}
+            value={searchQuery}
+            onChange={(value) => {
+              setSearchQuery(value)
+            }}
+            InputProps={{
+              startAdornment: <Icon className="ml-4" name="magnifying-glass" />,
+            }}
+          />
+          <Popper
+            PopperProps={{ placement: 'bottom-start' }}
+            opener={
+              <Button variant="secondary" endIcon="chevron-down">
+                {getFilterRoleDisplayName()}
+              </Button>
+            }
+          >
+            {({ closePopper }) => (
+              <MenuPopper>
+                <Button
+                  variant="quaternary"
+                  align="left"
+                  onClick={() => {
+                    handleRoleFilterChange(null)
+                    closePopper()
+                  }}
+                >
+                  {translate('text_1767710145806r6ewupk6dr8')}
+                </Button>
+                {roles.map((role) => (
+                  <Button
+                    variant="quaternary"
+                    align="left"
+                    key={role.name}
+                    onClick={() => {
+                      handleRoleFilterChange(role.name)
+                      closePopper()
+                    }}
+                  >
+                    {getDisplayName({ name: role.name })}
+                  </Button>
+                ))}
+              </MenuPopper>
+            )}
+          </Popper>
+        </div>
+        <Button
+          variant="inline"
+          onClick={openCreateInviteDialog}
+          data-test={CREATE_INVITE_BUTTON_TEST_ID}
+        >
+          {translate('text_63208b630aaf8df6bbfb265b')}
+        </Button>
+      </div>
+    </>
+  )
+}
+
+export default MembersFilters
---
+++ src/pages/settings/members/dialogs/EditMemberRoleDialog.tsx
@@ -0,0 +1,140 @@
+import { Stack } from '@mui/material'
+import { revalidateLogic } from '@tanstack/react-form'
+import { forwardRef, useImperativeHandle, useRef, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { z } from 'zod'
+
+import { Alert, Avatar, Button, Dialog, DialogRef, Typography } from '~/components/designSystem'
+import { HOME_ROUTE } from '~/core/router'
+import { MemberForEditRoleForDialogFragment, MembershipRole } from '~/generated/graphql'
+import { useInternationalization } from '~/hooks/core/useInternationalization'
+import { useAppForm } from '~/hooks/forms/useAppform'
+
+import RolePicker from './RolePicker'
+
+import { UpdateInviteSingleRole } from '../common/inviteTypes'
+import { useMembershipActions } from '../hooks/useMembershipActions'
+
+export interface EditMemberRoleDialogRef {
+  openDialog: (localData: MemberForEditRoleForDialogImperativeProps) => unknown
+  closeDialog: () => unknown
+}
+
+type MemberForEditRoleForDialogImperativeProps = {
+  member: MemberForEditRoleForDialogFragment | null
+  isEditingLastAdmin: boolean
+  isEditingMyOwnMembership: boolean
+}
+
+export const EDIT_MEMBER_ROLE_FORM_ID = 'form-edit-member-role'
+
+export const EditMemberRoleDialog = forwardRef<EditMemberRoleDialogRef>((_, ref) => {
+  const { translate } = useInternationalization()
+  const navigate = useNavigate()
+  const { updateMembershipRole } = useMembershipActions()
+  const dialogRef = useRef<DialogRef>(null)
+  const [localData, setLocalData] = useState<MemberForEditRoleForDialogImperativeProps | null>(null)
+
+  const validationSchema = z.object({
+    role: z.string(),
+  })
+
+  const initialValues: UpdateInviteSingleRole = {
+    role: localData?.member?.roles[0] || 'Admin',
+  }
+
+  const form = useAppForm({
+    defaultValues: initialValues,
+    validationLogic: revalidateLogic(),
+    validators: {
+      onDynamic: validationSchema,
+    },
+    onSubmit: async ({ value }) => {
+      const res = await updateMembershipRole({
+        variables: {
+          input: {
+            roles: [value.role],
+            id: localData?.member?.id as string,
+          },
+        },
+      })
+
+      dialogRef.current?.closeDialog()
+
+      // If you edit your own memberships role to something else than admin, you will get redirected to home page
+      if (
+        localData?.isEditingMyOwnMembership &&
+        res.data?.updateMembership?.roles[0] !== MembershipRole.Admin
+      ) {
+        // The redirection have to be retriggered on the next tick to avoid wrong forbidden page display
+        setTimeout(() => {
+          navigate(HOME_ROUTE)
+        }, 1)
+      }
+    },
+  })
+
+  useImperativeHandle(ref, () => ({
+    openDialog: (data) => {
+      setLocalData(data)
+      dialogRef.current?.openDialog()
+    },
+    closeDialog: () => {
+      dialogRef.current?.closeDialog()
+    },
+  }))
+
+  const handleClose = () => {
+    form.reset()
+  }
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    form.handleSubmit()
+  }
+
+  const getActions = ({ closeDialog }: { closeDialog: () => void }) => {
+    return (
+      <>
+        <Button variant="quaternary" onClick={closeDialog}>
+          {translate('text_62bb10ad2a10bd182d002031')}
+        </Button>
+        <form.AppForm>
+          <form.SubmitButton>{translate('text_664f035a68227f00e261b7f6')}</form.SubmitButton>
+        </form.AppForm>
+      </>
+    )
+  }
+
+  return (
+    <Dialog
+      ref={dialogRef}
+      title={translate('text_664f035a68227f00e261b7e9')}
+      onClose={handleClose}
+      actions={getActions}
+      formId={EDIT_MEMBER_ROLE_FORM_ID}
+      formSubmit={handleSubmit}
+    >
+      <div className="mb-8 flex flex-col gap-8">
+        <Stack gap={3} direction="row" alignItems="center">
+          <Avatar
+            variant="user"
+            identifier={(localData?.member?.user?.email || '').charAt(0)}
+            size="big"
+          />
+          <Typography variant="body" color="grey700">
+            {localData?.member?.user?.email}
+          </Typography>
+        </Stack>
+
+        <RolePicker form={form} fields={{ role: 'role' }} />
+
+        {localData?.isEditingLastAdmin && (
+          <Alert type="danger">{translate('text_664f035a68227f00e261b7f4')}</Alert>
+        )}
+      </div>
+    </Dialog>
+  )
+})
+
+EditMemberRoleDialog.displayName = 'forwardRef'
