493cfb3135b261bf3878496e22a643c3b988aaa3
Allan Michay <allan.michay@gmail.com>
2026-01-08 15:37:10 +0100

    misc: update snapshots test to remove dynamic part of mui

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,2 +1,6 @@
 // Console suppression is handled in jest-setup-early.ts (runs before imports)
 import '@testing-library/jest-dom'
+
+import muiSnapshotSerializer from './src/test-utils/snapshotSerializer'
+
+expect.addSnapshotSerializer(muiSnapshotSerializer)
