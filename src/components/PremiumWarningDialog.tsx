341b4f2a86f6606eac5b874313f629cb9b1c7e72
Allan Michay <allan.michay@gmail.com>
2026-02-05 10:52:41 +0100

    go back to old PremiumWarningDialog

--- src/components/PremiumWarningDialog.tsx
+++ src/components/PremiumWarningDialog.tsx
@@ -42,7 +42,9 @@ export const PremiumWarningDialog = forwardRef<PremiumWarningDialogRef>((_, ref)
           </Button>
           <a
             className="mb-0 mr-0"
-            href={`mailto:hello@getlago.com?subject=${localData?.mailtoSubject || translate('text_63b3f676d44671bf24d81411')}&body=${localData?.mailtoBody || translate('text_63b3f676d44671bf24d81413')}`}
+            href={`mailto:hello@getlago.com?subject=${
+              localData?.mailtoSubject || translate('text_63b3f676d44671bf24d81411')
+            }&body=${localData?.mailtoBody || translate('text_63b3f676d44671bf24d81413')}`}
           >
             <Button className="w-full">{translate('text_63b3155768489ee342482f55')}</Button>
           </a>
