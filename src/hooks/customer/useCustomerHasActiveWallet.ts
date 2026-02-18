38f4fc1fe25ad1a9ee489f118d44fa7eafa55180
Stefan M <stefan@getlago.com>
2026-02-18 14:22:32 +0200

    fix: ignore not found error

--- src/hooks/customer/useCustomerHasActiveWallet.ts
+++ src/hooks/customer/useCustomerHasActiveWallet.ts
@@ -1,9 +1,10 @@
-import { useGetCustomerWalletListQuery, WalletStatusEnum } from '~/generated/graphql'
+import { LagoApiError, useGetCustomerWalletListQuery, WalletStatusEnum } from '~/generated/graphql'
 
 export const useCustomerHasActiveWallet = ({ customerId }: { customerId?: string | null }) => {
   const { data: customerWalletData } = useGetCustomerWalletListQuery({
     variables: { customerId: customerId as string, page: 0, limit: 20 },
     skip: !customerId,
+    context: { silentErrorCodes: [LagoApiError.NotFound] },
   })
 
   const list = customerWalletData?.wallets?.collection || []
