0ed0594305ae545afed5d9b0dc8616eab0900d4c
Allan Michay <allan.michay@gmail.com>
2026-02-18 14:45:50 +0100

    fix: fix sonarqube feedback

--- src/pages/settings/teamAndSecurity/members/Members.tsx
+++ src/pages/settings/teamAndSecurity/members/Members.tsx
@@ -18,47 +18,45 @@ const Members = () => {
   const { translate } = useInternationalization()
 
   return (
-    <>
-      <SettingsPaddedContainer>
-        <SettingsPageHeaderContainer>
-          <Typography variant="headline">{translate('text_63208b630aaf8df6bbfb2657')}</Typography>
-          <Typography>{translate('text_63208b630aaf8df6bbfb2659')}</Typography>
-        </SettingsPageHeaderContainer>
+    <SettingsPaddedContainer>
+      <SettingsPageHeaderContainer>
+        <Typography variant="headline">{translate('text_63208b630aaf8df6bbfb2657')}</Typography>
+        <Typography>{translate('text_63208b630aaf8df6bbfb2659')}</Typography>
+      </SettingsPageHeaderContainer>
 
-        <div>
-          <NavigationTab
-            tabs={[
-              {
-                title: translate('text_63208b630aaf8df6bbfb2655'),
-                match: [
-                  generatePath(TEAM_AND_SECURITY_GROUP_ROUTE, {
-                    group: teamAndSecurityGroupOptions.members,
-                  }),
-                  generatePath(TEAM_AND_SECURITY_TAB_ROUTE, {
-                    group: teamAndSecurityGroupOptions.members,
-                    tab: teamAndSecurityTabOptions.members,
-                  }),
-                ],
-                link: generatePath(TEAM_AND_SECURITY_TAB_ROUTE, {
+      <div>
+        <NavigationTab
+          tabs={[
+            {
+              title: translate('text_63208b630aaf8df6bbfb2655'),
+              match: [
+                generatePath(TEAM_AND_SECURITY_GROUP_ROUTE, {
+                  group: teamAndSecurityGroupOptions.members,
+                }),
+                generatePath(TEAM_AND_SECURITY_TAB_ROUTE, {
                   group: teamAndSecurityGroupOptions.members,
                   tab: teamAndSecurityTabOptions.members,
                 }),
+              ],
+              link: generatePath(TEAM_AND_SECURITY_TAB_ROUTE, {
+                group: teamAndSecurityGroupOptions.members,
+                tab: teamAndSecurityTabOptions.members,
+              }),
 
-                component: <MembersList />,
-              },
-              {
-                title: translate('text_1728310120853rutc5q05ax6'),
-                link: generatePath(TEAM_AND_SECURITY_TAB_ROUTE, {
-                  group: teamAndSecurityGroupOptions.members,
-                  tab: teamAndSecurityTabOptions.invitations,
-                }),
-                component: <MembersInvitationList />,
-              },
-            ]}
-          />
-        </div>
-      </SettingsPaddedContainer>
-    </>
+              component: <MembersList />,
+            },
+            {
+              title: translate('text_1728310120853rutc5q05ax6'),
+              link: generatePath(TEAM_AND_SECURITY_TAB_ROUTE, {
+                group: teamAndSecurityGroupOptions.members,
+                tab: teamAndSecurityTabOptions.invitations,
+              }),
+              component: <MembersInvitationList />,
+            },
+          ]}
+        />
+      </div>
+    </SettingsPaddedContainer>
   )
 }
 
