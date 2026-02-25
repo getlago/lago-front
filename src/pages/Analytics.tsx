4fca4c484c2fdc7113c5405f2194296df2dfb3ac
Stefan M <stefan@getlago.com>
2026-02-25 17:51:22 +0200

    refactor: show analytics v2 for users when superset is not available

--- src/pages/Analytics.tsx
+++ src/pages/Analytics.tsx
@@ -2,6 +2,7 @@ import { Spinner } from '~/components/designSystem/Spinner'
 import { envGlobalVar } from '~/core/apolloClient'
 import { PremiumIntegrationTypeEnum } from '~/generated/graphql'
 import { useOrganizationInfos } from '~/hooks/useOrganizationInfos'
+import NewAnalytics from '~/pages/analytics/NewAnalytics'
 import Dashboards from '~/pages/dashboards/Dashboards'
 import OldAnalytics from '~/pages/OldAnalytics'
 
@@ -14,15 +15,20 @@ const Analytics = () => {
     organization,
   } = useOrganizationInfos()
 
-  const hasAccessToAnalyticsDashboardsFeature =
-    hasOrganizationPremiumAddon(PremiumIntegrationTypeEnum.AnalyticsDashboards) && lagoSupersetUrl
+  const hasAccessToAnalyticsDashboardsFeature = hasOrganizationPremiumAddon(
+    PremiumIntegrationTypeEnum.AnalyticsDashboards,
+  )
 
   if (organizationDataLoading || !organization?.id) {
     return <Spinner />
   }
 
   if (hasAccessToAnalyticsDashboardsFeature) {
-    return <Dashboards />
+    if (lagoSupersetUrl) {
+      return <Dashboards />
+    }
+
+    return <NewAnalytics />
   }
 
   return <OldAnalytics />
