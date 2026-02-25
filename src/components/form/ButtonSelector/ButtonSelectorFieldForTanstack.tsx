ce644da7d8551fce49b882888e24fc8ac28946af
Alexandre Monjol <alexandre.monjol@gmail.com>
2026-02-25 11:55:09 -0300

    misc: add error handling in button selector for tanstack

--- src/components/form/ButtonSelector/ButtonSelectorFieldForTanstack.tsx
+++ src/components/form/ButtonSelector/ButtonSelectorFieldForTanstack.tsx
@@ -1,17 +1,32 @@
+import { useStore } from '@tanstack/react-form'
+
+import { getErrorToDisplay } from '~/core/form/getErrorToDisplay'
 import { useFieldContext } from '~/hooks/forms/formContext'
 
 import { ButtonSelector, ButtonSelectorProps } from './ButtonSelector'
 
 type ValueType = string | number | boolean
 
-const ButtonSelectorField = (props: Omit<ButtonSelectorProps, 'value' | 'onChange'>) => {
+const ButtonSelectorField = (props: Omit<ButtonSelectorProps, 'value' | 'onChange' | 'error'>) => {
   const field = useFieldContext<ValueType>()
+
+  const error = useStore(field.store, (state) => state.meta.errors)
+    .map((e) => e.message)
+    .join('')
+
+  const errorMap = useStore(field.store, (state) => state.meta.errorMap)
+
+  const finalError = getErrorToDisplay({
+    error,
+    errorMap,
+  })
 
   return (
     <ButtonSelector
       {...props}
       value={field.state.value}
       onChange={(value) => field.handleChange(value)}
+      error={typeof finalError === 'string' ? finalError : undefined}
     />
   )
 }
