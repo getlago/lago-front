a557f815b9fc6cea9f4ebcddb47b37323695de45
Allan Michay <allan.michay@gmail.com>
2026-02-25 11:40:28 +0100

    feat(team): display security logs

---
+++ src/pages/settings/teamAndSecurity/securityLogs/SecurityLogs.tsx
@@ -0,0 +1,76 @@
+import { InfiniteScroll } from '~/components/designSystem/InfiniteScroll'
+import { Table, TableColumn } from '~/components/designSystem/Table'
+import { Typography } from '~/components/designSystem/Typography'
+import { SettingsPageHeaderContainer } from '~/components/layouts/Settings'
+import { useInternationalization } from '~/hooks/core/useInternationalization'
+
+import { SecurityLogWithId } from './common/securityLogsTypes'
+import { useSecurityLogs } from './hooks/useSecurityLogs'
+import { useSecurityLogsFormatting } from './hooks/useSecurityLogsFormatting'
+
+const SecurityLogs = () => {
+  const { translate } = useInternationalization()
+
+  const { securityLogs, securityLogsMetadata, isLoadingSecurityLogs, fetchMoreSecurityLogs } =
+    useSecurityLogs()
+
+  const { getFormattedLogEvent, getSecurityLogDescription, getSecurityLogDate } =
+    useSecurityLogsFormatting()
+
+  const columns: Array<TableColumn<SecurityLogWithId>> = [
+    {
+      key: 'logEvent',
+      title: translate('text_6560809c38fb9de88d8a52fb'),
+      content: ({ logEvent }: SecurityLogWithId) => {
+        return <Typography variant="captionCode">{getFormattedLogEvent(logEvent)}</Typography>
+      },
+    },
+    {
+      key: 'logEvent',
+      title: translate('text_6388b923e514213fed58331c'),
+      maxSpace: true,
+      content: (securityLog: SecurityLogWithId) => {
+        return <Typography color="grey700">{getSecurityLogDescription(securityLog)}</Typography>
+      },
+    },
+    {
+      key: 'loggedAt',
+      title: translate('text_664cb90097bfa800e6efa3f5'),
+      minWidth: 130,
+      content: (securityLog: SecurityLogWithId) => {
+        return <Typography>{getSecurityLogDate(securityLog)}</Typography>
+      },
+    },
+  ]
+
+  return (
+    <div className="flex flex-col gap-4 px-12 pb-20 pt-10">
+      <SettingsPageHeaderContainer>
+        <Typography variant="headline">{translate('text_1771855827236eqkaiznri70')}</Typography>
+        <Typography>{translate('text_1771855926675ji0pee3p6a6')}</Typography>
+      </SettingsPageHeaderContainer>
+
+      <InfiniteScroll
+        onBottom={async () => {
+          const { currentPage = 0, totalPages = 0 } = securityLogsMetadata || {}
+
+          if (currentPage < totalPages && !isLoadingSecurityLogs) {
+            await fetchMoreSecurityLogs({
+              variables: { page: currentPage + 1 },
+            })
+          }
+        }}
+      >
+        <Table
+          name="security-logs"
+          containerSize={{ default: 4 }}
+          columns={columns}
+          data={securityLogs}
+          isLoading={isLoadingSecurityLogs}
+        />
+      </InfiniteScroll>
+    </div>
+  )
+}
+
+export default SecurityLogs
