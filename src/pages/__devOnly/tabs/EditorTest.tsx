4111d83fa54594695b5f291e8dd22d69404ca7c3
Allan Michay <allan.michay@gmail.com>
2026-03-11 15:49:16 +0100

    feat: save as markdown

--- src/pages/__devOnly/tabs/EditorTest.tsx
+++ src/pages/__devOnly/tabs/EditorTest.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 import { Button } from '~/components/designSystem/Button'
 import RichTextEditor, {
-  RichTextEditorMode,
+  type RichTextEditorMode,
 } from '~/components/designSystem/RichTextEditor/RichTextEditor'
 import { Typography } from '~/components/designSystem/Typography'
 
@@ -36,7 +36,13 @@ const EditorTest = () => {
         </Button>
       </div>
       <Block className="h-80">
-        <RichTextEditor mode={mode} mentionValues={mentionValues} />
+        <RichTextEditor
+          mode={mode}
+          mentionValues={mentionValues}
+          onSave={(markdown: string) => {
+            console.log('Editor markdown:', markdown)
+          }}
+        />
       </Block>
     </Container>
   )
