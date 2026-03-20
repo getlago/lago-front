9a9683ef6781cb6a0fda81f75a0718117a410869
memmof <59845319+domenicofalco@users.noreply.github.com>
2026-03-20 14:36:26 +0100

    refactor(MainHeader): migrate Analytics to new header architecture (#3230)
    ## Context
    Continuing the phased migration to MainHeader. The Analytics and
    Forecast pages were not redesigned, but should be aligned with the new
    header architecture for consistency.
    
    ## Description
    - Migrate Analytics page: replace PageHeader.Wrapper + NavigationTab
    with MainHeader.Configure using entity.viewName and tabs prop. Tab
    content is resolved via useMainHeaderTabContent().
    - Forecasts page: removed the old PageHeader.Wrapper but intentionally
    did not add MainHeader. The page has a high level of inline
    customization (AI badge, Beta chip, info tooltip) that doesn't map
    cleanly to the current MainHeader entity props. Left the existing body
    layout as-is. The header integration for Forecast should be discussed
    with Product to define the desired behavior. But for now it is
    consistent from UI views and can be released as is
    
    <!-- Linear link -->
    Fixes ISSUE-1687

--- src/pages/analytics/NewAnalytics.tsx
+++ src/pages/analytics/NewAnalytics.tsx
@@ -1,8 +1,8 @@
-import { useEffect } from 'react'
+import { useEffect, useMemo } from 'react'
 import { generatePath, useLocation, useNavigate } from 'react-router-dom'
 
-import { NavigationTab } from '~/components/designSystem/NavigationTab'
-import { Typography } from '~/components/designSystem/Typography'
+import { MainHeader } from '~/components/MainHeader/MainHeader'
+import { useMainHeaderTabContent } from '~/components/MainHeader/useMainHeaderTabContent'
 import { NewAnalyticsTabsOptionsEnum } from '~/core/constants/tabsOptions'
 import { ANALYTICS_V2_ROUTE, ANALYTICS_V2_TABS_ROUTE } from '~/core/router'
 import { PremiumIntegrationTypeEnum } from '~/generated/graphql'
@@ -13,7 +13,6 @@ import Mrr from '~/pages/analytics/Mrr'
 import PrepaidCredits from '~/pages/analytics/PrepaidCredits'
 import RevenueStreams from '~/pages/analytics/RevenueStreams'
 import Usage from '~/pages/analytics/Usage'
-import { PageHeader } from '~/styles'
 
 const NewAnalytics = () => {
   const { translate } = useInternationalization()
@@ -36,65 +35,67 @@ const NewAnalytics = () => {
     }
   }, [pathname, navigate])
 
+  const tabs = useMemo(
+    () => [
+      {
+        title: translate('text_1739203651003n5f5qzxnhin'),
+        link: generatePath(ANALYTICS_V2_TABS_ROUTE, {
+          tab: NewAnalyticsTabsOptionsEnum.revenueStreams,
+        }),
+        match: [
+          ANALYTICS_V2_ROUTE,
+          generatePath(ANALYTICS_V2_ROUTE),
+          generatePath(ANALYTICS_V2_TABS_ROUTE, {
+            tab: NewAnalyticsTabsOptionsEnum.revenueStreams,
+          }),
+        ],
+        content: <RevenueStreams />,
+      },
+      {
+        title: translate('text_6553885df387fd0097fd738c'),
+        link: generatePath(ANALYTICS_V2_TABS_ROUTE, {
+          tab: NewAnalyticsTabsOptionsEnum.mrr,
+        }),
+        content: <Mrr />,
+      },
+      {
+        title: translate('text_17465414264635ktqocy7leo'),
+        link: generatePath(ANALYTICS_V2_TABS_ROUTE, {
+          tab: NewAnalyticsTabsOptionsEnum.usage,
+        }),
+        hidden: !hasAccessToUsage,
+        content: <Usage />,
+      },
+      {
+        title: translate('text_1744192691931osnm4ckcvzj'),
+        link: generatePath(ANALYTICS_V2_TABS_ROUTE, {
+          tab: NewAnalyticsTabsOptionsEnum.prepaidCredits,
+        }),
+        content: <PrepaidCredits />,
+      },
+      {
+        title: translate('text_1745933666707rlg89cuv1i0'),
+        link: generatePath(ANALYTICS_V2_TABS_ROUTE, {
+          tab: NewAnalyticsTabsOptionsEnum.invoices,
+        }),
+        content: <Invoices />,
+      },
+    ],
+    [translate, hasAccessToUsage],
+  )
+
+  const activeTabContent = useMainHeaderTabContent()
+
   return (
     <>
-      <PageHeader.Wrapper withSide>
-        <Typography variant="bodyHl" color="grey700" noWrap>
-          {translate('text_6553885df387fd0097fd7384')}
-        </Typography>
-      </PageHeader.Wrapper>
-
-      <NavigationTab
-        className="px-4 md:px-12"
-        tabs={[
-          {
-            title: translate('text_1739203651003n5f5qzxnhin'),
-            link: generatePath(ANALYTICS_V2_TABS_ROUTE, {
-              tab: NewAnalyticsTabsOptionsEnum.revenueStreams,
-            }),
-            match: [
-              ANALYTICS_V2_ROUTE,
-              generatePath(ANALYTICS_V2_ROUTE),
-              generatePath(ANALYTICS_V2_TABS_ROUTE, {
-                tab: NewAnalyticsTabsOptionsEnum.revenueStreams,
-              }),
-            ],
-            component: <RevenueStreams />,
-          },
-          {
-            title: translate('text_6553885df387fd0097fd738c'),
-            link: generatePath(ANALYTICS_V2_TABS_ROUTE, {
-              tab: NewAnalyticsTabsOptionsEnum.mrr,
-            }),
-            component: <Mrr />,
-          },
-          ...(hasAccessToUsage
-            ? [
-                {
-                  title: translate('text_17465414264635ktqocy7leo'),
-                  link: generatePath(ANALYTICS_V2_TABS_ROUTE, {
-                    tab: NewAnalyticsTabsOptionsEnum.usage,
-                  }),
-                  component: <Usage />,
-                },
-              ]
-            : []),
-          {
-            title: translate('text_1744192691931osnm4ckcvzj'),
-            link: generatePath(ANALYTICS_V2_TABS_ROUTE, {
-              tab: NewAnalyticsTabsOptionsEnum.prepaidCredits,
-            }),
-            component: <PrepaidCredits />,
-          },
-          {
-            title: translate('text_1745933666707rlg89cuv1i0'),
-            link: generatePath(ANALYTICS_V2_TABS_ROUTE, {
-              tab: NewAnalyticsTabsOptionsEnum.invoices,
-            }),
-            component: <Invoices />,
-          },
-        ]}
+      <MainHeader.Configure
+        entity={{
+          viewName: translate('text_6553885df387fd0097fd7384'),
+        }}
+        tabs={tabs}
       />
+
+      {activeTabContent}
     </>
   )
 }
