ddb9ed2aceddf6779423e883c3d16c97eea42a59
Allan Michay <allan.michay@gmail.com>
2026-03-25 10:14:23 +0100

    test: add more tests

--- src/components/designSystem/RichTextEditor/TemplateSelector/TemplateSelectorView.tsx
+++ src/components/designSystem/RichTextEditor/TemplateSelector/TemplateSelectorView.tsx
@@ -2,6 +2,9 @@ import { NodeViewProps, NodeViewWrapper } from '@tiptap/react'
 
 import type { EditorTemplate } from './types'
 
+export const TEMPLATE_SELECTOR_VIEW_TEST_ID = 'template-selector-view'
+export const TEMPLATE_SELECTOR_ITEM_TEST_ID = 'template-selector-item'
+
 export const TemplateSelectorView = ({ editor, node }: NodeViewProps) => {
   const templates: EditorTemplate[] = node.attrs.templates ?? []
 
@@ -11,12 +14,13 @@ export const TemplateSelectorView = ({ editor, node }: NodeViewProps) => {
 
   return (
     <NodeViewWrapper>
-      <div className="template-list">
+      <div className="template-list" data-test={TEMPLATE_SELECTOR_VIEW_TEST_ID}>
         {templates.map((template) => (
           <button
             key={template.id}
             type="button"
             className="template-item"
+            data-test={`${TEMPLATE_SELECTOR_ITEM_TEST_ID}-${template.id}`}
             onClick={() => handleSelect(template)}
           >
             <svg className="template-item__icon" viewBox="0 0 16 16" fill="none">
