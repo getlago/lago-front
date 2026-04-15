d278137a10cb3ba7587d1271ed70ea43d82dda03
Allan Michay <allan.michay@gmail.com>
2026-04-15 12:49:32 +0200

    fix: fix typing issues on schema validation

--- src/pages/quotes/createQuote/validationSchema.ts
+++ src/pages/quotes/createQuote/validationSchema.ts
@@ -6,7 +6,7 @@ export const createQuoteSchema = z
   .object({
     customerId: z.string().min(1, 'text_1776238919927l1m2n3o4p5q'),
     orderType: z.nativeEnum(OrderTypeEnum),
-    subscriptionId: z.string().optional().default(''),
+    subscriptionId: z.string(),
   })
   .refine(
     (data) => {
