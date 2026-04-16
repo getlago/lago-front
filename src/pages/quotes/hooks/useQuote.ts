9baf4dacc9a671c933e825e09dafc914d8e1ad84
Allan Michay <allan.michay@gmail.com>
2026-04-16 10:20:40 +0200

    feat(quotes): display quotes (#3342)
    ## Context
    
    We are currently working on Quotes and how to display them
    
    ## Description
    
    Display quotes and quote details in their own pages
    
    <!-- Linear link -->
    Fixes LAGO-1392
    Fixes LAGO-1394

---
+++ src/pages/quotes/hooks/useQuote.ts
@@ -0,0 +1,45 @@
+import { gql } from '@apollo/client'
+
+import { QuoteDetailItemFragment, useGetQuoteQuery } from '~/generated/graphql'
+
+gql`
+  fragment QuoteDetailItem on Quote {
+    id
+    number
+    status
+    version
+    orderType
+    currency
+    createdAt
+    customer {
+      id
+      name
+      externalId
+    }
+  }
+
+  query getQuote($id: ID!) {
+    quote(id: $id) {
+      ...QuoteDetailItem
+    }
+  }
+`
+
+interface UseQuoteReturn {
+  quote: QuoteDetailItemFragment | null | undefined
+  loading: boolean
+  error: Error | undefined
+}
+
+export const useQuote = (id?: string): UseQuoteReturn => {
+  const { data, loading, error } = useGetQuoteQuery({
+    variables: { id: id || '' },
+    skip: !id,
+  })
+
+  return {
+    quote: data?.quote,
+    loading,
+    error,
+  }
+}
