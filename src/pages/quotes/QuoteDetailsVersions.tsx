241ceb5959d3289ccca9c834f381eec2f6e0ec12
Allan Michay <allan.michay@gmail.com>
2026-04-17 16:11:27 +0200

    fix: display owners only if number > 0

--- src/pages/quotes/QuoteDetailsVersions.tsx
+++ src/pages/quotes/QuoteDetailsVersions.tsx
@@ -81,16 +81,20 @@ const QuoteDetailsVersions = ({ quote }: QuoteDetailsVersionsProps): JSX.Element
       label: translate('text_6560809c38fb9de88d8a52fb'),
       value: translate(getQuoteOrderTypeTranslationKey(quote.orderType)),
     },
-    {
-      label: translate('text_1776429591588dnpx1guz0cl'),
-      value: (
-        <div className="flex flex-row gap-4">
-          {(quote.owners || []).map((owner) => (
-            <Chip key={owner.id} label={owner.email} />
-          ))}
-        </div>
-      ),
-    },
+    ...((quote.owners ?? []).length > 0
+      ? [
+          {
+            label: translate('text_1776429591588dnpx1guz0cl'),
+            value: (
+              <div className="flex flex-row gap-4">
+                {quote.owners?.map((owner) => (
+                  <Chip key={owner.id} label={owner.email} />
+                ))}
+              </div>
+            ),
+          },
+        ]
+      : []),
   ]
 
   return (
