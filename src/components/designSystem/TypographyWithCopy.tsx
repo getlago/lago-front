df8ae386701561a2df1a27c63cd848c6cc4c9285
Allan Michay <allan.michay@gmail.com>
2026-04-20 15:35:27 +0200

    feat: use typographywithcopy

--- src/components/designSystem/TypographyWithCopy.tsx
+++ src/components/designSystem/TypographyWithCopy.tsx
@@ -4,6 +4,7 @@ import { Button } from '~/components/designSystem/Button'
 import { Tooltip } from '~/components/designSystem/Tooltip'
 import { Typography, TypographyProps } from '~/components/designSystem/Typography'
 import { addToast } from '~/core/apolloClient'
+import { MaskOptions, maskValue } from '~/core/formats/maskValue'
 import { copyToClipboard } from '~/core/utils/copyToClipboard'
 import { useInternationalization } from '~/hooks/core/useInternationalization'
 import { tw } from '~/styles/utils'
@@ -11,21 +12,36 @@ import { tw } from '~/styles/utils'
 export const TYPOGRAPHY_WITH_COPY_CONTAINER_TEST_ID = 'typography-with-copy-container'
 export const TYPOGRAPHY_WITH_COPY_BUTTON_TEST_ID = 'typography-with-copy-button'
 
-export const TypographyWithCopy: FC<TypographyProps> = ({
+interface TypographyWithCopyProps extends TypographyProps {
+  masked?: boolean
+  maskOptions?: MaskOptions
+}
+
+export const TypographyWithCopy: FC<TypographyWithCopyProps> = ({
   children,
   className,
+  masked,
+  maskOptions,
   ...typographyProps
 }) => {
   const { translate } = useInternationalization()
 
+  if (masked) {
+    return (
+      <Typography className={className} {...typographyProps}>
+        {maskOptions ? maskValue(children as string, maskOptions) : children}
+      </Typography>
+    )
+  }
+
   return (
-    <Tooltip placement="top-start" title={translate('text_623b42ff8ee4e000ba87d0c6')}>
+    <Tooltip placement="top" title={translate('text_623b42ff8ee4e000ba87d0c6')}>
       <Button
         data-test={TYPOGRAPHY_WITH_COPY_BUTTON_TEST_ID}
         endIcon="duplicate"
         variant="quaternary"
         size="small"
-        className={tw('-m-1 px-1 py-0', className)}
+        className={tw('-ml-1 px-1 py-0', className)}
         onClick={(e) => {
           e.stopPropagation()
           copyToClipboard(children as string)
