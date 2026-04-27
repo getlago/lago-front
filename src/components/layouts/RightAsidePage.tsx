c939cd5fb03332406dad1fc52dddea81a6d36f2d
Allan Michay <allan.michay@gmail.com>
2026-04-27 09:41:51 +0200

    feat: create edit quote layout

---
+++ src/components/layouts/RightAsidePage.tsx
@@ -0,0 +1,57 @@
+import { Button } from '../designSystem/Button'
+
+const Wrapper = ({ children }: { children: React.ReactNode }) => {
+  return <div className="flex h-full flex-col">{children}</div>
+}
+
+type ContentProps = {
+  children: React.ReactNode
+  aside: React.ReactNode
+}
+
+const Content = ({ children, aside }: ContentProps) => {
+  return (
+    <div className="flex flex-1 flex-row overflow-hidden">
+      <div className="flex-1 overflow-auto">{children}</div>
+      <div className="w-[400px] shrink-0 overflow-auto border-l border-grey-300">{aside}</div>
+    </div>
+  )
+}
+
+type HeaderProps = {
+  children?: React.ReactNode
+  title: React.ReactNode
+  onClose: () => void
+}
+
+type SubheaderProps = {
+  children?: React.ReactNode
+}
+
+const Header = ({ title, onClose, children }: HeaderProps) => {
+  return (
+    <div className="flex flex-row items-center justify-between gap-4 px-12 py-3 shadow-b">
+      {title}
+      <div className="flex flex-row items-center gap-3">
+        {children}
+        {children && <div className="h-10 border-l border-grey-300" />}
+        <Button variant="quaternary" icon="close" onClick={onClose} />
+      </div>
+    </div>
+  )
+}
+
+const SubHeader = ({ children }: SubheaderProps) => {
+  return (
+    <div className="flex flex-row items-center justify-between gap-4 px-12 py-3 shadow-b">
+      {children}
+    </div>
+  )
+}
+
+export const RightAsidePage = {
+  Wrapper,
+  Header,
+  SubHeader,
+  Content,
+}
--- src/pages/quotes/EditQuote.tsx
+++ src/pages/quotes/EditQuote.tsx
@@ -1,18 +1,25 @@
 import { generatePath, useNavigate, useParams } from 'react-router-dom'
 
 import { Button } from '~/components/designSystem/Button'
+import RichTextEditor from '~/components/designSystem/RichTextEditor/RichTextEditor'
+import { Skeleton } from '~/components/designSystem/Skeleton'
+import { Status } from '~/components/designSystem/Status'
 import { Typography } from '~/components/designSystem/Typography'
-import { CenteredPage } from '~/components/layouts/CenteredPage'
+import { RightAsidePage } from '~/components/layouts/RightAsidePage'
 import { QuoteDetailsTabsOptionsEnum } from '~/core/constants/tabsOptions'
 import { QUOTE_DETAILS_ROUTE } from '~/core/router'
 import { useInternationalization } from '~/hooks/core/useInternationalization'
 
-export const EDIT_QUOTE_CLOSE_BUTTON_TEST_ID = 'edit-quote-close-button'
+import { getQuoteStatusMapping } from './common/getQuoteStatusMapping'
+import { useQuote } from './hooks/useQuote'
+
+export const EDIT_QUOTE_SAVE_BUTTON_TEST_ID = 'edit-quote-save-button'
 
 const EditQuote = () => {
   const { translate } = useInternationalization()
   const navigate = useNavigate()
   const { quoteId } = useParams()
+  const { quote, loading } = useQuote(quoteId)
 
   const onClose = () => {
     if (quoteId) {
@@ -26,21 +33,36 @@ const EditQuote = () => {
   }
 
   return (
-    <CenteredPage.Wrapper>
-      <CenteredPage.Header>
-        <Typography className="font-medium text-grey-700">
+    <RightAsidePage.Wrapper>
+      <RightAsidePage.Header
+        title={
+          <div className="flex flex-row items-center gap-2">
+            {loading && (
+              <>
+                <Skeleton variant="text" className="w-40" />
+                <Skeleton variant="text" className="w-12" />
+              </>
+            )}
+            {!loading && quote && (
+              <>
+                <Typography variant="bodyHl" color="grey700">
+                  {quote.number} - v{quote.version}
+                </Typography>
+                <Status {...getQuoteStatusMapping(quote.status, translate)} />
+              </>
+            )}
+          </div>
+        }
+        onClose={onClose}
+      >
+        <Button variant="secondary" data-testid={EDIT_QUOTE_SAVE_BUTTON_TEST_ID} onClick={onClose}>
           {translate('text_1776414006125387qynzm000')}
-        </Typography>
-        <Button
-          data-test={EDIT_QUOTE_CLOSE_BUTTON_TEST_ID}
-          variant="quaternary"
-          icon="close"
-          onClick={() => onClose()}
-        />
-      </CenteredPage.Header>
-
-      <CenteredPage.Container>{/* Rich text editor will be added here */}</CenteredPage.Container>
-    </CenteredPage.Wrapper>
+        </Button>
+      </RightAsidePage.Header>
+      <RightAsidePage.Content aside={<div />}>
+        <RichTextEditor />
+      </RightAsidePage.Content>
+    </RightAsidePage.Wrapper>
   )
 }
 
