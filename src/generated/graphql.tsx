2f52f44bd06f51078af1fe44a1beb64c22745ff4
Allan Michay <allan.michay@gmail.com>
2026-04-27 15:57:59 +0200

    fix: fix conflicts

--- src/generated/graphql.tsx
+++ src/generated/graphql.tsx
@@ -3898,8 +3898,6 @@ export enum FeatureFlagEnum {
   NonPersistableChargeCacheOptimization = 'non_persistable_charge_cache_optimization',
   OrderForms = 'order_forms',
   PaymentGatedSubscriptions = 'payment_gated_subscriptions',
-  OrderForms = 'order_forms',
-  PaymentGatedSubscriptions = 'payment_gated_subscriptions',
   PostgresEnrichedEvents = 'postgres_enriched_events',
   WalletTraceability = 'wallet_traceability'
 }
