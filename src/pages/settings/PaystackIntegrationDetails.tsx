53735f9cdfa9dec2a6ca1e38abbab3cbc32f60d7
Samuel Ajisegiri <samajisegiri@gmail.com>
2026-05-18 19:18:43 +0100

    fix(router): use slug-aware navigation for Paystack

--- src/pages/settings/PaystackIntegrationDetails.tsx
+++ src/pages/settings/PaystackIntegrationDetails.tsx
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client'
 import { Icon } from 'lago-design-system'
 import { useMemo, useRef } from 'react'
-import { generatePath, useNavigate, useParams } from 'react-router-dom'
+import { generatePath, useParams } from 'react-router-dom'
 
 import { Alert } from '~/components/designSystem/Alert'
 import { Button } from '~/components/designSystem/Button'
@@ -25,7 +25,7 @@ import {
 } from '~/components/settings/integrations/DeletePaystackIntegrationDialog'
 import { addToast, envGlobalVar } from '~/core/apolloClient'
 import { IntegrationsTabsOptionsEnum } from '~/core/constants/tabsOptions'
-import { INTEGRATIONS_ROUTE, PAYSTACK_INTEGRATION_ROUTE } from '~/core/router'
+import { INTEGRATIONS_ROUTE, PAYSTACK_INTEGRATION_ROUTE, useNavigate } from '~/core/router'
 import { copyToClipboard } from '~/core/utils/copyToClipboard'
 import {
   DeletePaystackIntegrationDialogFragmentDoc,
