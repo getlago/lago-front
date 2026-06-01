f7b70f5e479a5e222cae4a2e8cef9b3d88dabc3a
Allan Michay <allan.michay@gmail.com>
2026-06-01 18:24:55 +0200

    feat: style clickable pricing for invoices

--- src/components/designSystem/RichTextEditor/PricingBlock/PricingBlockView.tsx
+++ src/components/designSystem/RichTextEditor/PricingBlock/PricingBlockView.tsx
@@ -1,4 +1,10 @@
 import { NodeViewProps, NodeViewWrapper } from '@tiptap/react'
+import { Icon } from 'lago-design-system'
+
+import { intlFormatNumber } from '~/core/formats/intlFormatNumber'
+import { CurrencyEnum } from '~/generated/graphql'
+import { useInternationalization } from '~/hooks/core/useInternationalization'
+import { useOrganizationInfos } from '~/hooks/useOrganizationInfos'
 
 import { useRichTextEditorContext } from '../common/RichTextEditorContext'
 import { PricingType } from '../extensions/PricingBlock.schema'
@@ -9,6 +15,9 @@ export const PRICING_BLOCK_VIEW_UNRESOLVED_TEST_ID = 'pricing-block-view-unresol
 
 export const PricingBlockView = ({ node, updateAttributes, selected }: NodeViewProps) => {
   const { entities, onPricingCommand } = useRichTextEditorContext()
+  const { translate } = useInternationalization()
+  const { organization } = useOrganizationInfos()
+  const currency = organization?.defaultCurrency ?? CurrencyEnum.Usd
 
   const pricingType = (node.attrs.pricingType ?? 'plan') as PricingType
   const entityIds = (node.attrs.entityIds ?? []) as string[]
@@ -47,18 +56,40 @@ export const PricingBlockView = ({ node, updateAttributes, selected }: NodeViewP
     const displayText =
       pricingType === 'plan'
         ? `${resolvedEntities[0].name} (${resolvedEntities[0].code})`
-        : resolvedEntities.map((e) => e.name).join(', ')
+        : translate('text_17803276502818bsd9sn8888', {
+            subtotal: intlFormatNumber(
+              resolvedEntities.reduce(
+                (sum, entity) => sum + Number.parseFloat(entity.totalAmount ?? '0'),
+                0,
+              ),
+              { currency },
+            ),
+          })
 
     return (
       <NodeViewWrapper className="spacer" data-type="pricingBlock">
         <div className="block-wrapper">
-          <button
-            className={`pricing-block pricing-block--clickable ${selected ? 'pricing-block--selected' : ''}`}
-            onClick={handleClick}
-            data-test={PRICING_BLOCK_VIEW_TEST_ID}
-          >
-            <span>{displayText}</span>
-          </button>
+          <div className="block-type-wrapper">
+            <div className="block-type-tag">{translate('text_1779802343219a1cl5ckvtrn')}</div>
+            <button
+              className="pricing-block pricing-block--clickable"
+              onClick={handleClick}
+              data-test={PRICING_BLOCK_VIEW_TEST_ID}
+            >
+              <div className="pricing-block-content">
+                <div className="icon-wrapper">
+                  <Icon name="document" />
+                </div>
+                <div className="pricing-block-text">
+                  <span>{displayText}</span>
+                  <span>{translate('text_1780329442633n0oe3prszsw')}</span>
+                </div>
+              </div>
+              <div className="click-icon-wrapper">
+                <Icon name="chevron-right-filled" />
+              </div>
+            </button>
+          </div>
         </div>
       </NodeViewWrapper>
     )
