aeb97bfe08ffce506fa8eea5e544600c70825de1
Stefan M <stefan@getlago.com>
2026-06-30 15:07:10 +0300

    refactor: remove history from the finance assistant

--- src/components/aiAgent/FinanceAssistantResult.tsx
+++ src/components/aiAgent/FinanceAssistantResult.tsx
@@ -1,9 +1,5 @@
-import { useState } from 'react'
-
 import { ChatMessages } from '~/components/aiAgent/ChatMessages'
 import { MarkdownContent } from '~/components/aiAgent/llmOutputs/Markdown'
-import { Button } from '~/components/designSystem/Button'
-import { Typography } from '~/components/designSystem/Typography'
 import { ChatMessage } from '~/hooks/aiAgent/aiAgentReducer'
 import { useInternationalization } from '~/hooks/core/useInternationalization'
 
@@ -13,7 +9,6 @@ type FinanceAssistantResultProps = {
 
 export const FinanceAssistantResult = ({ message }: FinanceAssistantResultProps) => {
   const { translate } = useInternationalization()
-  const [showSql, setShowSql] = useState(false)
   const result = message.financeAssistantResult
 
   if (!result) return null
@@ -31,29 +26,6 @@ export const FinanceAssistantResult = ({ message }: FinanceAssistantResultProps)
           <div className="min-w-max p-3">
             <MarkdownContent>{result.results}</MarkdownContent>
           </div>
-        </div>
-      )}
-
-      {!!result.sqlQuery && (
-        <div className="flex flex-col gap-2">
-          <Button
-            className="self-start"
-            size="small"
-            variant="inline"
-            onClick={() => setShowSql((value) => !value)}
-          >
-            {showSql
-              ? translate('text_1780562979520mxbgpqksusb')
-              : translate('text_17805629795206sa8qhvz16j')}
-          </Button>
-
-          {showSql && (
-            <pre className="overflow-x-auto rounded-lg bg-grey-100 p-3">
-              <Typography component="code" variant="caption" color="grey700">
-                {result.sqlQuery}
-              </Typography>
-            </pre>
-          )}
         </div>
       )}
     </div>
